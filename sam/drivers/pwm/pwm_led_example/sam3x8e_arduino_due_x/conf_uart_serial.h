483ed383abc385d6ceaa164c1274bf53fe351fc6
maintainer <Welcome2>
2012-09-05 15:19:34 +0200

    Update all Licences Check job : http://norvm-tools-m.norway.atmel.com/hudson/job/asf-update-licences-linux/15/

--- sam/drivers/pwm/pwm_led_example/sam3x8e_arduino_due_x/conf_uart_serial.h
+++ sam/drivers/pwm/pwm_led_example/sam3x8e_arduino_due_x/conf_uart_serial.h
@@ -6,23 +6,25 @@
  * Copyright (C) 2012 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
- * 
+ *
+ * \page License
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name of Atmel may not be used to endorse or promote products derived
  *    from this software without specific prior written permission.
- * 
+ *
  * 4. This software may only be redistributed and used in connection with an
  *    Atmel microcontroller product.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
@@ -34,7 +36,7 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * \asf_license_stop
  *
  */
--- sam/services/resistive_touch/example/sam3u4e_sam3u_ek/conf_ads7843.h
+++ sam/services/resistive_touch/example/sam3u4e_sam3u_ek/conf_ads7843.h
@@ -8,7 +8,7 @@
  * \asf_license_start
  *
  * \page License
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
