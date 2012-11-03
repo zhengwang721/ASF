0be51ba97505712c104086f2be7431652a7e9361
maintainer <DL-Global-ASF-Maintainers@corp.atmel.com>
2012-11-03 12:41:02 +0100

    re-integrate branch ASFP-2771 to main Fix bug ASFP-2771, ASFP-2772 and ASFP-2779

--- common/services/usb/class/hid/device/kbd/example/sam4lc4c_sam4l_ek/conf_clock.h
+++ common/services/usb/class/hid/device/kbd/example/sam4lc4c_sam4l_ek/conf_clock.h
@@ -50,8 +50,8 @@
 //#define CONFIG_SYSCLK_INIT_PBDMASK  (1 << SYSCLK_AST)
 //#define CONFIG_SYSCLK_INIT_HSBMASK  (1 << SYSCLK_PDCA_HSB)
 
-#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_RCSYS
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_OSC0
+//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_RCSYS
+#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_OSC0
 //#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_PLL0
 //#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_DFLL
 //#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_RC80M
@@ -63,9 +63,6 @@
 //#define CONFIG_RCFAST_FRANGE    1
 //#define CONFIG_RCFAST_FRANGE    2
 
-/* 0: disable PicoCache, 1: enable PicoCache  */
-#define CONFIG_HCACHE_ENABLE          1
-
 /* Fbus = Fsys / (2 ^ BUS_div) */
 #define CONFIG_SYSCLK_CPU_DIV         0
 #define CONFIG_SYSCLK_PBA_DIV         0
@@ -73,25 +70,47 @@
 #define CONFIG_SYSCLK_PBC_DIV         0
 #define CONFIG_SYSCLK_PBD_DIV         0
 
-//#define CONFIG_USBCLK_SOURCE        USBCLK_SRC_OSC0
-#define CONFIG_USBCLK_SOURCE        USBCLK_SRC_PLL0
+// ===== Disable all non-essential peripheral clocks
+//#define CONFIG_SYSCLK_INIT_CPUMASK  0
+//#define CONFIG_SYSCLK_INIT_PBAMASK  SYSCLK_USART1
+//#define CONFIG_SYSCLK_INIT_PBBMASK  0
+//#define CONFIG_SYSCLK_INIT_PBCMASK  0
+//#define CONFIG_SYSCLK_INIT_PBDMASK  0
+//#define CONFIG_SYSCLK_INIT_HSBMASK  0
 
-/* Fusb = Fsys / USB_div */
-#define CONFIG_USBCLK_DIV           1
-
+// ===== PLL Options
 #define CONFIG_PLL0_SOURCE          PLL_SRC_OSC0
+//#define CONFIG_PLL0_SOURCE          PLL_SRC_GCLK9
 
 /* Fpll0 = (Fclk * PLL_mul) / PLL_div */
 #define CONFIG_PLL0_MUL             (48000000UL / BOARD_OSC0_HZ)
 #define CONFIG_PLL0_DIV             1
+//#define CONFIG_PLL0_MUL             (192000000 / FOSC0) /* Fpll = (Fclk * PLL_mul) / PLL_div */
+//#define CONFIG_PLL0_DIV             4 /* Fpll = (Fclk * PLL_mul) / PLL_div */
 
+// ==== DFLL Options
+//#define CONFIG_DFLL0_SOURCE         GENCLK_SRC_OSC0
 //#define CONFIG_DFLL0_SOURCE         GENCLK_SRC_RCSYS
 //#define CONFIG_DFLL0_SOURCE         GENCLK_SRC_OSC32K
+//#define CONFIG_DFLL0_SOURCE         GENCLK_SRC_RC120M
 //#define CONFIG_DFLL0_SOURCE         GENCLK_SRC_RC32K
 
 /* Fdfll = (Fclk * DFLL_mul) / DFLL_div */
 //#define CONFIG_DFLL0_FREQ           48000000UL
+//#define CONFIG_DFLL0_MUL            ((4 * CONFIG_DFLL0_FREQ) / BOARD_OSC32_HZ)
+//#define CONFIG_DFLL0_DIV            4
 //#define CONFIG_DFLL0_MUL            (CONFIG_DFLL0_FREQ / BOARD_OSC32_HZ)
 //#define CONFIG_DFLL0_DIV            1
+
+// ===== USB Clock Source Options
+#define CONFIG_USBCLK_SOURCE        USBCLK_SRC_PLL0
+//#define CONFIG_USBCLK_SOURCE        USBCLK_SRC_DFLL
+
+/* Fusb = Fsys / USB_div */
+#define CONFIG_USBCLK_DIV           1
+
+// ===== GCLK9 option
+//#define CONFIG_GCLK9_SOURCE           GENCLK_SRC_GCLKIN0
+//#define CONFIG_GCLK9_DIV              1
 
 #endif /* CONF_CLOCK_H_INCLUDED */
