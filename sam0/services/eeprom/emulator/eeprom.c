3a5e5f8ded08a5269d1988efd0e0d38fd555dd68
Even Larsen <ted@ted-VirtualBox.(none)>
2012-11-13 13:37:37 +0100

    Adding EEPROM service

---
+++ sam0/services/eeprom/emulator/eeprom.c
@@ -0,0 +1,366 @@
+#include "asf.h"
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+
+/* Magic key is the ASCII codes for "AtEEPROMEmu.", which is much faster to check as uint32_t values  
+   than a string compare. But this numbers will show up as the string in studio memory view, 
+   which makes the data easy to identify */
+#define EEPROM_MAGIC_KEY_0                 0x41744545
+#define EEPROM_MAGIC_KEY_1                 0x50524f4d
+#define EEPROM_MAGIC_KEY_2                 0x456d752e
+#define EEPROM_MAGIC_KEY_COUNT             3
+
+#define EEPROM_MAJOR_VERSION        1
+#define EEPROM_MINOR_VERSION        0
+#define EEPROM_REVISION             0
+#define EEPROM_MASTER_PAGE_NUMBER   0
+
+#define EEPROM_HEADER_SIZE 4
+#define EEPROM_DATA_SIZE   60
+#define EEPROM_MAX_PAGES   124
+
+#define EMULATOR_MAX_PAGES    256
+#define EMULATOR_ROW_SIZE     256
+#define EMULATOR_PAGE_SIZE    64
+#define EMULATOR_PAGES_PR_ROW 4
+
+struct eeprom_data {
+	uint8_t header[EEPROM_HEADER_SIZE];
+	uint8_t data[EEPROM_DATA_SIZE];
+};
+
+struct page_translater {
+	uint8_t lpage;
+	uint8_t ppage;
+};
+
+struct eeprom_master_page {
+        uint32_t magic_key[3];
+        uint8_t  major_version;
+        uint8_t  minor_version;
+        uint8_t  revision;
+
+	uint8_t  reserved[45];
+}
+
+struct {
+	bool initalized;
+	uint8_t *flash;
+
+	uint32_t physical_start_page;
+	uint16_t physical_pages;
+	uint8_t logical_pages;
+
+	uint8_t page_map[EMULATOR_MAX_PAGES];
+
+	uint8_t spare_row;
+
+	uint8_t cache_buffer[EMULATOR_PAGE_SIZE];
+	bool    cache_active;
+	uint8_t cached_page;
+} _eeprom_emulator_device;
+
+void _eeprom_emulator_copy_memory(uint32_t *dst, uint32_t *src, uint8_t size)
+{
+	uint8_t c;
+
+	for(c=0;c<size;++c) {
+		dst[c] = src[c];
+	}
+}
+
+void _eeprom_emulator_create_memory(void)
+{
+	struct eeprom_data data;
+	uint16_t ppage, lpage=0;
+
+	for(ppage=0;ppage<(_eeprom_emulator_device.physical_pages  - 4);++ppage)
+	{
+		// Is this a new row?
+		if(ppage%4 == 0) {
+			nvm_erase_row(ppage / 4);	
+		}
+
+		// Is this the first or second page of a row?
+		if((ppage%4 == 0) | (ppage%4 == 1)) {
+			data.header[0] = 0x40;
+			data.header[1] = lpage;
+
+			nvm_write_page(ppage, (uint32_t*)&data);
+
+			lpage++;
+		}
+	}
+
+	// Set last row as spare row
+	_eeprom_emulator_device.spare_row = (_eeprom_emulator_device.physical_pages / 4) - 1;
+}
+
+void _eeprom_emulator_scan_memory(void)
+{
+	uint16_t c;
+	uint8_t logical_page;
+	struct eeprom_data *data_ptr = (struct eeprom_data*)_eeprom_emulation_device.flash;
+
+	for(c=0;c<(_eeprom_emulator_device.physical_pages - 4);++c) {
+
+		logical_page = data_ptr[c].header[1];
+
+		if(logical_page != 0xff) {
+			_eeprom_emulator_device.page_map[logical_page] = c;
+		}
+	}
+}
+
+void _eeprom_emulator_clean_memory()
+{
+	uint16_t c,c2;
+	uint8_t current_row, last_row, last_page;
+	struct eeprom_data *data_ptr = (struct eeprom_data*)_eeprom_emulation_device.flash;
+
+	for(c=0;c<_eeprom_emulator_device.logical_pages;++c) {
+
+		last_row = 0xff;
+		current_row = 0xff;
+
+		for(c2=0;c2<_eeprom_emulator_device.physical_pages;++c2) {
+			current_row = c2 / 4;
+
+			if(data_ptr[c2].header[1] == _eeprom_emulator_device.page_map[c]) {
+				if(last_row == 0xff) {
+					last_row = current_row;
+					last_page = c2;
+				} else {
+					if(last_row != current_row) {
+
+						if(c2%4>1) {
+							nvm_erase_row(current_row);
+							_eeprom_emulator_device.page_map[c] = last_page;
+						} else {
+							nvm_erase_row(last_row);
+							_eeprom_emulator_device.page_map[c] = c2;
+						}
+					}
+				}
+			}
+
+
+		}
+	}
+}
+
+bool _eeprom_emulator_find_free_page(uint8_t current_page, uint8_t *new_page)
+{
+	uint8_t c;
+	uint8_t row = current_page / 4;
+	uint8_t rest_pages = 4 - (current_page % 4);
+
+	struct eeprom_data *ee_data = (struct eeprom_data*)_eeprom_emulation_device.flash;
+
+	// Is this the first page in a row?
+	if((current_page%4) == 0)
+		rest_pages = 4;
+
+	if(rest_pages>0) {
+		for(c=4-rest_pages;c<4;++c) {
+			uint8_t page = (row*4) + c;
+
+			if(ee_data[page].header[1] == 0xff) {
+				*new_page = page;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void _eeprom_emulator_scan_row(uint8_t row, struct page_translater *page_trans)
+{
+	struct eeprom_data *row_data = (struct eeprom_data*)&_eeprom_emulation_device.flash[row * EMULATOR_ROW_SIZE];
+	uint8_t c, c2=0, c3=0;
+
+	page_trans[0].lpage = row_data[0].header[1];
+	page_trans[0].ppage = row*4;
+
+	page_trans[1].lpage = row_data[1].header[1];
+	page_trans[1].ppage = (row*4) + 1;
+
+	for(c=0;c<2;++c) {
+
+		for(c2=2;c2<4;++c2) {
+			if(page_trans[c].lpage == row_data[c2].header[1]) {
+				page_trans[c].ppage = (row * 4) + c2;
+			}
+		}
+
+	}
+}
+
+void _eeprom_emulator_move_data_to_spare(uint8_t row, uint8_t lpage, uint8_t *data)
+{
+	struct page_translater page_trans[2];
+	uint8_t header[4];
+	uint8_t c;
+	uint32_t new_page;
+
+
+	// Scan row for content to be copied to spare row
+	_eeprom_emulation_scan_row(row, page_trans);
+
+	for(c=0;c<2;++c) {
+
+		new_page = ((_eeprom_emulator_device.spare_row * 4) + c);
+
+		if(lpage == page_trans[c].lpage) {
+			header[0] = 0x40;
+			header[1] = lpage;
+	
+			//Flush cache
+		
+			// Write header to SRAM cache
+			_eeprom_emulator_copy_memory((uint32_t*)_eeprom_emulator_device.cache_buffer, (uint32_t*)&header, 1);
+
+			// Write data to SRAM cache
+			_eeprom_emulator_copy_memory((uint32_t*)&_eeprom_emulator_device.cache_buffer[4], (uint32_t*)data, EEPROM_DATA_SIZE / 4);
+
+			// Write data to page buffer
+			nvm_write_page(new_page, (uint32_t*)_eeprom_emulator_device.cache_buffer);
+
+			_eeprom_emulator_device.page_map[page_trans[c].lpage] = new_page;
+			_eeprom_emulator_device.cached_page = page_trans[c].lpage;
+			_eeprom_emulator_device.cache_active = true;
+		} else {
+
+			//Flush cache
+
+			//Copy data buffer to cache buffer
+			_eeprom_emulator_copy_memory((uint32_t*)_eeprom_emulator_device.cache_buffer,
+					(uint32_t*)_eeprom_emulator_device.flash[page_trans[c].ppage * EMULATOR_PAGE_SIZE], EMULATOR_PAGE_SIZE / 4);
+
+			// Write data to page buffer
+			nvm_write_page(new_page, (uint32_t*) _eeprom_emulator_device.cache_buffer);
+
+			_eeprom_emulator_device.page_map[page_trans[c].lpage] = new_page;
+			_eeprom_emulator_device.cached_page = page_trans[c].lpage;
+			_eeprom_emulator_device.cache_active = true; 
+		}
+	}
+
+	// Erase row and set as spare row
+	nvm_erase_row(row);
+	_eeprom_emulator_device.spare_row = row;
+
+}
+
+void _eeprom_emulator_create_master_block(void)
+{
+        uint32_t magic_key[] = {MAGIC_KEY_0, MAGIC_KEY_1, MAGIC_KEY_2};
+        uint8_t c;
+	struct eeprom_master_page master_page;
+
+	for(c=0;c<EEPROM_MAGIC_KEY_COUNT;++c) {
+		master_page.magic_key = magic_key[c];
+	}
+
+	master_page.major_version = EEPROM_MAJOR_VERSION;
+	master_page.minor_version = EEPROM_MINOR_VERSION;
+	master_page.revision      = EEPROM_REVISION;
+
+	nvm_write_page(EEPROM_MASTER_PAGE_NUMBER, (uint32_t*)&master_page);
+}
+
+
+enum status_code eeprom_emulator_init(void)
+{
+	struct nvm_parameters parm;
+	struct eeprom_master_page master_page;
+
+        uint32_t magic_key[] = {MAGIC_KEY_0, MAGIC_KEY_1, MAGIC_KEY_2};
+        struct _eeprom_mem_header  = page;
+        uint8_t c;
+
+	_eeprom_emulator_device.flash = _eeprom_emulator_get_start_of_eeprom_memory_ptr();
+
+	nvn_get_parameters(&parm);
+
+        _eeprom_emulator_device.physical_pages = parm.number_of_pages;
+        _eeprom_emulator_device.logical_pages = (parm.number_of_pages / 2) - EMULATOR_PAGES_PR_ROW;
+                
+        _eeprom_emulator_device.cache_active = false;
+        _eeprom_emulator_device.cached_page = 0xff;
+
+	_eeprom_emulator_scan_memory();
+
+	eeprom_emulator_read_page(0, &master_page);
+
+	for(c=0;c<EEPROM_MAGIC_KEY_COUNT;++c) {
+		if(master_page.magic_key[c] != magic_key[c]
+			return STATUS_ERR_BAD_FORMAT;
+	}
+
+}
+
+
+void eeprom_emulator_write_page(uint8_t lpage, uint8_t *data)
+{
+	uint8_t eeprom_header[EEPROM_HEADER_SIZE];
+	uint8_t new_page;
+
+	eeprom_header[0] = 0x40;
+	eeprom_header[1] = lpage;
+
+	if(_eeprom_emulator_device.cache_active) {
+		if(_eeprom_emulator_device.cached_page != lpage) {
+			// Flush cache
+
+			if(!find_free_page(_eeprom_emulator_device.page_map[lpage], &new_page)) {
+				move_data_to_spare(_eeprom_emulator_device.page_map[lpage]  / 4, lpage , data);
+				return;
+			}
+
+			_eeprom_emulator_device.cache_active = false;
+		}
+	}
+
+	_eeprom_emulator_copy_memory((uint32_t*)_eeprom_emulator_device.cache_buffer, (uint32_t*)eeprom_header, EEPROM_HEADER_SIZE / 4);
+	_eeprom_emulator_copy_memory((uint32_t*)&_eeprom_emulator_device.cache_buffer[EEPROM_HEADER_SIZE], (uint32_t*)data, EEPROM_DATA_SIZE / 4);
+
+	nvm_write_page(new_page, (uint32_t*)_eeprom_emulator_device.cache_buffer);
+
+	_eeprom_emulator_device.page_map[lpage] = new_page;
+
+	_eeprom_emulator_device.cached_page = lpage;
+	_eeprom_emulator_device.cache_active = true;
+}
+
+enum status_code eeprom_emulator_read_page(uint8_t page, uint8_t *data)
+{
+	if(_eeprom_emulator_device.cache_active) {
+		if(_eeprom_emulator_device.cached_page == page) {
+			_eeprom_emulator_copy_memory((uint32_t*)data, &_eeprom_emulator_device.cache_buffer[4], EEPROM_DATA_SIZE); 
+			return;
+		}
+	}
+
+	_eeprom_emulator_copy_memory((uint32_t*)data, 
+			(uint32_t*)&_eeprom_emulator_device.flash[(_eeprom_emulator_device.page_map[page] * EMULATOR_PAGE_SIZE) + 4],
+			EEPROM_DATA_SIZE);
+
+}
+
+enum status_code eeprom_erase_memory()
+{
+	//Create new EEPROM memory block in EEPROM emulation section
+	_eeprom_emulator_create_memory();
+
+	//Map the newly created EEPROM memory block
+	_eeprom_emulator_scan_memory();
+
+	//Write eeprom emulation master block
+	_eeprom_emulator_create_master_block();
+}
+
---
+++ sam0/services/eeprom/emulator/eeprom.h
@@ -0,0 +1,130 @@
+/**
+ * \file
+ *
+ * \brief SAM0+ Peripheral Digital to Analog Converter Driver
+ *
+ * Copyright (C) 2012 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+#include "asf.h"
+
+#ifndef _EEPROM_EMULATOR_H_
+#define _EEPROM_EMULATOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EEPROM_EMUALTOR_START_ADDRESS  ????
+
+/**
+ * \brief Initializes the eeprom emualtor service driver
+ *
+ * This function will check the EEPROM emualtor space, if there is already 
+ * initialized eeprom data in the EEPROM space it will
+ *
+ * \li Re-fresh all used row to initial state
+ * \li Re-map data
+ *
+ * If the EEPROM space is unused it will:
+ *
+ * \li Re-fresh all rows to make sure the row state is known
+ * \li Initialize the EEPROM space 
+ *
+ * \return This function return a status code
+ *
+ */
+enum status_code eeprom_emulator_init(void);
+
+enum status_code eeprom_emulator_enable();
+enum status_code eeprom_emulator_disable();
+
+/**
+ * \brief Writes a data buffer to the flash page buffer
+ *
+ * This function will write data to the flash page buffer, the data will be keept 
+ * there until another write is executed or the flush function is used.
+ *
+ * \param[in] page  The number of the page in eeprom emualtor space to be written
+ * \param[in] *data Pointer to the data buffer containing data to be written
+ *
+ * \return This function return a status code
+ *
+ */
+enum status_code eeprom_write_page(uint16_t page, uint8_t *data);
+
+/**
+ * \brief Reads a page sized data buffer from EEPROM emulator flash space
+ *
+ * This function will read a pagesized buffer from the EEPROM emualtor space and
+ * place it in the SRAM buffer. 
+ *
+ * \param[in] page The number of the page in eeprom emualtor space to be written
+ * \param[out] *data Data buffer of page size that will contain the data read from memory
+ */
+enum status_code eeprom_read_page(uint16_t page, uint8_t *data);
+
+/**
+ * \brief Flushes the page buffer to flash memory
+ *
+ * This function will flush the content of the page buffer to the correct location
+ * in EEPROM emualtor memory space
+ *
+ * \note This should be the first function executed in a BOD33 interupt routine
+ * \note If the nvm driver is used to write other location in flash memory this
+ *       this function has to be executed first
+ *
+ * \return This function may return some status code of unknown value
+ */
+enum status_code eeprom_flush_page_buffer();
+
+/**
+ * \brief This function will initalize the EEPROM memory bank i flash
+ *
+ * This function fill erase the whole EEPROM memory block in flash. This includes
+ * erase all rows, write EEPROM memory header, and create new EEPROM pages.
+ *
+ * \note This function will destroy any content in the alotted EEPROM memory block.
+ *
+ * \return This function may return some status code of unknown value
+ */
+enum status_code eeprom_erase_memory():
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
