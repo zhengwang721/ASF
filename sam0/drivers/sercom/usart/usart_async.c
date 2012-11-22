f5f78ab57d8c0f311a908ce787011072ff4fefb3
iwalberg <irun.walberg@atmel.com>
2012-11-22 10:16:39 +0100

    added license text

--- sam0/drivers/sercom/usart/usart_async.c
+++ sam0/drivers/sercom/usart/usart_async.c
@@ -1,42 +1,47 @@
+/**
+ * \file
+ *
+ * \brief SAM0+ SERCOM USART Asynchronous Driver
+ *
+ * Copyright (C) 2012 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
 #include <usart_async.h>
 
-/**
- * \internal 
- *
- *
- * param[]
- *
- * returns 
- * retval  
- */
-//TODO: move to sercom
-uint8_t _sercom_get_module_irq_index(struct usart_dev_inst *const dev_inst)
-{
 
-
-	switch((uint32_t)dev_inst->hw_dev){
-		//TODO: fix casting	
-		/*
-		case (uint32_t *)SERCOM0:
-			return 0;
-
-		case SERCOM1:
-			return 1;
-
-		case SERCOM2:
-			return 2;
-
-		case SERCOM3:
-			return 3;
-		*/
-
-		case 0: 
-			return 120;
-
-		default:
-			Assert(false);
-	}
-}
 
 /**
  * \internal 
--- sam0/drivers/sercom/usart/usart_async.h
+++ sam0/drivers/sercom/usart/usart_async.h
@@ -1,10 +1,50 @@
+/**
+ * \file
+ *
+ * \brief SAM0+ SERCOM USART Asynchronous Driver
+ *
+ * Copyright (C) 2012 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
 #ifndef USART_ASYNC_H_INCLUDED
 #define USART_ASYNC_H_INCLUDED
 
 
 #include <stdlib.h>
 #include <usart.h>
-
 
 
 uint8_t _sercom_get_module_irq_index(struct usart_dev_inst *const dev_inst);
@@ -15,7 +55,7 @@ uint8_t _sercom_get_module_irq_index(struct usart_dev_inst *const dev_inst);
  * {@
  */
 void usart_async_register_callback(struct usart_dev_inst *const dev_inst,
-		const usart_async_callback_t *const callback_func,
+		usart_async_callback_t *callback_func,
 		enum usart_callback_type callback_type);
 
 void usart_async_unregister_callback(struct usart_dev_inst *dev_inst,
