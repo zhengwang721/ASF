7d86407fa039dd48d39758c5ea8e96bd3e7b3159
Christian Naess <cnaess@atmel.com>
2012-11-23 16:07:24 +0100

    Fixed review comments

--- sam0/drivers/adc/adc.c
+++ sam0/drivers/adc/adc.c
@@ -1,25 +1,10 @@
 #include "adc.h"
 #include <status_codes.h>
 
-status_code_t adc_init(struct adc_dev_inst *const dev_inst, ADC_t *hw_dev,
-		struct adc_config *config)
+
+status_code_t _adc_set_config (ADC_t *const hw_dev,
+		struct adc_conf *const config)
 {
-	uint8_t adjres;
-	enum adc_average_samples average;
-
-	if (hw_dev->CTRLA & ADC_SWRST_bm) {
-		/* We are in the middle of a reset. Abort. */
-		return STATUS_ERR_BUSY;
-	}
-
-	_adc_wait_for_sync(hw_dev);
-	if (hw_dev->CTRLA & ADC_ENABLE_bm) {
-		/* Module must be disabled before initialization. Abort. */
-		return STATUS_ERR_DENIED;
-	}
-
-	/* Set up GCLK_ADC using sysclock driver*/
-
 	/* Configure CTRLA */
 	hw_dev->CTRLA = (config->sleep_enable << ADC_SLEEPEN_bp);
 
@@ -31,26 +16,27 @@ status_code_t adc_init(struct adc_dev_inst *const dev_inst, ADC_t *hw_dev,
 	/* Configure AVGCTRL */
 	switch (config->oversampling_and_decimation) {
 
-	ADC_OVERSAMPLING_AND_DECIMATION_DISABLE:
+	case ADC_OVERSAMPLING_AND_DECIMATION_DISABLE:
 		adjres = 0;
 		average = config->average_samples;
 		break;
-	ADC_OVERSAMPLING_AND_DECIMATION_1BIT:
+
+	case ADC_OVERSAMPLING_AND_DECIMATION_1BIT:
 		adjres = 0x01;
 		average = ADC_AVERAGE_SAMPLES_4;
 		break;
 
-	ADC_OVERSAMPLING_AND_DECIMATION_2BIT:
+	case ADC_OVERSAMPLING_AND_DECIMATION_2BIT:
 		adjres = 0x02;
 		average = ADC_AVERAGE_SAMPLES_16;
 		break;
 
-	ADC_OVERSAMPLING_AND_DECIMATION_3BIT:
+	case ADC_OVERSAMPLING_AND_DECIMATION_3BIT:
 		adjres = 0x01;
 		average = ADC_AVERAGE_SAMPLES_64;
 		break;
 
-	ADC_OVERSAMPLING_AND_DECIMATION_4BIT:
+	case ADC_OVERSAMPLING_AND_DECIMATION_4BIT:
 		adjres = 0x00;
 		average = ADC_AVERAGE_SAMPLES_256;
 		break;
@@ -77,18 +63,20 @@ status_code_t adc_init(struct adc_dev_inst *const dev_inst, ADC_t *hw_dev,
 	_adc_wait_for_sync(hw_dev);
 	hw_dev->WINCTRL = config->window_mode        << ADC_WINMODE_bp;
 	_adc_wait_for_sync(hw_dev);
+	/* this will NOT work for negative values, please fix! */
 	hw_dev->WINLT   = config->window_lower_value << ADC_WINLT_bp;
 	_adc_wait_for_sync(hw_dev);
+	/* this will NOT work for negative values, please fix! */
 	hw_dev->WINUT   = config->window_upper_value << ADC_WINUT_bp;
 
 	/* Configure INPUTCTRL */
 	_adc_wait_for_sync(hw_dev);
 	hw_dev->INPUTCTRL =
-			(config->gain_factor    << ADC_GAIN_bp)   |
-			// INPUTOFFSET goes here
-			// INPUTSCAN goes here
-			(config->negative_input << ADC_MUXNEG_bp) |
-			(config->positive_input << ADC_MUXPOS_bp);
+			(config->gain_factor       << ADC_GAIN_bp)        |
+			(config->offset_start_scan << ADC_INPUTOFFSET_bp) |
+			(config->inputs_to_scan    << ADC_INPUTSCAN_bp)   |
+			(config->negative_input    << ADC_MUXNEG_bp)      |
+			(config->positive_input    << ADC_MUXPOS_bp);
 
 	/* Configure EVCTRL */
 	hw_dev->EVCTRL =
@@ -106,4 +94,34 @@ status_code_t adc_init(struct adc_dev_inst *const dev_inst, ADC_t *hw_dev,
 	hw_dev->GAINCORR   = config->gain_correction   << ADC_GAINCORR_bp;
 	hw_dev->OFFSETCORR = config->offset_correction << ADC_OFFSETCORR_bp;
 
+	return STATUS_OK;
+}
+
+
+status_code_t adc_init(struct adc_dev_inst *const dev_inst, ADC_t *hw_dev,
+		struct adc_config *config)
+{
+	uint8_t adjres;
+	enum adc_average_samples average;
+
+	dev_inst->hw_dev = hw_dev;
+
+	if (hw_dev->CTRLA & ADC_SWRST_bm) {
+		/* We are in the middle of a reset. Abort. */
+		return STATUS_ERR_BUSY;
+	}
+
+	_adc_wait_for_sync(hw_dev);
+	if (hw_dev->CTRLA & ADC_ENABLE_bm) {
+		/* Module must be disabled before initialization. Abort. */
+		return STATUS_ERR_DENIED;
+	}
+
+	/* TODO: Set up GCLK_ADC using sysclock driver*/
+
+
+	/* Write configuration to module */
+	_adc_set_config(hw_dev, config);
+
+	return STATUS_OK;
 }
