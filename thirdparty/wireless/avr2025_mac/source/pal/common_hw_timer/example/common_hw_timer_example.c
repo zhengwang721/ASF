db31303f5294119845f2077c967a87d64780018d
Vinoth Kumar <vinoth.kumar@atmel.com>
2012-12-31 12:48:37 +0530

    Changes related to timer directory structure and bug fix for - WSD-870 and WSD-867

---
+++ thirdparty/wireless/avr2025_mac/source/pal/common_hw_timer/example/common_hw_timer_example.c
@@ -0,0 +1,123 @@
+/**
+ * \file
+ *
+ * \brief AVR XMEGA TC Driver Example 1
+ *
+ * Copyright (c) 2010 - 2011 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+/**
+ * \mainpage
+ *
+ * \section intro Introduction
+ * This simple example shows how to use the \ref tc_group to toggle LEDs.
+ *
+ * \section files Main files:
+ *  - tc.c Timer XMEGA Timer Counter driver implementation
+ *  - tc.h Timer XMEGA Timer Counter driver definitions
+ *  - tc_example1.c example application
+ *  - conf_example.h: configuration of the example
+ *
+ * \section driverinfo TC Driver
+ * The XMEGA TC driver can be found \ref tc_group "here".
+ *
+ * \section deviceinfo Device Info
+ * All AVR XMEGA devices with an tc can be used.
+ *
+ * \section exampledescription Description of the example
+ * The example will configure one TC module (\ref TIMER_EXAMPLE) in normal mode and
+ * use three TC interrupts (overflow, Capture/Compare Channels A and B).
+ *
+ * The TC is setup to use a 31250Hz resolution clock and a 31250 period value
+ * resulting in a 1Hz TC overflow frequency.
+ * Compare Channels A and B will be setup at 1/4 and 1/2 of the TC period.
+
+ * Each callback interrupts functions are setup to toggle a LED.
+ *  - LED0: Toggles on TC overflow interrupt
+ *  - LED1: Toggle on Compare Channel A
+ * In the case of A1 Xplain:
+ *  - LED2: Toggle on Compare Channel B interrupts
+ * In the case of A3BU Xplained:
+ *  - Status LED: Toggles red on Compare Channel B interrupts
+ *
+ * \section compinfo Compilation Info
+ * This software was written for the GNU GCC and IAR for AVR.
+ * Other compilers may or may not work.
+ *
+ * \section contactinfo Contact Information
+ * For further information, visit
+ * <A href="http://www.atmel.com/">Atmel</A>.\n
+ */
+#include <conf_example.h>
+#include <asf.h>
+#include "common_hw_timer.h"
+
+uint16_t time_out = 5000;
+
+void overflow_cb(void)
+{
+	ioport_toggle_pin(J2_PIN1);
+}
+
+void expiry_cb(void)
+{
+	ioport_toggle_pin(J2_PIN0);
+	common_tc_delay(time_out);
+}
+
+int main(void)
+{
+	pmic_init();
+	board_init();
+	sysclk_init();
+	sleepmgr_init();
+
+	cpu_irq_enable();
+
+	set_common_tc_overflow_callback(overflow_cb);
+	set_common_tc_expiry_callback(expiry_cb);
+
+	common_tc_init();
+
+	ioport_configure_pin(J2_PIN0, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
+	ioport_configure_pin(J2_PIN1, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
+
+	common_tc_delay(time_out);
+
+	while (1);
+}
---
+++ thirdparty/wireless/avr2025_mac/source/pal/common_sw_timer/example/common_sw_timer_example.c
@@ -0,0 +1,220 @@
+/**
+ * \file
+ *
+ * \brief Timer/Counter example 3.
+ *
+ * Copyright (c) 2011 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+/**
+ * \mainpage
+ * \section intro Introduction
+ * This is the documentation for the data structures, functions, variables,
+ * defines, enums, and typedefs for the TC driver.\n It also gives an example
+ * of the usage of the TC module.
+ *
+ * This example will start a timer/counter and generates "tick" interrupts.
+ *
+ * The selected timer input clock is the internal clock labelled TC3
+ * referred to as TIMER_CLOCK3 in the datasheet. TIMER_CLOCK3 is connected to
+ * fPBA / 8 (see datasheet).
+ *
+ * The 16-bit timer/counter channel will cycle from 0x0000 to RC. RC is
+ * initialized to (fPBA / 8) / 100, so that an interrupt will be triggered
+ * every 1 ms. Upon interrupt, a LED0 is toggled thus producing a square
+ * signal of frequency 500 Hz. Check the toggled GPIO pin connected to LED0 on
+ *  an oscilloscope and see the square signal at frequency 500 Hz.
+ *
+ * Also, a message is displayed on USART and the 'Timer:' value is updated
+ * every second.
+ * The displayed message is:\n
+ * <code> ATMEL \n AVR32 UC3 - TC example \n Timer: 1 s </code>
+ *
+ * \section files Main Files
+ * - tc.c: TC driver
+ * - tc.h: TC driver header file
+ * - tc_example3.c: TC example 3
+ * - conf_example.h: TC Configuration
+ * - conf_clock.h: Clock configuration for example
+ * - conf_board.h: Simulator configuration
+ *
+ * \section compinfo Compilation Info
+ * This software was written for GCC for AVR32 and IAR Embedded Workbench
+ * for AVR32. Other compilers may or may not work.
+ *
+ * \section Configuration Information
+ * This example has been tested with the following configuration:
+ * - EVK1100, EVK1101, EVK1104, EVK1105, UC3L_EK, UC3C_EK, UC3-A3 XPLAINED,
+ *   UC3-L0 XPLAINED, STK600 + RCUC3D routing board boards.
+ * - CPU clock: 16MHz in UC3C_EK and 12 MHz in all other boards.
+ * - PC terminal settings:
+ *   - 57600 bps,
+ *   - 8 data bits,
+ *   - no parity bit,
+ *   - 1 stop bit,
+ *   - no flow control.
+ *
+ * \section contactinfo Contact Information
+ * For further information, visit
+ * <A href="http://www.atmel.com/uc3">Atmel AVR UC3</A>.\n
+*/
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "sysclk.h"
+#include "board.h"
+#include "print_funcs.h"
+#include "tc.h"
+#include "gpio.h"
+#if defined (__GNUC__)
+#  include "intc.h"
+#endif
+#include "common_sw_timer.h"
+
+
+//! \name Example configuration
+//!@{
+/**
+ * \def CONFIG_SYSCLK_SOURCE
+ * \brief Clock source to use
+ */
+/**
+ * \def EXAMPLE_TC
+ * \brief Base address of TC module to use.
+ */
+/**
+ * \def EXAMPLE_TC_CHANNEL
+ * \brief Channel to use with TC module.
+ */
+/**
+ * \def EXAMPLE_TC_IRQ
+ * \brief IRQ Line number for TC module
+ */
+ /**
+ * \def EXAMPLE_TC_IRQ_PRIORITY
+ * \brief IRQ Group number for TC module
+ */
+//!@}
+
+uint32_t time_out1 = 10000;
+uint32_t time_out2 = 20000;
+uint32_t time_out3 = 30000;
+uint32_t time_out4 = 40000;
+
+static void timeout1_cb(void *parameter);
+static void timeout2_cb(void *parameter);
+static void timeout3_cb(void *parameter);
+static void timeout4_cb(void *parameter);
+
+
+/*! \brief Main function:
+ *  - Configure the CPU to run at 12MHz
+ *  - Configure the USART
+ *  - Register the TC interrupt (GCC only)
+ *  - Configure, enable the CPCS (RC compare match) interrupt, and start a
+ *    TC channel in waveform mode
+ *  - In an infinite loop, update the USART message every second.
+ */
+int main(void)
+{
+	/**
+	 * \note the call to sysclk_init() will disable all non-vital
+	 * peripheral clocks, except for the peripheral clocks explicitly
+	 * enabled in conf_clock.h.
+	 */
+	sysclk_init();
+	// Enable the clock to the selected example Timer/counter peripheral module.
+//	sysclk_enable_peripheral_clock(EXAMPLE_TC);
+	// Initialize the USART module for trace messages
+//	init_dbg_rs232(sysclk_get_pba_hz());
+	// Disable the interrupts
+	cpu_irq_disable();
+
+#if defined (__GNUC__)
+	// Initialize interrupt vectors.
+	INTC_init_interrupts();
+	// Register the RTC interrupt handler to the interrupt controller.
+	INTC_register_interrupt(&tc_irq, EXAMPLE_TC_IRQ, EXAMPLE_TC_IRQ_PRIORITY);
+#endif
+
+	sw_timer_init();
+	// Enable the interrupts
+	cpu_irq_enable();
+
+	gpio_configure_pin(AVR32_PIN_PB00,GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_configure_pin(AVR32_PIN_PB01,GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_configure_pin(AVR32_PIN_PB02,GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	gpio_configure_pin(AVR32_PIN_PB03,GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+
+	while(STATUS_OK != sw_timer_start(0, time_out1, TIMEOUT_RELATIVE, (FUNC_PTR)timeout1_cb, NULL));
+	while(STATUS_OK != sw_timer_start(1, time_out2, TIMEOUT_RELATIVE, (FUNC_PTR)timeout2_cb, NULL));
+	while(STATUS_OK != sw_timer_start(2, time_out3, TIMEOUT_RELATIVE, (FUNC_PTR)timeout3_cb, NULL));
+	while(STATUS_OK != sw_timer_start(3, time_out4, TIMEOUT_RELATIVE, (FUNC_PTR)timeout4_cb, NULL));
+
+	while (1) {
+		sw_timer_service();
+	}
+}
+
+static void timeout1_cb(void *parameter)
+{
+	gpio_toggle_pin(AVR32_PIN_PB00);
+
+	while(STATUS_OK != sw_timer_start(0, time_out1, TIMEOUT_RELATIVE, (FUNC_PTR)timeout1_cb, NULL));
+}
+
+static void timeout2_cb(void *parameter)
+{
+	gpio_toggle_pin(AVR32_PIN_PB01);
+	//sw_timer_stop(0);
+	while(STATUS_OK != sw_timer_start(1, time_out2, TIMEOUT_RELATIVE, (FUNC_PTR)timeout2_cb, NULL));
+}
+
+static void timeout3_cb(void *parameter)
+{
+	gpio_toggle_pin(AVR32_PIN_PB02);
+
+	while(STATUS_OK != sw_timer_start(2, time_out3, TIMEOUT_RELATIVE, (FUNC_PTR)timeout3_cb, NULL));
+}
+
+static void timeout4_cb(void *parameter)
+{
+	gpio_toggle_pin(AVR32_PIN_PB03);
+
+	while(STATUS_OK != sw_timer_start(3, time_out4, TIMEOUT_RELATIVE, (FUNC_PTR)timeout4_cb, NULL));
+}
