422d09b371c3225914ad05425c51b4e98a60a612
Andreas Loehre <andreas.loehre@atmel.com>
2013-01-19 14:54:59 +0100

    add debug functions using the OLEDwing TODO: move to a debug utils for reuse

---
+++ sam0/drivers/system/quick_start/debug.h
@@ -0,0 +1,24 @@
+#include <asf.h>
+
+
+
+#define LED1 30
+#define LED2 31
+#define LED3 32 + 13
+#define LED4 40
+
+#define BUTTON1 32 + 16
+#define BUTTON2 6
+#define BUTTON3 7
+#define BUTTON4 41
+
+/* EXT1 pin 3 - 6 */
+#define IOPIN1 4
+#define IOPIN2 5
+#define IOPIN3 32 + 4
+#define IOPIN4 32 + 5
+
+
+void init_debug_pins(void);
+
+void debug_set_val(uint8_t val);
