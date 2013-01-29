c1ebb5ca7b712c83cbbced6767d86f4cd77639b4
Vinoth Kumar <vinoth.kumar@atmel.com>
2013-01-29 17:57:01 +0530

    IOPORT api changed for ioport_get_pin_level(GPIO_PUSH_BUTTON_0) and removed comments included during debug.

--- thirdparty/wireless/avr2025_mac/examples/mac/no_beacon/coord/main.c
+++ thirdparty/wireless/avr2025_mac/examples/mac/no_beacon/coord/main.c
@@ -277,10 +277,10 @@ int main(void)
 	{
 		wpan_task();
 #ifdef GPIO_PUSH_BUTTON_0
-		if (ioport_pin_is_low(GPIO_PUSH_BUTTON_0))
+		if (!ioport_get_pin_level(GPIO_PUSH_BUTTON_0))
 		{
 			delay_ms(DEBOUNCE_DELAY_MS);
-			if (ioport_pin_is_low(GPIO_PUSH_BUTTON_0))
+			if (!ioport_get_pin_level(GPIO_PUSH_BUTTON_0))
 			{
 				dst_addr.Addr.short_address = BROADCAST;
 				wpan_mcps_data_req(FCF_SHORT_ADDR, &dst_addr, 14, 
