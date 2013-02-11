2ed5da26a9d97e2671d058d85f389706c82deca0
Selvamegala.S <selvamegala.s@atmel.com>
2013-02-11 10:38:11 +0530

    Main page tag included in application files

--- thirdparty/wireless/avr2102_rf4control/apps/zrc/terminal_tgt/main.c
+++ thirdparty/wireless/avr2102_rf4control/apps/zrc/terminal_tgt/main.c
@@ -42,10 +42,55 @@
  * @author    Atmel Corporation: http://www.atmel.com
  * @author    Support email: avr@atmel.com
  */
-/*
- * Copyright (c) 2013, Atmel Corporation All rights reserved.
+/**
+* \mainpage
+* \section preface Preface
+* This is the reference manual for ZRC Terminal target application.
+* \section toc Table of Contents
+*  - \subpage overview
+*  -  \b Application \b Interface(API)\n
+*    - \ref group_rf4control
+*    - \ref group_mac
+*    - \ref group_pal
+*    - \ref group_tal
+*    - \ref group_resources
+*  - \subpage main_files
+*  - \subpage devsup
+*  - \subpage compinfo
+*  - \subpage references
+*  - \subpage contactinfo
+*/
+
+/**
+ * \page overview Overview
+ * \section intro Introduction
+ * To be added.
+ */
+
+
+/** \page main_files Application Files
+ * - main.c\n                      Application main file.
+ * - vendor_data.c\n               Vendor Specific API functions.
+
+ * \page devsup Device Support
+ * - \b ATXMEGA256A3BU
+ *                     - <A href="http://www.atmel.com/tools/xmega-a3buxplained.aspx"> \b   XMEGA-A3BU Xplained </A>  <A href="http://store.atmel.com/PartDetail.aspx?q=p:10500293">\a Buy </A>\n
+
+ * - \b UC3A3256S
+*                      - <A href="http://www.atmel.com/tools/rz600.aspx"> \b RZ600</A> <A href="http://store.atmel.com/PartDetail.aspx?q=p:10500245;c:100118">\a Buy </A>\n
+ * \page compinfo Compilation Info
+ * This software was written for the GNU GCC and IAR for AVR.
+ * Other compilers may or may not work.
  *
- * Licensed under Atmel's Limited License Agreement --> EULA.txt
+ * \page references References
+ * 1)  <A href="http://www.atmel.com/images/doc8412.pdf">AVR2025 IEEE 802.15.4 MAC Reference Manual\n </A>.
+ * 2)  IEEE Std 802.15.4-2006 Part 15.4: Wireless Medium Access Control (MAC)
+ *     and Physical Layer (PHY) Specifications for Low-Rate Wireless Personal Area
+ *     Networks (WPANs).\n\n
+ * 3)  AVR Wireless Support <A href="http://avr@atmel.com">avr@atmel.com</A>.\n
+ * \page contactinfo Contact Information
+ * For further information,visit
+ * <A href="http://www.atmel.com/avr">www.atmel.com</A>.\n
  */
 
 /* === INCLUDES ============================================================ */
