284baf804e5db6945e391610db0ef89444cbf711
iwalberg <irun.walberg@atmel.com>
2013-02-15 12:52:04 +0100

    added comment

--- sam0/drivers/sercom/i2c/quick_start_slave/qs_i2c_slave_basic_use.c
+++ sam0/drivers/sercom/i2c/quick_start_slave/qs_i2c_slave_basic_use.c
@@ -133,7 +133,8 @@ static void configure_callbacks(void)
 
 	//![reg_en_i2c_callback]
 
-	/* Enable global interrupts for SERCOM instance. */
+	/* TODO: Remove, should be in enable/disable.
+	 * Enable global interrupts for SERCOM instance. */
 	//![enable_global_i2c_interrupts]
 	system_interrupt_enable(SYSTEM_INTERRUPT_MODULE_SERCOM2);
 	//![enable_global_i2c_interrupts]
