4fba3c8eed5507bd6a1486c5eda46d2764742a58
Thibault RICHARD <thibault.richard@atmel.com>
2013-02-25 15:06:32 +0100

    Fixed warning.

--- common/components/memory/eeprom/at30tse75x/at30tse75x.c
+++ common/components/memory/eeprom/at30tse75x/at30tse75x.c
@@ -126,6 +126,7 @@ uint8_t at30tse_write_register(uint8_t reg, uint8_t reg_type, uint8_t reg_size, 
 
 	// cppcheck-suppress unreadVariable
 	data[0] = 0x00FF & (reg_value >> 8);
+	// cppcheck-suppress unreadVariable
 	data[1] = 0x00FF & reg_value;
 
 	return twi_master_write(TWI0, &packet);
