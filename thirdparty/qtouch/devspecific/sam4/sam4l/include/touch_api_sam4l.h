e60d1b5794455958e4bcab88cbfcff286535688a
Nash Zhang <nash.zhang@atmel.com>
2013-03-04 17:46:38 +0800

    [ASFP-3293]Fix issue for asf-branch-board-atmel-linux#3749 (hide uc3l0 qtouch lib) Add files: common\applications\user_application\sam4lc4c_sam4l_ek\touch_config_sam4l.h thirdparty\qtouch\devspecific\sam4\sam4l\module_config\touch_config_sam4l.h
    Modified files:
    common\applications\user_application\sam4lc4c_sam4l_ek\asf.xml
    thirdparty\qtouch\devspecific\sam4\sam4l\include\touch_api_sam4l.h
    thirdparty\qtouch\devspecific\uc3\uc3l0\asf.xml

--- thirdparty/qtouch/devspecific/sam4/sam4l/include/touch_api_sam4l.h
+++ thirdparty/qtouch/devspecific/sam4/sam4l/include/touch_api_sam4l.h
@@ -9,7 +9,7 @@
  * - Userguide:          QTouch Library User Guide - doc8207.pdf.
  * - Support email:      touch@atmel.com
  *
- * Copyright (c) 2012 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2012-2013 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
