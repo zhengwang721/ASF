4c3854d5dfff81490918999d6ffeba2daa36afdb
Hu Sheng <sheng.hu@atmel.com>
2013-03-22 13:09:56 +0800

    [ASFP-3431] Update afec.c file.

--- sam/drivers/afec/afec.c
+++ sam/drivers/afec/afec.c
@@ -164,11 +164,22 @@ void afec_ch_set_config(Afec *const afec, const enum afec_channel_num channel,
 		struct afec_ch_config *config)
 {
 	afec_ch_sanity_check(afec, channel);
+	uint32_t reg = 0;
 
-	afec->AFEC_CDOR = (config->offset) ? (0x1u << channel) : 0;
-	afec->AFEC_DIFFR = (config->diff) ? (0x1u << channel) : 0;
-	afec->AFEC_CGR = (0x03u << (2 * channel))
-			& ((config->gain) << (2 * channel));
+	reg = afec->AFEC_CDOR;
+	reg &= ~(0x1u << channel);
+	reg |= (config->offset) ? (0x1u << channel) : 0;
+	afec->AFEC_CDOR = reg;
+
+	reg = afec->AFEC_DIFFR;
+	reg &= ~(0x1u << channel);
+	reg |= (config->diff) ? (0x1u << channel) : 0;
+	afec->AFEC_DIFFR = reg;
+
+	reg = afec->AFEC_CGR;
+	reg &= ~(0x03u << (2 * channel));
+	reg |= (config->gain) << (2 * channel);
+	afec->AFEC_CGR = reg;
 }
 
 /**
@@ -250,7 +261,7 @@ void afec_ch_get_config_defaults(struct afec_ch_config *const cfg)
 
 	cfg->diff = false;
 	cfg->offset = false;
-	cfg->gain = AFEC_GAINVALUE_0;
+	cfg->gain = AFEC_GAINVALUE_1;
 }
 
 /**
