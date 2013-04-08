4bf2dc168991cd2df3d7cb332761285ff2ff5624
Sriram R <sriram.r@atmel.com>
2013-04-08 13:49:07 +0530

    Added additional info for description of various wireless modules for ASF Wizard

--- thirdparty/wireless/avr2102_rf4control/addons/api_parser/api_parser.h
+++ thirdparty/wireless/avr2102_rf4control/addons/api_parser/api_parser.h
@@ -65,7 +65,8 @@
   
  /**
   * \defgroup group_api_parser_rf4ce RF4Control API Parser Module
-  * API Parser for RF4Control acts as a interface for converting Nwk and Zrc Api's to appropriate command byte stream
+  * This module is responsible for encoding and decoding the serial bytes to/from NCP into NWK or ZRC APIs to provide seamless working for application similar to stack running in same processor or on 
+  * NCP.
   * @{
   */
 
