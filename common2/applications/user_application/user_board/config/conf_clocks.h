d151a717495a2f03eb86ed3ee45a4000ecab42c2
Dean Camera <dean_patrick.camera@atmel.com>
2013-04-17 15:24:32 +0200

    Update conf_clocks.h comments to be uniform.

--- common2/applications/user_application/user_board/config/conf_clocks.h
+++ common2/applications/user_application/user_board/config/conf_clocks.h
@@ -42,8 +42,8 @@
  */
 #include <clock.h>
 
-#ifndef CONF_CLOCKS_H
-#  define CONF_CLOCKS_H
+#ifndef CONF_CLOCKS_H_INCLUDED
+#  define CONF_CLOCKS_H_INCLUDED
 
 /* System clock bus configuration */
 #  define CONF_CLOCK_CPU_CLOCK_FAILURE_DETECT     true
@@ -53,17 +53,17 @@
 #  define CONF_CLOCK_APBB_DIVIDER                 SYSTEM_MAIN_CLOCK_DIV_1
 
 
-/* SYSTEM_CLOCK_SOURCE_OSC8M configuration */
+/* SYSTEM_CLOCK_SOURCE_OSC8M configuration - Internal 8MHz oscillator */
 #  define CONF_CLOCK_OSC8M_PRESCALER              SYSTEM_OSC8M_DIV_0
 
-/* SYSTEM_CLOCK_SOURCE_XOSC External oscillator/clock configuration */
+/* SYSTEM_CLOCK_SOURCE_XOSC configuration - External clock/oscillator */
 #  define CONF_CLOCK_XOSC_ENABLE                  false
 #  define CONF_CLOCK_XOSC_EXTERNAL_CRYSTAL        SYSTEM_CLOCK_EXTERNAL_CRYSTAL
 #  define CONF_CLOCK_XOSC_EXTERNAL_FREQUENCY      12000000UL
 #  define CONF_CLOCK_XOSC_STARTUP_TIME            SYSTEM_XOSC_STARTUP_32768
 #  define CONF_CLOCK_XOSC_AUTO_GAIN_CONTROL       true
 
-/* SYSTEM_CLOCK_SOURCE_XOSC32K Configuration - External 32KHz crystal/clock oscillator */
+/* SYSTEM_CLOCK_SOURCE_XOSC32K configuration - External 32KHz crystal/clock oscillator */
 #  define CONF_CLOCK_XOSC32K_ENABLE               true
 #  define CONF_CLOCK_XOSC32K_EXTERNAL_CRYSTAL     SYSTEM_CLOCK_EXTERNAL_CRYSTAL
 #  define CONF_CLOCK_XOSC32K_STARTUP_TIME         SYSTEM_XOSC32K_STARTUP_65536
@@ -71,13 +71,13 @@
 #  define CONF_CLOCK_XOSC32K_ENABLE_1KHZ_OUPUT    false
 #  define CONF_CLOCK_XOSC32K_ENABLE_32KHZ_OUTPUT  true
 
-/* SYSTEM_CLOCK_SOURCE_OSC32K Configuration - Internal 32KHz Oscillator */
+/* SYSTEM_CLOCK_SOURCE_OSC32K configuration - Internal 32KHz oscillator */
 #  define CONF_CLOCK_OSC32K_ENABLE                true
 #  define CONF_CLOCK_OSC32K_STARTUP_TIME          SYSTEM_OSC32K_STARTUP_128
 #  define CONF_CLOCK_OSC32K_ENABLE_1KHZ_OUTPUT    true
 #  define CONF_CLOCK_OSC32K_ENABLE_32KHZ_OUTPUT   true
 
-/* SYSTEM_CLOCK_SOURCE_DFLL Configuration - Digital Frequency locked loop */
+/* SYSTEM_CLOCK_SOURCE_DFLL configuration - Digital Frequency Locked Loop */
 #  define CONF_CLOCK_DFLL_ENABLE                  true
 #  define CONF_CLOCK_DFLL_LOOP_MODE               SYSTEM_CLOCK_DFLL_OPEN_LOOP
 
@@ -86,7 +86,7 @@
 #  define CONF_CLOCK_DFLL_COARSE_VALUE            (0x1f / 4)
 #  define CONF_CLOCK_DFLL_FINE_VALUE              (0xff / 4)
 
-/* DFLL Closed loop mode configuration */
+/* DFLL closed loop mode configuration */
 #  define CONF_CLOCK_DFLL_SOURCE_GCLK_GENERATOR   GCLK_GENERATOR_6
 #  define CONF_CLOCK_DFLL_MULTIPLY_FACTOR         6
 #  define CONF_CLOCK_DFLL_QUICK_LOCK              true
@@ -98,7 +98,7 @@
 
 
 /* Set this to true to configure the GCLK when running clocks_init. If set to
- * false none of the GCLK generators will be configured in clocks_init(). */
+ * false, none of the GCLK generators will be configured in clocks_init(). */
 #  define CONF_CLOCK_CONFIGURE_GCLK               true
 
 /* Configure GCLK generator 0 (Main Clock) */
@@ -141,5 +141,5 @@
 #  define CONF_CLOCK_GCLK_7_CLOCK_SOURCE          SYSTEM_CLOCK_SOURCE_OSC8M
 #  define CONF_CLOCK_GCLK_7_PRESCALER             1
 
-#endif /* CONF_CLOCKS_H */
+#endif /* CONF_CLOCKS_H_INCLUDED */
 
