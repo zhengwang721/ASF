37ba77ec5f235921b6ae253b957e11102bd9ee87
Dean Camera <dean_patrick.camera@atmel.com>
2013-05-15 10:05:07 +0200

    Update ADC basic use case quick start guide to the latest template.

--- sam0/drivers/adc/quick_start/qs_adc_basic.h
+++ sam0/drivers/adc/quick_start/qs_adc_basic.h
@@ -70,32 +70,46 @@
  * There are no special setup requirements for this use-case.
  *
  * \subsection asfdoc_samd20_adc_basic_use_case_code Code
- * Add to user application %main():
- * \snippet qs_adc_basic.c main_setup
+ * Add to the main application source file, outside of any functions:
+ * \snippet qs_adc_basic.c module_inst
+ *
+ * Copy-paste the following setup code to your user application:
+ * \snippet qs_adc_basic.c setup
+ *
+ * Add to user application initialization (typically the start of \c main()):
+ * \snippet qs_adc_basic.c setup_init
  *
  * \section asfdoc_samd20_adc_basic_use_case_workflow Workflow
- * -# Create variables for device software instance, config and channel config.
- *  \snippet qs_adc_basic.c variable
- * -# Create variable for storing the ADC result.
- *  \snippet qs_adc_basic.c res_variable
- * -# Get ADC config defaults.
- *  \snippet qs_adc_basic.c get_conf
- * -# Initialize software instance with hardware module and write configuration
- *    to the module.
- *  \snippet qs_adc_basic.c init_adc
- * -# Enable the ADC.
- *  \snippet qs_adc_basic.c enable
+ * -# Create a module software instance structure for the ADC module to store
+ *    the ADC driver state while it is in use.
+ *    \note This should never go out of scope as long as the module is in use.
+ *          In most cases, this should be global.
+ *
+ *    \snippet qs_adc_basic.c module_inst
+ * -# Configure the ADC module.
+ *  -# Create a ADC module configuration struct, which can be filled out to
+ *     adjust the configuration of a physical ADC peripheral.
+ *     \snippet qs_adc_basic.c setup_config
+ *  -# Initialize the ADC configuration struct with the module's default values.
+ *     \note This should always be performed before using the configuration
+ *           struct to ensure that all values are initialized to known default
+ *           settings.
+ *
+ *     \snippet qs_adc_basic.c setup_config_defaults
+ *  -# Enable the ADC module so that conversions can be made.
+ *     \snippet qs_adc_basic.c setup_enable
  *
  * \section asfdoc_samd20_adc_basic_use_case_use Use Case
+ *
  * \subsection asfdoc_samd20_adc_basic_use_case_use_code Code
  * Copy-paste the following code to your user application:
- * \snippet qs_adc_basic.c main_use_case
+ * \snippet qs_adc_basic.c main
  *
  * \subsection asfdoc_samd20_adc_basic_use_case_use_workflow Workflow
  *  -# Start conversion.
  *  \snippet qs_adc_basic.c start_conv
  *  -# Wait until conversion is done and read result.
  *  \snippet qs_adc_basic.c get_res
- *  -# Start infinite loop.
+ *  -# Enter an infinite loop once the conversion is complete.
  *  \snippet qs_adc_basic.c inf_loop
  */
