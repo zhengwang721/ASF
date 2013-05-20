0d56c13f6ba9293847ee7c6bbfbcf442d02d124a
Hu Sheng <sheng.hu@atmel.com>
2013-05-20 11:56:11 +0800

    [ANPI-1925] Modify PLLA source to internal 8M RC.

--- sam/drivers/pmc/pmc_clock_switching_example/sam4n16c_sam4n_xplained_pro/conf_clock.h
+++ sam/drivers/pmc/pmc_clock_switching_example/sam4n16c_sam4n_xplained_pro/conf_clock.h
@@ -67,16 +67,16 @@
 
 // ===== PLL0 (A) Options   (Fpll = (Fclk * PLL_mul) / PLL_div)
 // Use mul and div effective values here.
-#define CONFIG_PLL0_SOURCE          PLL_SRC_MAINCK_XTAL
-#define CONFIG_PLL0_MUL             50
-#define CONFIG_PLL0_DIV             3
+#define CONFIG_PLL0_SOURCE          PLL_SRC_MAINCK_8M_RC
+#define CONFIG_PLL0_MUL             25
+#define CONFIG_PLL0_DIV             1
 
 // ===== Target frequency (System clock)
 // - XTAL frequency: 12MHz
 // - System clock source: PLLA
 // - System clock prescaler: 2 (divided by 2)
 // - PLLA source: XTAL
-// - PLLA output: XTAL * 50 / 3
-// - System clock: XTAL * 50 / 3 / 2 = 100MHz
+// - PLLA output: XTAL * 25 / 1
+// - System clock: XTAL * 25 / 1 / 2 = 100MHz
 
 #endif /* CONF_CLOCK_H_INCLUDED */
