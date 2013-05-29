8083644d5b4255dfe0172be82580ff2c4e5782c4
Dean Camera <dean_patrick.camera@atmel.com>
2013-05-29 10:52:41 +0200

    Fix copyright build job failures.

--- sam0/drivers/extint/unit_test/samd20_xplained_pro/conf_board.h
+++ sam0/drivers/extint/unit_test/samd20_xplained_pro/conf_board.h
@@ -3,7 +3,7 @@
  *
  * \brief SAMD20 Xplained PRO board configuration.
  *
- * Copyright (c) 2012 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2013 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
