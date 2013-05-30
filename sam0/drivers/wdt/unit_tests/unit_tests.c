a5b36dac7eb1fa826fa6a327ca82707bceea2fc4
Martin Etnestad Johansen <mejohansen@atmel.com>
2013-05-30 15:13:54 +0200

    Update wdt unit test for EDBG CDC

--- sam0/drivers/wdt/unit_tests/unit_tests.c
+++ sam0/drivers/wdt/unit_tests/unit_tests.c
@@ -170,8 +170,10 @@ static void configure_stdio_serial(void)
 	/* Configure USART for unit test output */
 	usart_get_config_defaults(&usart_conf);
 	usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
-	usart_conf.pinmux_pad3 = CONF_STDIO_RX_PINMUX;
-	usart_conf.pinmux_pad2 = CONF_STDIO_TX_PINMUX;
+	usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
 	usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
 
 	stdio_serial_init(&unit_test_output, CONF_STDIO_USART, &usart_conf);
