070d8ed26bca3bb37c27183eb83582e4198e6a42
Thibault RICHARD <thibault.richard@atmel.com>
2013-06-25 16:39:35 +0200

    Initial import. Base demo is working with LwIP 141 but transfer is slow.

---
+++ thirdparty/lwip/http_netconn_example/main.c
@@ -0,0 +1,265 @@
+/**
+ * \file
+ *
+ * \brief FreeRTOS Web/DSP Demo.
+ *
+ * Copyright (c) 2013 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+/**
+ * \mainpage web_dsp_demo FreeRTOS Web/DSP Demo
+ *
+ * \section Purpose
+ *
+ * This demo demonstrates how to use the DACC, TC, GMAC, SPI, SMC in a real-time
+ * context, using the FreeRTOS kernel. The demo also features the LwIP stack on
+ * top of the GMAC driver.
+ *
+ * \section Requirements
+ *
+ * This example can be used on the following boards:<br>
+ * - SAM4E-EK<br>
+ *
+ * \section Description
+ *
+ * The demo relies on several FreeRTOS task to work:
+ * - DSP task : based on the selected source mode, the DSP task will process data
+ *   from a wave file (loopback mode) or from a generated sinus wave (its frequency
+ *   can be changed using the potentiometer). The input signal is then modulated
+ *   based on the QTouch slider last known position.
+ *   The DSP task then performs a Fast Fourrier Transform to compute the magnitude
+ *   of the signal.
+ * - GFX task : draws the IP configuration menu, then the input signal source and
+ *   magnitude on screen.
+ * - HTTP task : handles the LwIP stack and HTTPD callbacks to answer remote
+ *   client request.
+ *   The device IP can either be configured statically (192.168.0.100) or
+ *   dynamically using DHCP.
+ * - QTouch task : used to compute the slider's position.
+ *
+ * Once the IP address is configured from the touch screen interface, the board
+ * ressources can be accessed from any web browser providing the IP address. For
+ * instance, when the IP address is configured statically :
+ * <a href="http://192.168.0.100/">
+ *
+ * The web page gives the following information about the board status:
+ * - CPU core type.
+ * - Chip name.
+ * - SRAM size.
+ * - Flash size.
+ * - Audio out connector.
+ * - Boot from memory.
+ * - Board IP.
+ * - Your IP.
+ * - Total number of HTTP status requests.
+ * - FreeRTOS uptime.
+ * - Temperature sensor value.
+ * - Led status.
+ * - Potentiometer value.
+ * - Graph of the FFT input signal magnitude (range from 0 to 10 khz).
+ *
+ * The web page is periodically refreshed to keep the board status up-to-date.
+ * With Chrome browser (V25), sometimes two http webpage requests are needed to
+ * get correct status periodical update on browser. This is a known issue.
+ * And please refer to the datasheet for the temperature sensor accuracy.
+ *
+ * For IAR Embedded Workbench IDE project, the source supports both FPU on
+ * and FPU off cases. FPU on or FPU off can be configured in
+ * IAR project "General Options" by selecting "VFPv4" or "None" respectively.
+ * With FPU on, hardware floating-point instruction is used, which can improve
+ * the performance of floating-point arithmetic in the DSP task.
+ *
+ */
+
+#include "task.h"
+#include "ethernet.h"
+#include "status_codes.h"
+#include "conf_uart_serial.h"
+#include "uart_serial.h"
+#include "stdio_serial.h"
+
+/** The priorities at which various tasks will get created. */
+#define mainGFX_TASK_PRIORITY          (tskIDLE_PRIORITY + 1)
+
+/** The stack sizes allocated to the DSP stack: (1048 * 4) = 4096 bytes. */
+#define mainGFX_TASK_STACK_SIZE        (1048)
+
+/** The priorities at which various tasks will get created. */
+#define mainHTTP_TASK_PRIORITY         (tskIDLE_PRIORITY + 2)
+
+/** The stack sizes allocated to the DSP stack: (1048 * 4) = 4096 bytes. */
+#define mainHTTP_TASK_STACK_SIZE       (1048)
+
+/*
+ * FreeRTOS hook (or callback) functions that are defined in this file.
+ */
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(xTaskHandle pxTask,
+		signed char *pcTaskName);
+void vApplicationTickHook(void);
+
+
+
+static void prvSetupHardware(void)
+{
+	/* ASF function to setup clocking. */
+	sysclk_init();
+
+	/* Ensure all priority bits are assigned as preemption priority bits. */
+	NVIC_SetPriorityGrouping(0);
+
+	/* Atmel library function to setup for the evaluation kit being used. */
+	board_init();
+}
+
+/**
+ * Configure UART console.
+ */
+static void configure_console(void)
+{
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+		.paritytype = CONF_UART_PARITY
+	};
+
+	/** Configure console UART. */
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	stdio_serial_init(CONF_UART, &uart_serial_options);
+}
+
+int main(void)
+{
+	/* Prepare the hardware to run this demo. */
+	prvSetupHardware();
+#if SAM4E
+	NVIC_DisableIRQ(ARM_IRQn);
+#endif
+	/** Initilize the SAM system */
+	sysclk_init();
+	board_init();
+
+	/** Initialize the console uart */
+	configure_console();
+
+	/** Create GFX task. */
+	create_gfx_task(mainGFX_TASK_STACK_SIZE, mainGFX_TASK_PRIORITY);
+
+	/** Create WebServer task. */
+	create_http_task(mainHTTP_TASK_STACK_SIZE, mainHTTP_TASK_PRIORITY);
+
+	/** Start the RTOS scheduler. */
+	vTaskStartScheduler();
+
+	/* If all is well, the scheduler will now be running, and the following line
+	will never be reached.  If the following line does execute, then there was
+	insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+	to be created.  See the memory management section on the FreeRTOS web site
+	for more details. */
+	for (;;) {
+	}
+
+	return 0;
+}
+
+void vApplicationMallocFailedHook(void)
+{
+	/**
+	 * vApplicationMallocFailedHook() will only be called if
+	 * configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.
+	 * It is a hook function that will get called if a call to
+	 * pvPortMalloc() fails. pvPortMalloc() is called internally by
+	 * the kernel whenever a task, queue, timer or semaphore is created.
+	 * It is also called by various parts of the demo application.
+	 * If heap_1.c or heap_2.c are used, then the size of the heap
+	 * available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	 * FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can
+	 * be used to query the size of free heap space that remains
+	 * (although it does not provide information on how the remaining heap
+	 * might be fragmented).
+	 */
+	taskDISABLE_INTERRUPTS();
+	for (;;) {
+	}
+}
+
+void vApplicationIdleHook(void)
+{
+	/**
+	 * vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK
+	 * is set to 1 in FreeRTOSConfig.h.  It will be called on each iteration
+	 * of the idle task.  It is essential that code added to this hook
+	 * function never attempts to block in any way (for example, call
+	 * xQueueReceive() with a block time specified, or call vTaskDelay()).
+	 * If the application makes use of the vTaskDelete() API function
+	 * (as this demo application does) then it is also important that
+	 * vApplicationIdleHook() is permitted to return to its calling
+	 * function, because it is the responsibility of the idle task to
+	 * clean up memory allocated by the kernel to any task that has
+	 * since been deleted.
+	 */
+}
+
+void vApplicationStackOverflowHook(xTaskHandle pxTask,
+		signed char *pcTaskName)
+{
+	(void) pcTaskName;
+	(void) pxTask;
+
+	/**
+	 * Run time stack overflow checking is performed if
+	 * configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.
+	 * This hook function is called if a stack overflow is
+	 * detected.
+	 */
+	taskDISABLE_INTERRUPTS();
+	for (;;) {
+	}
+}
+
+void vApplicationTickHook(void)
+{
+	/**
+	 * This function will be called by each tick interrupt if
+	 * configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.
+	 * User code can be added here, but the tick hook is called from
+	 * an interrupt context, so code must not attempt to block,
+	 * and only the interrupt safe FreeRTOS API
+	 * functions can be used (those that end in FromISR()).
+	 */
+}
---
+++ thirdparty/lwip/http_netconn_example/task_gfx.c
@@ -0,0 +1,410 @@
+/**
+ * \file
+ *
+ * \brief GFX task from the FreeRTOS Web/DSP Demo.
+ *
+ * Copyright (c) 2011-2012 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+#include "task.h"
+#include "arm_math.h"
+#include "logo_atmel.h"
+
+#include <FreeRTOS.h>
+#include <FreeRTOS_CLI.h>
+#include <StackMacros.h>
+#include <croutine.h>
+#include <list.h>
+#include <mpu_wrappers.h>
+#include <portable.h>
+#include <projdefs.h>
+#include <queue.h>
+#include <semphr.h>
+#include <task.h>
+#include <timers.h>
+
+#include <pmc.h>
+#include <sysclk.h>
+
+#include "ili9325.h"
+#include "aat31xx.h"
+#include "gpio.h"
+#include "pio_handler.h"
+#include "smc.h"
+#include "ethernet.h"
+#include "rtouch.h"
+
+static void gfx_task(void *pvParameters);
+static void gfx_init(void);
+static void gfx_draw_bmpfile(const uint8_t *bmpImage);
+
+/* Chip select number to be set */
+#define ILI9325_LCD_CS       1
+
+/** IRQ priority for PIO (The lower the value, the greater the priority) */
+#define IRQ_PRIOR_PIO        13
+
+/** Refresh rate in frames per second. */
+#define GFX_REFRESH_RATE     24
+
+const portTickType instructions_delay = 50UL / portTICK_RATE_MS;
+static const portTickType presentation_delay = 2000UL / portTICK_RATE_MS;
+static const portTickType touch_delay = 10UL / portTICK_RATE_MS;
+static const portTickType gfx_refresh_rate = 1000UL / GFX_REFRESH_RATE / portTICK_RATE_MS;
+
+/* Hold the instructions screen and prevent other task from running. */
+uint32_t app_hold = 1;
+
+uint32_t g_ip_mode = 0; // 0 unselected, 1 static, 2 dhcp.
+
+int8_t g_c_ipconfig[25];
+
+/* BMP (Windows) Header Format. */
+COMPILER_PACK_SET(1)
+struct bmpfile_header {
+	uint16_t type;		/* signature, must be 4D42 hex */
+	uint32_t file_size; /* size of BMP file in bytes (unreliable) */
+	uint16_t reserved1; /* reserved, must be zero */
+	uint16_t reserved2; /* reserved, must be zero */
+	uint32_t offset;	/* offset to start of image data in bytes */
+	uint32_t header_size; /* size of BITMAPINFOHEADER structure, must be 40 */
+	uint32_t width;		/* image width in pixels */
+	uint32_t height;	/* image height in pixels */
+	uint16_t planes;	/* number of planes in the image, must be 1 */
+	uint16_t bits;		/* number of bits per pixel (1, 4, 8, 16, 24, 32) */
+	uint32_t compression; /* compression type (0=none, 1=RLE-8, 2=RLE-4) */
+	uint32_t inage_size; /* size of image data in bytes (including padding) */
+	uint32_t h_resolution;	/* horizontal resolution in pixels per meter */
+							/* (unreliable) */
+	uint32_t v_resolution;	/* vertical resolution in pixels per meter */
+							/* (unreliable) */
+	uint32_t colours;		/* number of colors in image, or zero */
+	uint32_t important_colors;	/* number of important colors, or zero */
+};
+COMPILER_PACK_RESET()
+
+/**
+ * \brief Callback called when the pen is pressed on the touchscreen.
+ *
+ * \param x horizontal position (in pixel if ts calibrated).
+ * \param y vertical position (in pixel if ts calibrated).
+ */
+static void event_handler(rtouch_event_t const *event)
+{
+	switch(event->type) {
+	case RTOUCH_MOVE:
+	case RTOUCH_PRESS:
+	case RTOUCH_RELEASE:
+		if (event->panel.y < 160)
+		{
+			/* Static IP button selected. */
+			/* Button 1. */
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_rectangle(20,70,220,130);
+			ili9325_set_foreground_color(0xE4E4E4u);
+			ili9325_draw_rectangle(22,72,222,132);
+			ili9325_set_foreground_color(COLOR_LIGHTGREY);
+			ili9325_draw_filled_rectangle(21,71,221,131);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_string(92, 91, (uint8_t *)"Static");
+
+			/* Button 2. */
+			ili9325_set_foreground_color(0xE4E4E4u);
+			ili9325_draw_rectangle(20,170,220,230);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_rectangle(22,172,222,232);
+			ili9325_set_foreground_color(COLOR_LIGHTGREY);
+			ili9325_draw_filled_rectangle(21,171,221,231);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_string(102, 191, (uint8_t *)"DHCP");
+		}
+		else
+		{
+			/* DHCP IP button selected. */
+			/* Button 1. */
+			ili9325_set_foreground_color(0xE4E4E4u);
+			ili9325_draw_rectangle(20,70,220,130);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_rectangle(22,72,222,132);
+			ili9325_set_foreground_color(COLOR_LIGHTGREY);
+			ili9325_draw_filled_rectangle(21,71,221,131);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_string(92, 91, (uint8_t *)"Static");
+
+			/* Button 2. */
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_rectangle(20,170,220,230);
+			ili9325_set_foreground_color(0xE4E4E4u);
+			ili9325_draw_rectangle(22,172,222,232);
+			ili9325_set_foreground_color(COLOR_LIGHTGREY);
+			ili9325_draw_filled_rectangle(21,171,221,231);
+			ili9325_set_foreground_color(COLOR_BLACK);
+			ili9325_draw_string(102, 191, (uint8_t *)"DHCP");
+		}
+		break;
+	default:
+		break;
+	}
+
+	/* If released, freeze buttons and perform IP settings. */
+	if (event->type == RTOUCH_RELEASE)
+	{
+		ili9325_set_foreground_color(COLOR_GRAY);
+		ili9325_draw_string(92, 91, (uint8_t *)"Static");
+		ili9325_draw_string(102, 191, (uint8_t *)"DHCP");
+		ili9325_set_foreground_color(COLOR_BLACK);
+		if (event->panel.y < 160)
+			g_ip_mode = 1;
+		else
+			g_ip_mode = 2;
+	}
+}
+
+/**
+ * \brief Create the GFX task.
+ *
+ * \param stack_depth_words Task stack size in 32 bits word.
+ * \param task_priority Task priority.
+ */
+void create_gfx_task(uint16_t stack_depth_words,
+		unsigned portBASE_TYPE task_priority)
+{
+	/* Initialize display. */
+	gfx_init();
+
+	/* Create the task as described above. */
+	xTaskCreate(gfx_task, (const signed char *const) "GFX",
+			stack_depth_words, NULL, task_priority,
+			NULL);
+}
+
+/**
+ * \brief GFX task core function.
+ *
+ * \param pvParameters Junk parameter.
+ */
+static void gfx_task(void *pvParameters)
+{
+	/* Get rid of this compiler warning. */
+	pvParameters = pvParameters;
+
+	/* Display ATMEL logo. */
+	ili9325_set_cursor_position(0,0);
+	gfx_draw_bmpfile(logo_atmel_bmp);
+	vTaskDelay(presentation_delay);
+
+	/* Initialize touchscreen without calibration */
+	pmc_enable_periph_clk(ID_PIOA);
+	pio_handler_set_priority(PIOA, PIOA_IRQn, IRQ_PRIOR_PIO);
+	pmc_enable_periph_clk(ID_SPI);
+	rtouch_init(ILI9325_LCD_WIDTH, ILI9325_LCD_HEIGHT);
+	rtouch_enable();
+	rtouch_set_event_handler(event_handler);
+	/*
+	 * Set touch screen calibration parameters which are got
+	 * from manual calibration when developing.
+	 */
+	rtouch_set_calibration_parameter(7432, -5637, 318, 1845);
+
+	/* Draw IP config menu. */
+	ili9325_set_foreground_color(COLOR_WHITE);
+	ili9325_draw_filled_rectangle(0, 0, ILI9325_LCD_WIDTH, ILI9325_LCD_HEIGHT);
+
+	/* Button 1. */
+	ili9325_set_foreground_color(0xE4E4E4u);
+	ili9325_draw_rectangle(20,70,220,130);
+	ili9325_set_foreground_color(COLOR_BLACK);
+	ili9325_draw_rectangle(22,72,222,132);
+	ili9325_set_foreground_color(COLOR_LIGHTGREY);
+	ili9325_draw_filled_rectangle(21,71,221,131);
+	ili9325_set_foreground_color(COLOR_BLACK);
+	ili9325_draw_string(92, 91, (uint8_t *)"Static");
+
+	/* Button 2. */
+	ili9325_set_foreground_color(0xE4E4E4u);
+	ili9325_draw_rectangle(20,170,220,230);
+	ili9325_set_foreground_color(COLOR_BLACK);
+	ili9325_draw_rectangle(22,172,222,232);
+	ili9325_set_foreground_color(COLOR_LIGHTGREY);
+	ili9325_draw_filled_rectangle(21,171,221,231);
+	ili9325_set_foreground_color(COLOR_BLACK);
+	ili9325_draw_string(102, 191, (uint8_t *)"DHCP");
+
+	ili9325_draw_string(22, 30, (uint8_t *)"IP Configuration");
+
+//	ili9325_draw_line(15, 110, 225, 110);
+
+	ili9325_draw_string(20, 260, (uint8_t *)"Assigned IP:");
+	ili9325_draw_rectangle(20,280,220,310);
+
+
+	while (g_ip_mode == 0) {
+		rtouch_process();
+		vTaskDelay(touch_delay);
+	}
+
+	/* Bring up the ethernet interface & initializes timer0, channel0 */
+	ili9325_draw_string(30, 290, (uint8_t *)"Please wait...");
+	init_ethernet();
+	app_hold = 0;
+
+	/* Wait for IP to be configured. Done in LwIP. */
+	while (g_ip_mode != 3) {
+		vTaskDelay(touch_delay);
+	}
+
+	/* Show configured IP and unlock all other waiting tasks. */
+	ili9325_set_foreground_color(COLOR_WHITE);
+	ili9325_draw_filled_rectangle(20,280,220,310);
+	ili9325_draw_filled_rectangle(0,0,240,60);
+	ili9325_set_foreground_color(COLOR_BLACK);
+	ili9325_draw_rectangle(20,280,220,310);
+	ili9325_draw_string(30, 290, g_c_ipconfig);
+	ili9325_draw_string(5, 30, (uint8_t *)"HTTP server running!");
+
+	//while (rtouch_is_pressed() == false) {
+	//	vTaskDelay(touch_delay);
+	//}
+////
+	///* Draw application context. */
+	//ili9325_set_foreground_color(COLOR_WHITE);
+	//ili9325_draw_filled_rectangle(0, 0, ILI9325_LCD_WIDTH, ILI9325_LCD_HEIGHT);
+	//ili9325_set_foreground_color(COLOR_BLACK);
+#//if SAM4S
+	//ili9325_draw_string(10, 30, (uint8_t *)"SAM4S Web/DSP Demo");
+#//elif SAM4E
+	//ili9325_draw_string(10, 30, (uint8_t *)"SAM4E Web/DSP Demo");
+#//else
+#// error No proper title found!
+#//endif
+	//ili9325_draw_rectangle(20,60,220,160);
+	//ili9325_draw_line(15, 110, 225, 110);
+	//ili9325_draw_rectangle(20,180,220,280);
+	//ili9325_draw_string(15, 295, (uint8_t *)"0Hz");
+	//ili9325_draw_string(165, 295, (uint8_t *)"10kHz");
+
+	/* GFX task Loop. */
+	while (1)
+	{
+
+
+		/* Display cycle count for STOPWATCH macro */
+		/*ili9325_set_foreground_color(COLOR_WHITE);
+		ili9325_draw_filled_rectangle(60, 285, 160, 315);
+		ili9325_set_foreground_color(COLOR_RED);
+		sprintf(cyclenum, "%dc", cnt);
+		ili9325_draw_string(70, 295, cyclenum);*/
+
+		/* Sleep to reach the expected refresh rate. */
+		/* Leave some CPU time for other tasks. */
+		vTaskDelay(gfx_refresh_rate);
+	}
+}
+
+/**
+ * \brief Initialize the LCD.
+ */
+static void gfx_init(void)
+{
+	struct ili9325_opt_t g_ili9325_display_opt;
+
+	/* Enable peripheral clock */
+	pmc_enable_periph_clk(ID_SMC);
+
+	/* Configure SMC interface for Lcd */
+	smc_set_setup_timing(SMC,ILI9325_LCD_CS,SMC_SETUP_NWE_SETUP(2)
+	| SMC_SETUP_NCS_WR_SETUP(2)
+	| SMC_SETUP_NRD_SETUP(2)
+	| SMC_SETUP_NCS_RD_SETUP(2));
+	smc_set_pulse_timing(SMC, ILI9325_LCD_CS , SMC_PULSE_NWE_PULSE(4)
+	| SMC_PULSE_NCS_WR_PULSE(4)
+	| SMC_PULSE_NRD_PULSE(10)
+	| SMC_PULSE_NCS_RD_PULSE(10));
+	smc_set_cycle_timing(SMC, ILI9325_LCD_CS, SMC_CYCLE_NWE_CYCLE(10)
+	| SMC_CYCLE_NRD_CYCLE(22));
+	smc_set_mode(SMC, ILI9325_LCD_CS, SMC_MODE_READ_MODE
+	| SMC_MODE_WRITE_MODE);
+
+	/* Initialize display parameter */
+	g_ili9325_display_opt.ul_width = ILI9325_LCD_WIDTH;
+	g_ili9325_display_opt.ul_height = ILI9325_LCD_HEIGHT;
+	g_ili9325_display_opt.foreground_color = COLOR_WHITE;
+	g_ili9325_display_opt.background_color = COLOR_WHITE;
+
+	/* Initialize LCD */
+	if (1 == ili9325_init(&g_ili9325_display_opt)) {
+		while(1);
+	}
+
+	/* Set backlight level */
+	aat31xx_set_backlight(AAT31XX_MAX_BACKLIGHT_LEVEL);
+
+	/* Turn on LCD */
+	ili9325_display_on();
+}
+
+/**
+ * \brief Draw a bitmap image.
+ * Expected file format is 320x240, 24 bits, RGB888.
+ *
+ * \param bmpImage Pointer to the bmp file image.
+ */
+static void gfx_draw_bmpfile(const uint8_t *bmpImage)
+{
+	struct bmpfile_header *bmp_header;
+	uint32_t length;
+	uint32_t i = 0;
+	uint32_t offset;
+
+	bmp_header = (struct bmpfile_header*) bmpImage;
+	length = bmp_header->height * bmp_header->width * 3;
+	offset = sizeof(struct bmpfile_header);
+
+	ili9325_set_cursor_position(0,0);
+
+	/* Prepare to write in GRAM */
+	LCD_IR(0);
+	LCD_IR(ILI9325_GRAM_DATA_REG);
+
+	for (i = offset; i < length; i += 3) {
+	  	/* Invert red and blue. */
+		LCD_WD(bmpImage[i+2]);
+		LCD_WD(bmpImage[i+1]);
+		LCD_WD(bmpImage[i]);
+	}
+}
