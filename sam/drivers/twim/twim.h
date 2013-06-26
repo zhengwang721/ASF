1e211ec2a5d453378c3925097aeaf350228a8d56
henry feng <hao.feng@atmel.com>
2013-06-26 11:36:13 +0800

    [ASFP-3023] Added TWIM PDCA example

--- sam/drivers/twim/twim.h
+++ sam/drivers/twim/twim.h
@@ -212,6 +212,8 @@ uint32_t twim_get_status(Twim *twim);
 void twim_clear_status(Twim *twim, uint32_t clear_status);
 void twim_set_callback(Twim *twim, uint32_t interrupt_source,
 	twim_callback_t callback, uint8_t irq_level);
+void twim_pdca_transfer_prepare(Twim *twim, twi_package_t *package,
+		bool read);
 
 /**
  * \}
