e0fc59c925e3b6f3b8ffa53ed43af2a89c2118c2
Even Larsen <even.larsen@atmel.com>
2013-08-14 07:43:50 +0200

    Cleaned some code belonging to another branch

--- common/components/xplained_pro_wings/oled1/oled1.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OLED1_WING_H_INCLUDED_
-#define _OLED1_WING_H_INCLUDED_
-
-#include "stdint.h"
-#include "xplained_pro_wings.h"
-
-void wing_oled1_init(struct wing_object *wing, uint8_t position);
-
-#endif
