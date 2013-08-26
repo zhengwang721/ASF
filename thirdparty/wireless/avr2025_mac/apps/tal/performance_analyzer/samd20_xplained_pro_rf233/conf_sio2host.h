099bc6532a5b57d59a50279de60781cf1df209a5
Sriram.r <sriram.r@atmel.com>
2013-08-26 17:53:17 +0530

    Changed baud rate to 9600

--- thirdparty/wireless/avr2025_mac/apps/tal/performance_analyzer/samd20_xplained_pro_rf233/conf_sio2host.h
+++ thirdparty/wireless/avr2025_mac/apps/tal/performance_analyzer/samd20_xplained_pro_rf233/conf_sio2host.h
@@ -46,7 +46,7 @@
 #define USART_HOST                 EDBG_CDC_MODULE
 
 /** Baudrate setting */
-#define USART_HOST_BAUDRATE        115200
+#define USART_HOST_BAUDRATE        9600
  
 
 #define USART_HOST_RX_ISR_ENABLE()  _sercom_set_handler(3, USART_HOST_ISR_VECT);\
