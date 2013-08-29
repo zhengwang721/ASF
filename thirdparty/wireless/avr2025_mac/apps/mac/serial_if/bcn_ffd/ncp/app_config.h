83414a212a0898576c6a6338506ac71337810f2b
mathivanan elangovan <mathivanan.e@atmel.com>
2013-08-29 11:22:53 +0530

    Adding Serial Interface Application for MAC Security.

--- thirdparty/wireless/avr2025_mac/apps/mac/serial_if/bcn_ffd/ncp/app_config.h
+++ thirdparty/wireless/avr2025_mac/apps/mac/serial_if/bcn_ffd/ncp/app_config.h
@@ -92,6 +92,43 @@
 /* Offset of IEEE address storage location within EEPROM */
 #define EE_IEEE_ADDR                (0)
 
+#ifdef MAC_SECURITY_ZIP
+/**
+ * The maximum number of entries supported in the macKeyTable.
+ * This value is mplementation specific.
+ */
+#define MAC_ZIP_MAX_KEY_TABLE_ENTRIES           (3)
+
+/**
+ * The maximum number of entries supported in the macDeviceTable.
+ * This value is mplementation specific.
+ */
+#if (MAC_START_REQUEST_CONFIRM == 1)    /* FFD like device */
+#define MAC_ZIP_MAX_DEV_TABLE_ENTRIES           (10)
+#else
+#define MAC_ZIP_MAX_DEV_TABLE_ENTRIES           (3)
+#endif  /* (MAC_START_REQUEST_CONFIRM == 1) */
+/**
+ * The maximum number of entries supported in the macSecurityLevelTable.
+ * This value is mplementation specific.
+ */
+#define MAC_ZIP_MAX_SEC_LVL_TABLE_ENTRIES       (2)
+
+/**
+ * The maximum number of entries supported in the KeyIdLookupList
+ */
+#define MAC_ZIP_MAX_KEY_ID_LOOKUP_LIST_ENTRIES  (1)
+
+/**
+ * The maximum number of entries supported in the KeyDeviceList
+ */
+#define MAC_ZIP_MAX_KEY_DEV_LIST_ENTRIES        (3)
+
+/**
+ * The maximum number of entries supported in the KeyUsageList
+ */
+#define MAC_ZIP_MAX_KEY_USAGE_LIST_ENTRIES      (1)
+#endif
 /* === Types ================================================================ */
 
 /* === Externals ============================================================ */
