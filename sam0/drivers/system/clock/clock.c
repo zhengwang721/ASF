f130d8c52aed29b9e1e6e0b02a174b95027f6768
Martin Etnestad Johansen <mejohansen@atmel.com>
2013-10-14 13:21:29 +0200

    Delay DFLL enabling in closed loop mode until GCLK set up

--- sam0/drivers/system/clock/clock.c
+++ sam0/drivers/system/clock/clock.c
@@ -767,7 +767,10 @@ void system_clock_init(void)
 	dfll_conf.fine_max_step   = CONF_CLOCK_DFLL_MAX_FINE_STEP_SIZE;
 
 	system_clock_source_dfll_set_config(&dfll_conf);
-	system_clock_source_enable(SYSTEM_CLOCK_SOURCE_DFLL);
+
+	if (CONF_CLOCK_DFLL_LOOP_MODE == SYSTEM_CLOCK_DFLL_LOOP_MODE_OPEN) {
+		system_clock_source_enable(SYSTEM_CLOCK_SOURCE_DFLL);
+	}
 #endif
 
 
@@ -791,7 +794,7 @@ void system_clock_init(void)
 	 * is configured later after all other clock systems are set up */
 	MREPEAT(GCLK_GEN_NUM_MSB, _CONF_CLOCK_GCLK_CONFIG_NONMAIN, ~);
 
-#  if (CONF_CLOCK_DFLL_ENABLE)
+#  if CONF_CLOCK_DFLL_ENABLE == true
 	/* Enable DFLL reference clock if in closed loop mode */
 	if (CONF_CLOCK_DFLL_LOOP_MODE == SYSTEM_CLOCK_DFLL_LOOP_MODE_CLOSED) {
 		struct system_gclk_chan_config dfll_gclk_chan_conf;
@@ -802,7 +805,19 @@ void system_clock_init(void)
 		system_gclk_chan_enable(SYSCTRL_GCLK_ID_DFLL48);
 	}
 #  endif
+#endif
 
+
+	/* DFLL (Closed Loop) */
+#if CONF_CLOCK_DFLL_ENABLE == true
+	if (CONF_CLOCK_DFLL_LOOP_MODE == SYSTEM_CLOCK_DFLL_LOOP_MODE_CLOSED) {
+		system_clock_source_enable(SYSTEM_CLOCK_SOURCE_DFLL);
+	}
+#endif
+
+
+	/* GCLK 0 */
+#if CONF_CLOCK_CONFIGURE_GCLK == true
 	/* Configure the main GCLK last as it might depend on other generators */
 	_CONF_CLOCK_GCLK_CONFIG(0, ~);
 #endif
