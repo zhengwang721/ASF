61c6558c4c329eb7913bdd6554b1a05587107914
Christian Guemes <cristian.guemes@atmel.com>
2013-10-23 12:41:35 +0200

    [ASFP-4044] update copyright dates

--- sam/drivers/pmc/sleep.c
+++ sam/drivers/pmc/sleep.c
@@ -3,7 +3,7 @@
  *
  * \brief Sleep mode access
  *
- * Copyright (c) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2012 - 2013 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
