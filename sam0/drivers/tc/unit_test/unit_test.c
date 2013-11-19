d8f4f126c35e5c5a45bfafd862f6b5973c611962
Roeder Cao <yue.cao@atmel.com>
2013-11-19 16:08:53 +0800

    update unit test

--- sam0/drivers/tc/unit_test/unit_test.c
+++ sam0/drivers/tc/unit_test/unit_test.c
@@ -75,7 +75,7 @@
  *
  * \section appdoc_sam0_tc_unit_test_setup Setup
  * The following connections has to be made using wires:
- *  - \b EXTINT 0 (PA16) <-----> TC0 WO1 (PB31)
+ *  - \b EXTINT 0 (PA16, EXT2 pin 17) <-----> TC0 WO1 (PA05, EXT1 pin 15)
  *
  * To run the test:
  *  - Connect the SAM D2x Xplained Pro board to the computer using a
@@ -410,8 +410,8 @@ static void run_16bit_capture_and_compare_test(const struct test_case *test)
 
 	/* Configure external interrupt controller */
 	struct extint_chan_conf extint_chan_config;
-	extint_chan_config.gpio_pin            = CONF_EIC_PIN;
-	extint_chan_config.gpio_pin_mux        = CONF_EIC_MUX;
+	extint_chan_config.gpio_pin            = PIN_PA16A_EIC_EXTINT0;
+	extint_chan_config.gpio_pin_mux        = MUX_PA16A_EIC_EXTINT0;
 	extint_chan_config.gpio_pin_pull       = EXTINT_PULL_UP;
 	extint_chan_config.wake_if_sleeping    = false;
 	extint_chan_config.filter_input_signal = false;
@@ -425,20 +425,22 @@ static void run_16bit_capture_and_compare_test(const struct test_case *test)
 	extint_enable_events(&extint_event_conf);
 
 	/* Configure event system */
-	struct events_resource event_res;
+	events_init();
+	/* Configure user */
+	struct events_user_config event_user_conf;
+	events_user_get_config_defaults(&event_user_conf);
+	event_user_conf.event_channel_id = EVENT_CHANNEL_0;
+
+	events_user_set_config(CONF_EVENT_USED_ID, &event_user_conf);
 	
 	/* Configure channel */
-	struct events_config config;
-	events_get_config_defaults(&config);
-	
-	config.generator      = CONF_EVENT_GENERATOR_ID;
-	config.edge_detect    = EVENTS_EDGE_DETECT_NONE;
-	config.path           = EVENTS_PATH_ASYNCHRONOUS;
-	events_allocate(&event_res, &config);
+	struct events_chan_config events_chan_conf;
+	events_chan_get_config_defaults(&events_chan_conf);
+	events_chan_conf.generator_id   = EVSYS_ID_GEN_EIC_EXTINT_0;
+	events_chan_conf.path           = EVENT_PATH_ASYNCHRONOUS;
+	events_chan_conf.edge_detection = EVENT_EDGE_NONE;
+	events_chan_set_config(EVENT_CHANNEL_0, &events_chan_conf);
 
-	/* Configure user */
-	events_attach_user(&event_res, CONF_EVENT_USED_ID);
-	
 	/* Enable TC modules */
 	tc_enable(&tc_test1_module);
 	tc_enable(&tc_test0_module);
