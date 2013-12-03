cebce4a3003e2d9005577f67276a7d5bb5e4a4ce
sam.sun <sam.sun@atmel.com>
2013-12-03 15:28:13 +0800

    [SAMG]Update clock configuration.

--- sam/applications/sam_low_power/samg53g19_samg53_xplained_pro/conf_clock.h
+++ sam/applications/sam_low_power/samg53g19_samg53_xplained_pro/conf_clock.h
@@ -44,39 +44,39 @@
 #ifndef CONF_CLOCK_H_INCLUDED
 #define CONF_CLOCK_H_INCLUDED
 
-// ===== System Clock (MCK) Source Options
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_RC
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_XTAL
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_BYPASS
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_8M_RC
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_16M_RC
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_24M_RC
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_XTAL
-//#define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_BYPASS
+/* ===== System Clock (MCK) Source Options */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_RC */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_XTAL */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_SLCK_BYPASS */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_8M_RC */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_16M_RC */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_24M_RC */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_XTAL */
+/* #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_MAINCK_BYPASS */
 #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_PLLACK
 
-// ===== System Clock (MCK) Prescaler Options   (Fmck = Fsys / (SYSCLK_PRES))
+/* ===== System Clock (MCK) Prescaler Options (Fmck = Fsys / (SYSCLK_PRES)) */
 #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_1
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_2
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_4
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_8
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_16
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_32
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_64
-//#define CONFIG_SYSCLK_PRES          SYSCLK_PRES_3
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_2 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_4 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_8 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_16 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_32 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_64 */
+/* #define CONFIG_SYSCLK_PRES          SYSCLK_PRES_3 */
 
-// ===== PLL0 (A) Options   (Fpll = (Fclk * PLL_mul) / PLL_div)
-// Use mul and div effective values here.
+/* ===== PLL0 (A) Options   (Fpll = (Fclk * PLL_mul) / PLL_div) */
+/* Use mul and div effective values here. */
 #define CONFIG_PLL0_SOURCE          PLL_SRC_SLCK_XTAL
 #define CONFIG_PLL0_MUL             1500
 #define CONFIG_PLL0_DIV             1
 
-// ===== Target frequency (System clock)
-// - Internal RC frequency: 32000Hz
-// - System clock source: SLCK RC
-// - System clock prescaler: 1 (divided by 1)
-// - PLLA source: SLCK_RC
-// - PLLA output: SLCK_RC * 1500 / 1
-// - System clock: SLCK_RC * 1500 / 1 / 1 = 48MHz
+/* ===== Target frequency (System clock) */
+/* - Internal RC frequency: 32000Hz */
+/* - System clock source: SLCK RC */
+/* - System clock prescaler: 1 (divided by 1) */
+/* - PLLA source: SLCK_RC */
+/* - PLLA output: SLCK_RC * 1500 / 1 */
+/* - System clock: SLCK_RC * 1500 / 1 / 1 = 48MHz */
 
 #endif /* CONF_CLOCK_H_INCLUDED */
