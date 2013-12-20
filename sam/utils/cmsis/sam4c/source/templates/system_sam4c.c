fe7d82f86ef6fd1a98d5520f7eccfd672701ba64
Tonny Lan <shouzhen.lan@atmel.com>
2013-12-20 11:09:58 +0800

    update startup_sam4c for iar

--- sam/utils/cmsis/sam4c/source/templates/system_sam4c.c
+++ sam/utils/cmsis/sam4c/source/templates/system_sam4c.c
@@ -103,17 +103,17 @@ void system_init_flash(uint32_t ul_clk)
 {
 	/* Set FWS for embedded Flash access according to operating frequency */
 	if (ul_clk < CHIP_FREQ_FWS_0) {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(0);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(0)|EEFC_FMR_CLOE;
 	} else if (ul_clk < CHIP_FREQ_FWS_1) {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(1);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(1)|EEFC_FMR_CLOE;
 	} else if (ul_clk < CHIP_FREQ_FWS_2) {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(2);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(2)|EEFC_FMR_CLOE;
 	} else if (ul_clk < CHIP_FREQ_FWS_3) {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(3);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(3)|EEFC_FMR_CLOE;
 	} else if (ul_clk < CHIP_FREQ_FWS_4) {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(4);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(4)|EEFC_FMR_CLOE;
 	} else {
-		EFC->EEFC_FMR = EEFC_FMR_FWS(5);
+		EFC->EEFC_FMR = EEFC_FMR_FWS(5)|EEFC_FMR_CLOE;
 	}
 }
 #else
