f82dc2f0ae81d635b148c92d0c6b2bcb73c4ef7e
Sriram.R <sriram.r@atmel.com>
2014-01-09 15:54:59 +0530

    copyright issue fixed

--- thirdparty/wireless/avr2025_mac/source/tal/atmegarfr2/inc/tal_irq_handler.h
+++ thirdparty/wireless/avr2025_mac/source/tal/atmegarfr2/inc/tal_irq_handler.h
@@ -3,7 +3,7 @@
  *
  * \brief This header file contains the interrupt handling definitions
  *
- * Copyright (c) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
