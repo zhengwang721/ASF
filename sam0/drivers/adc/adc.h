07da32a2422d3296ed6507e21e26ef090b423589
Nash Zhang <nash.zhang@atmel.com>
2014-01-09 17:51:34 +0800

    [ANPI-3031]Update AN for ADC module.

--- sam0/drivers/adc/adc.h
+++ sam0/drivers/adc/adc.h
@@ -1703,6 +1703,9 @@ static inline void adc_disable_interrupt(struct adc_module *const module_inst,
  *		<th>Changelog</th>
  *	</tr>
  *	<tr>
+ *		<td>Added support for SAMD21</td>
+ *	</tr>
+ *	<tr>
  *		<td>Added ADC calibration constant loading from the device signature
  *          row when the module is initialized.</td>
  *	</tr>
@@ -1733,6 +1736,11 @@ static inline void adc_disable_interrupt(struct adc_module *const module_inst,
  *		<th>Doc. Rev.</td>
  *		<th>Date</td>
  *		<th>Comments</td>
+ *	</tr>
+ *	<tr>
+ *		<td>C</td>
+ *		<td>01/2014</td>
+ *		<td>Added support for SAMD21.</td>
  *	</tr>
  *	<tr>
  *		<td>B</td>
