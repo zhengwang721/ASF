2a99aefc8c4cdc4a7f2464c96aa6b65154aa33a6
Even Larsen <even.larsen@atmel.com>
2014-01-12 13:25:41 +0100

    Added documentation for interrupt hook code

--- sam0/drivers/events/events.c
+++ sam0/drivers/events/events.c
@@ -3,7 +3,7 @@
  *
  * \brief SAM D2x Event System Controller Driver
  *
- * Copyright (C) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,7 +40,6 @@
  * \asf_license_stop
  *
  */
-
 #include <events.h>
 #include <system.h>
 #include <system_interrupt.h>
