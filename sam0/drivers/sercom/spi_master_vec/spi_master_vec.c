7a6ea77adaf44748734e8cadbb4850e8fa361128
Martin Etnestad Johansen <mejohansen@atmel.com>
2014-01-20 09:54:09 +0100

    Update copyright years to avoid stupid errors later

--- sam0/drivers/sercom/spi_master_vec/spi_master_vec.c
+++ sam0/drivers/sercom/spi_master_vec/spi_master_vec.c
@@ -3,7 +3,7 @@
  *
  * \brief SERCOM SPI master with vectored I/O driver implementation
  *
- * Copyright (C) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
