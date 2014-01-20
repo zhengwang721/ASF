1e99c29adde4d3948add2e971ee01dfb825a065b
Martin Etnestad Johansen <mejohansen@atmel.com>
2014-01-20 09:33:21 +0100

    Doc update, make more dox-mod-v2 compatible

--- sam0/drivers/sercom/spi_master_vec/spi_master_vec.c
+++ sam0/drivers/sercom/spi_master_vec/spi_master_vec.c
@@ -293,13 +293,13 @@ void spi_master_vec_reset(struct spi_master_vec_module *const module)
  *
  * To initiate a unidirectional transfer, pass \c NULL as the address of either
  * buffer descriptor array, like this:
- * \code
- *     // Transmit some buffers
- *     spi_master_vec_transceive_buffer_job(&module, tx_buffers, NULL);
- *
- *     // Receive some buffers
- *     spi_master_vec_transceive_buffer_job(&module, NULL, rx_buffers);
- * \endcode
+\code
+	// Transmit some buffers
+	spi_master_vec_transceive_buffer_job(&module, tx_buffers, NULL);
+
+	// Receive some buffers
+	spi_master_vec_transceive_buffer_job(&module, NULL, rx_buffers);
+\endcode
  *
  * \pre \ref spi_master_vec_init() and \ref spi_master_vec_enable() must have
  * been called before this function.
