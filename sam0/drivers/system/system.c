1361dd1d07fbc77af489b5e60ea6bd8e39a5c32b
Nash Zhang <nash.zhang@atmel.com>
2014-01-21 15:09:11 +0800

    [ANPI-3182]Update changelog and fix D2x for system and ADC module.

--- sam0/drivers/system/system.c
+++ sam0/drivers/system/system.c
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief SAM D2x System related functionality
+ * \brief SAM D20/D21 System related functionality
  *
  * Copyright (C) 2012-2014 Atmel Corporation. All rights reserved.
  *
