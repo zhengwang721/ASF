bff8ba6bfca05ba94726f2c62d43050ebe155dfa
Roeder Cao <yue.cao@atmel.com>
2014-01-21 16:27:28 +0800

    update sleepmgr.h

--- common/services/sleepmgr/samd/sleepmgr.h
+++ common/services/sleepmgr/samd/sleepmgr.h
@@ -52,7 +52,6 @@ extern "C" {
 #include <conf_sleepmgr.h>
 #include <interrupt.h>
 #include "system.h"
-#include <conf_clocks.h> 
 
 /**
  * \weakgroup sleepmgr_group
@@ -111,12 +110,6 @@ static inline void sleepmgr_sleep(const enum sleepmgr_mode sleep_mode)
 	system_set_sleepmode((enum system_sleepmode)(sleep_mode - 1));
 	system_sleep();
 	cpu_irq_enable();
-#if CONF_CLOCK_XOSC32K_ENABLE == true
-	while(!system_clock_source_is_ready(SYSTEM_CLOCK_SOURCE_XOSC32K));
-#endif
-#if CONF_CLOCK_DFLL_ENABLE == true
-	while(!system_clock_source_is_ready(SYSTEM_CLOCK_SOURCE_DFLL));
-#endif
 #else
 	UNUSED(sleep_mode);
 	cpu_irq_enable();
