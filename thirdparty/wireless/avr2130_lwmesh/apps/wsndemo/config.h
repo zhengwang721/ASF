95febb9e9566e5a9508a1e3b4ddd9d6e404268f6
Sriram.R <sriram.r@atmel.com>
2014-02-07 15:31:10 +0530

    Moved mega/drivers from 2025_mac to wireless/services

--- thirdparty/wireless/avr2130_lwmesh/apps/wsndemo/config.h
+++ thirdparty/wireless/avr2130_lwmesh/apps/wsndemo/config.h
@@ -51,8 +51,8 @@
 /*****************************************************************************
 *****************************************************************************/
 #define APP_ADDR                0x0000
-#define APP_PANID               0x1239
-#define APP_SENDING_INTERVAL    10000
+#define APP_PANID               0x1234
+#define APP_SENDING_INTERVAL    2000
 #define APP_ENDPOINT            1
 #define APP_SECURITY_KEY        "TestSecurityKey0"
 
