1080b703f995dcafe2e93910304dd1d3cbf64852
cq.xiao <cq.xiao@atmel.com>
2014-02-13 10:39:28 +0800

    Output to LED.

--- sam0/boards/samd21_xplained_pro/samd21_xplained_pro.h
+++ sam0/boards/samd21_xplained_pro/samd21_xplained_pro.h
@@ -110,6 +110,12 @@ void system_board_init(void);
 #define LED_0_PIN                 LED0_PIN
 #define LED_0_ACTIVE              LED0_ACTIVE
 #define LED_0_INACTIVE            LED0_INACTIVE
+
+#define LED_0_PWM_MODULE          TCC0
+#define LED_0_PWM_CHANNEL         0
+#define LED_0_PWM_PIN             PIN_PB30E_TCC0_WO0
+#define LED_0_PWM_MUX             MUX_PB30E_TCC0_WO0
+#define LED_0_PWM_PINMUX          PINMUX_PB30E_TCC0_WO0
 /** @} */
 
 /** Number of on-board LEDs */
