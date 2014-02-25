16e693373facdc4694ea9443dbb622ef6ace0b08
Even Larsen <even.larsen@atmel.com>
2014-02-25 11:08:33 +0100

    Minor updates after review comments

--- sam0/drivers/events/events_common.h
+++ sam0/drivers/events/events_common.h
@@ -1,9 +1,9 @@
 /*
  * \file
  *
- * \brief SAM D2x Event System Controller Driver
+ * \brief SAM D20/D21 Event System Controller Driver
  *
- * Copyright (C) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
