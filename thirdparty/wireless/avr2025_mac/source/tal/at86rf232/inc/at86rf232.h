fd219ce3edb56282bac5a19ada154acc67a35327
Sriram.R <sriram.r@atmel.com>
2014-02-26 13:27:08 +0530

    Fixed Jnkins errors for compilation and cpyright

--- thirdparty/wireless/avr2025_mac/source/tal/at86rf232/inc/at86rf232.h
+++ thirdparty/wireless/avr2025_mac/source/tal/at86rf232/inc/at86rf232.h
@@ -3,7 +3,7 @@
  *
  * @brief 
  *
- * Copyright (c) 2013 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2013-2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,7 +40,7 @@
  * \asf_license_stop
  */
 /*
- * Copyright (c) 2010, Atmel Corporation All rights reserved.
+ * Copyright (c) 2013-2014, Atmel Corporation All rights reserved.
  *
  * Licensed under Atmel's Limited License Agreement --> EULA.txt
  */
