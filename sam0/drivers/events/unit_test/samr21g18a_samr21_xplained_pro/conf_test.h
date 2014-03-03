6442dd485e33f861f8b89de78476f00cad2d7542
cq.xiao <cq.xiao@atmel.com>
2014-03-03 14:56:06 +0800

    Add EVENTS.

--- sam0/drivers/events/unit_test/samr21g18a_samr21_xplained_pro/conf_test.h
+++ sam0/drivers/events/unit_test/samr21g18a_samr21_xplained_pro/conf_test.h
@@ -1,9 +1,9 @@
 /**
  * \file
  *
- * \brief SAM D20/D21/R21 Event System Driver Quick Start
+ * \brief SAM R21 Xplained Pro test configuration.
  *
- * Copyright (C) 2013-2014 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,72 +40,16 @@
  * \asf_license_stop
  *
  */
-#include <asf.h>
 
-//! [setup]
+#ifndef CONF_TEST_H_INCLUDED
+#define CONF_TEST_H_INCLUDED
 
-#define EXAMPLE_EVENT_GENERATOR    EVSYS_ID_GEN_TC4_MCX_0
-#define EXAMPLE_EVENT_USER         EVSYS_ID_USER_TC3_EVU
+#define CONF_STDIO_USART          EDBG_CDC_MODULE
+#define CONF_STDIO_MUX_SETTING    EDBG_CDC_SERCOM_MUX_SETTING
+#define CONF_STDIO_PINMUX_PAD0    EDBG_CDC_SERCOM_PINMUX_PAD0
+#define CONF_STDIO_PINMUX_PAD1    EDBG_CDC_SERCOM_PINMUX_PAD1
+#define CONF_STDIO_PINMUX_PAD2    EDBG_CDC_SERCOM_PINMUX_PAD2
+#define CONF_STDIO_PINMUX_PAD3    EDBG_CDC_SERCOM_PINMUX_PAD3
+#define CONF_STDIO_BAUDRATE       38400
 
-static void configure_event_channel(struct events_resource *resource)
-{
-//! [setup_1]
-	struct events_config config;
-//! [setup_1]
-
-//! [setup_2]
-	events_get_config_defaults(&config);
-//! [setup_2]
-
-//! [setup_3]
-	config.generator      = EXAMPLE_EVENT_GENERATOR;
-	config.edge_detect    = EVENTS_EDGE_DETECT_RISING;
-	config.path           = EVENTS_PATH_SYNCHRONOUS;
-	config.clock_source   = GCLK_GENERATOR_0;
-//! [setup_3]
-
-//! [setup_4]
-	events_allocate(resource, &config);
-//! [setup_4]
-}
-
-static void configure_event_user(struct events_resource *resource)
-{
-//! [setup_5]
-	events_attach_user(resource, EXAMPLE_EVENT_USER);
-//! [setup_5]
-}
-
-//! [setup]
-
-int main(void)
-{
-//! [events_resource_struct]
-	struct events_resource example_event;
-//! [events_resource_struct]
-
-	system_init();
-
-//! [setup_init]
-	configure_event_channel(&example_event);
-	configure_event_user(&example_event);
-//! [setup_init]
-
-	//! [main]
-
-	//! [main_1]
-	while (events_is_busy(&example_event)) {
-		/* Wait for channel */
-	};
-	//! [main_1]
-
-	//! [main_2]
-	events_trigger(&example_event);
-	//! [main_2]
-
-	while (true) {
-		/* Nothing to do */
-	}
-
-	//! [main]
-}
+#endif /* CONF_TEST_H_INCLUDED */
