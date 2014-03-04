157e45e330719c5235aad430a1cec98f0c6ae001
“zheng.wang” <zheng.wang@atmel.com>
2014-03-04 14:14:20 +0800

    [ANPI-3567] USB host QS update.

--- common/services/usb/uhc/uhc.h
+++ common/services/usb/uhc/uhc.h
@@ -374,16 +374,18 @@ bool uhc_dev_is_high_speed_support(uhc_device_t* dev);
  * \defgroup uhc_basic_use_case_setup_prereq USB Host Controller (UHC) - Prerequisites
  * Common prerequisites for all USB hosts.
  *
- * This module is based on USB host stack full interrupt driven, and supporting
- * \ref sleepmgr_group sleepmgr and \ref clk_group clock services.
+ * This module is based on USB host stack full interrupt driven and supporting
+ * \ref sleepmgr_group sleepmgr.
  *
  * The following procedure must be executed to setup the project correctly:
  * - Specify the clock configuration:
- *   - UC3 and SAM devices without USB high speed support need 48MHz clock input.\n
+ *   - UC3 and SAM3/4 devices without USB high speed support need 48MHz clock input.\n
  *     You must use a PLL and an external OSC.
- *   - UC3 and SAM devices with USB high speed support need 12MHz clock input.\n
+ *   - UC3 and SAM3/4 devices with USB high speed support need 12MHz clock input.\n
  *     You must use an external OSC.
  *   - UC3 devices with USBC hardware need CPU frequency higher than 25MHz.
+ *   - SAMD devices without USB high speed support need 48MHz clock input.\n
+ *     You must use a DFLL and an external OSC.
  * - In conf_board.h, the define CONF_BOARD_USB_PORT must be added to enable USB lines.
  * (Not mandatory for all boards)
  * - Enable interrupts
@@ -398,10 +400,14 @@ bool uhc_dev_is_high_speed_support(uhc_device_t* dev);
  *
  * Add to the initialization code:
  * \code
+ * #if SAMD21
+ *     system_init();
+ * #else
  *     sysclk_init();
+ *     board_init();
+ * #endif
  *     irq_initialize_vectors();
  *     cpu_irq_enable();
- *     board_init();
  *     sleepmgr_init(); // Optional
  * \endcode
  * Add to the main IDLE loop:
@@ -480,6 +486,55 @@ bool uhc_dev_is_high_speed_support(uhc_device_t* dev);
  * // USB Clock Source fixed at UPLL.
  * #define CONFIG_USBCLK_SOURCE        USBCLK_SRC_UPLL
  * #define CONFIG_USBCLK_DIV           1
+ * \endcode
+ * 
+ * Content of conf_clocks.h for SAMD devices (USB):
+ * \code
+ * // USB Clock Source fixed at DFLL.
+ * // SYSTEM_CLOCK_SOURCE_XOSC32K configuration - External 32KHz crystal/clock oscillator
+ * #  define CONF_CLOCK_XOSC32K_ENABLE               true
+ * #  define CONF_CLOCK_XOSC32K_EXTERNAL_CRYSTAL     SYSTEM_CLOCK_EXTERNAL_CRYSTAL
+ * #  define CONF_CLOCK_XOSC32K_STARTUP_TIME         SYSTEM_XOSC32K_STARTUP_65536
+ * #  define CONF_CLOCK_XOSC32K_AUTO_AMPLITUDE_CONTROL  false
+ * #  define CONF_CLOCK_XOSC32K_ENABLE_1KHZ_OUPUT    false
+ * #  define CONF_CLOCK_XOSC32K_ENABLE_32KHZ_OUTPUT  true
+ * #  define CONF_CLOCK_XOSC32K_ON_DEMAND            false
+ * #  define CONF_CLOCK_XOSC32K_RUN_IN_STANDBY       true
+ * // SYSTEM_CLOCK_SOURCE_DFLL configuration - Digital Frequency Locked Loop
+ * #  define CONF_CLOCK_DFLL_ENABLE                  true
+ * #  define CONF_CLOCK_DFLL_LOOP_MODE               SYSTEM_CLOCK_DFLL_LOOP_MODE_CLOSED
+ * #  define CONF_CLOCK_DFLL_ON_DEMAND               true
+ *
+ * // DFLL open loop mode configuration
+ * #  define CONF_CLOCK_DFLL_COARSE_VALUE            (0x1f / 4)
+ * #  define CONF_CLOCK_DFLL_FINE_VALUE              (0xff / 4)
+ *
+ * // DFLL closed loop mode configuration
+ * #  define CONF_CLOCK_DFLL_SOURCE_GCLK_GENERATOR   GCLK_GENERATOR_1
+ * #  define CONF_CLOCK_DFLL_MULTIPLY_FACTOR         (48000000/32768)
+ * #  define CONF_CLOCK_DFLL_QUICK_LOCK              true
+ * #  define CONF_CLOCK_DFLL_TRACK_AFTER_FINE_LOCK   true
+ * #  define CONF_CLOCK_DFLL_KEEP_LOCK_ON_WAKEUP     true
+ * #  define CONF_CLOCK_DFLL_ENABLE_CHILL_CYCLE      true
+ * #  define CONF_CLOCK_DFLL_MAX_COARSE_STEP_SIZE    (0x1f / 8)
+ * #  define CONF_CLOCK_DFLL_MAX_FINE_STEP_SIZE      (0xff / 8)
+ *
+ * #  define CONF_CLOCK_CONFIGURE_GCLK               true
+ *
+ * // Configure GCLK generator 0 (Main Clock)
+ * #  define CONF_CLOCK_GCLK_0_ENABLE                true
+ * #  define CONF_CLOCK_GCLK_0_RUN_IN_STANDBY        true
+ * #  define CONF_CLOCK_GCLK_0_CLOCK_SOURCE          SYSTEM_CLOCK_SOURCE_DFLL
+ * #  define CONF_CLOCK_GCLK_0_PRESCALER             1
+ * #  define CONF_CLOCK_GCLK_0_OUTPUT_ENABLE         false
+ *
+ * // Configure GCLK generator 1
+ * #  define CONF_CLOCK_GCLK_1_ENABLE                true
+ * #  define CONF_CLOCK_GCLK_1_RUN_IN_STANDBY        false
+ * #  define CONF_CLOCK_GCLK_1_CLOCK_SOURCE          SYSTEM_CLOCK_SOURCE_XOSC32K
+ * #  define CONF_CLOCK_GCLK_1_PRESCALER             1
+ * #  define CONF_CLOCK_GCLK_1_OUTPUT_ENABLE         false
+ *
  * \endcode
  */
 
