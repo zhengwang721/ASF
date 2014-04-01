f92dc48d2bf947ee2075bc3bc72b85ac88d95208
Sriram.R <sriram.r@atmel.com>
2014-04-01 15:31:31 +0530

    Fix for http://norvm-tools-m.norway.atmel.com/hudson/job/asf-branch-board-atmel-linux/5065/

--- thirdparty/wireless/services/common_hw_timer/module_config/conf_hw_timer.h
+++ thirdparty/wireless/services/common_hw_timer/module_config/conf_hw_timer.h
@@ -65,7 +65,7 @@
 #define TIMER_CHANNEL_ID     (0)
 #endif /* SAM4L */
 
-#if (SAM4S)
+#if (SAM4S || SAM4E)
 #define TIMER                (TC0)
 #define TIMER_CHANNEL_ID     0
 #define ID_TC                (ID_TC0)
