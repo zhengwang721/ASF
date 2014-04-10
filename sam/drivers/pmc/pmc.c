78603b84d8e8bf59562270286518eb166b35e38f
Nash Zhang <nash.zhang@atmel.com>
2014-04-10 17:23:00 +0800

    Fix fastrc switch function and pmc_clock_switch_example.

--- sam/drivers/pmc/pmc.c
+++ sam/drivers/pmc/pmc.c
@@ -316,21 +316,22 @@ uint32_t pmc_osc_is_ready_32kxtal(void)
  */
 void pmc_switch_mainck_to_fastrc(uint32_t ul_moscrcf)
 {
-	uint32_t ul_needXTEN = 0;
+	bool moscxt_enabled = false;
 
 	/* Enable Fast RC oscillator but DO NOT switch to RC now */
 	if (PMC->CKGR_MOR & CKGR_MOR_MOSCXTEN) {
-		PMC->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN;
-	} else {
-		ul_needXTEN = 1;
-		PMC->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN |
+		moscxt_enabled = true;
+/*		PMC->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN |
 				CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCXTST_Msk;
+	} else {
+		PMC->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN;*/
 	}
+	PMC->CKGR_MOR |= (CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN);
 
 	/* Wait the Fast RC to stabilize */
 	while (!(PMC->PMC_SR & PMC_SR_MOSCRCS));
 
-	/* Change Fast RC oscillator frequency but DO NOT switch to RC now */
+	/* Change Fast RC oscillator frequency */
 	PMC->CKGR_MOR = (PMC->CKGR_MOR & ~CKGR_MOR_MOSCRCF_Msk) |
 			CKGR_MOR_KEY_PASSWD | ul_moscrcf;
 
@@ -342,7 +343,7 @@ void pmc_switch_mainck_to_fastrc(uint32_t ul_moscrcf)
 			CKGR_MOR_KEY_PASSWD;
 
 	/* Disable xtal oscillator */
-	if (ul_needXTEN) {
+	if (moscxt_enabled) {
 		PMC->CKGR_MOR = (PMC->CKGR_MOR & ~CKGR_MOR_MOSCXTEN) |
 				CKGR_MOR_KEY_PASSWD;
 	}
@@ -355,10 +356,14 @@ void pmc_switch_mainck_to_fastrc(uint32_t ul_moscrcf)
  */
 void pmc_osc_enable_fastrc(uint32_t ul_rc)
 {
-	/* Enable Fast RC oscillator but DO NOT switch to RC now.
-	 * Keep MOSCSEL to 1 */
-	PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCSEL |
-			CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCRCEN | ul_rc;
+	/* Enable Fast RC oscillator but DO NOT switch to RC */
+	PMC->CKGR_MOR |= (CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN);
+	/* Wait the Fast RC to stabilize */
+	while (!(PMC->PMC_SR & PMC_SR_MOSCRCS));
+
+	/* Change Fast RC oscillator frequency */
+	PMC->CKGR_MOR = (PMC->CKGR_MOR & ~CKGR_MOR_MOSCRCF_Msk) |
+			CKGR_MOR_KEY_PASSWD | ul_rc;
 	/* Wait the Fast RC to stabilize */
 	while (!(PMC->PMC_SR & PMC_SR_MOSCRCS));
 }
