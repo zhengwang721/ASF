3f68aaddff08709833cf5af8c755899157971ce7
Sriram.R <sriram.r@atmel.com>
2014-04-23 13:29:51 +0530

    Updated EULA.txt

--- thirdparty/wireless/avr2130_lwmesh/apps/wsndemo/config.h
+++ thirdparty/wireless/avr2130_lwmesh/apps/wsndemo/config.h
@@ -50,9 +50,9 @@
 
 /*****************************************************************************
 *****************************************************************************/
-#define APP_ADDR                0x0100
-#define APP_PANID               0x1260
-#define APP_SENDING_INTERVAL    25000
+#define APP_ADDR                0x0000
+#define APP_PANID               0x1234
+#define APP_SENDING_INTERVAL    2000
 #define APP_ENDPOINT            1
 #define APP_SECURITY_KEY        "TestSecurityKey0"
 
