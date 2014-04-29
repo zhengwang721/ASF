67d783baff43aa372872c5e95c25d6103fd6408b
sheng.hu <sheng.hu@atmel.com>
2014-04-29 12:35:57 +0800

    [ANPI-3237] Update code after review.

--- sam0/drivers/dma/dma.h
+++ sam0/drivers/dma/dma.h
@@ -792,8 +792,8 @@ enum status_code dma_add_descriptor(struct dma_resource *resource,
  *        <th>Date</td>
  *        <th>Comments</td>
  *    </tr>
-  *    <tr>
- *        <td>B</td>
+ *    <tr>
+ *        <td>C</td>
  *        <td>04/2014</td>
  *        <td>Add SAMD10/D11 support</td>
  *    </tr>
