6542a2900425f9f934c09389e2a6557d7c142592
Christian Guemes <cristian.guemes@atmel.com>
2014-06-19 12:33:06 +0200

    [ASFP-4680] Review style code using uncrustify tool (PLC services)

--- sam/services/plc/pplc_if/atpl230/module_config/conf_pplc_if.h
+++ sam/services/plc/pplc_if/atpl230/module_config/conf_pplc_if.h
@@ -1,45 +1,45 @@
 /**
-* \file
-*
-* \brief PPLC interface Configuration.
-*
-* Copyright (c) 2014 Atmel Corporation. All rights reserved.
-*
-* \asf_license_start
-*
-* \page License
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice,
-*    this list of conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*
-* 3. The name of Atmel may not be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-* 4. This software may only be redistributed and used in connection with an
-*    Atmel microcontroller product.
-*
-* THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
-* EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-* \asf_license_stop
-*
-*/
+ * \file
+ *
+ * \brief PPLC interface Configuration.
+ *
+ * Copyright (c) 2014 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
 
 #ifndef CONF_PPLC_IF_H_INCLUDE
 #define CONF_PPLC_IF_H_INCLUDE
@@ -60,7 +60,7 @@
 #define PPLC_INT_SENSE (IOPORT_SENSE_FALLING)
 
 #define PPLC_INT       {PIO_PB30, PIOB, ID_PIOB, PIO_INPUT, \
-		PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
+			PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
 #define PPLC_INT_MASK  PIO_PB30
 #define PPLC_INT_PIO   PIOB
 #define PPLC_INT_ID    ID_PIOB
@@ -68,7 +68,7 @@
 #define PPLC_INT_ATTR  (PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
 #define PPLC_INT_IRQn  PIOB_IRQn
 
-/* Asynchronous PPLC Reset pin definition */ 
+/* Asynchronous PPLC Reset pin definition */
 #define PPLC_ARST_GPIO             (PIO_PC6_IDX)
 #define PPLC_ARST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_ARST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -81,7 +81,7 @@
 #define PPLC_ARST_TYPE   PIO_OUTPUT_1
 #define PPLC_ARST_ATTR   PIO_DEFAULT
 
-/* Synchronous PPLC Reset pin definition */ 
+/* Synchronous PPLC Reset pin definition */
 #define PPLC_SRST_GPIO             (PIO_PC7_IDX)
 #define PPLC_SRST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_SRST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -108,15 +108,15 @@
 #define PPLC_INT_SENSE (IOPORT_SENSE_FALLING)
 
 #define PPLC_INT       {PIO_PA12, PIOB, ID_PIOB, PIO_INPUT, \
-		PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
+			PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
 #define PPLC_INT_MASK  PIO_PA12
 #define PPLC_INT_PIO   PIOB
 #define PPLC_INT_ID    ID_PIOB
 #define PPLC_INT_TYPE  PIO_INPUT
 #define PPLC_INT_ATTR  (PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
 #define PPLC_INT_IRQn  PIOB_IRQn
- 
-/* Asynchronous PPLC Reset pin definition */ 
+
+/* Asynchronous PPLC Reset pin definition */
 #define PPLC_ARST_GPIO             (PIO_PB0_IDX)
 #define PPLC_ARST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_ARST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -129,7 +129,7 @@
 #define PPLC_ARST_TYPE   PIO_OUTPUT_1
 #define PPLC_ARST_ATTR   PIO_DEFAULT
 
-/* Synchronous PPLC Reset pin definition */ 
+/* Synchronous PPLC Reset pin definition */
 #define PPLC_SRST_GPIO             (PIO_PA25_IDX)
 #define PPLC_SRST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_SRST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -156,15 +156,15 @@
 #define PPLC_INT_SENSE (IOPORT_SENSE_FALLING)
 
 #define PPLC_INT       {PIO_PB13, PIOB, ID_PIOB, PIO_INPUT, \
-		PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
+			PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
 #define PPLC_INT_MASK  PIO_PB13
 #define PPLC_INT_PIO   PIOB
 #define PPLC_INT_ID    ID_PIOB
 #define PPLC_INT_TYPE  PIO_INPUT
 #define PPLC_INT_ATTR  (PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
 #define PPLC_INT_IRQn  PIOB_IRQn
- 
-/* Asynchronous PPLC Reset pin definition */ 
+
+/* Asynchronous PPLC Reset pin definition */
 #define PPLC_ARST_GPIO             (PIO_PB0_IDX)
 #define PPLC_ARST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_ARST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -177,7 +177,7 @@
 #define PPLC_ARST_TYPE   PIO_OUTPUT_1
 #define PPLC_ARST_ATTR   PIO_DEFAULT
 
-/* Synchronous PPLC Reset pin definition */ 
+/* Synchronous PPLC Reset pin definition */
 #define PPLC_SRST_GPIO             (PIO_PA25_IDX)
 #define PPLC_SRST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_SRST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -204,15 +204,15 @@
 #define PPLC_INT_SENSE (IOPORT_SENSE_FALLING)
 
 #define PPLC_INT       {PIO_PB13, PIOB, ID_PIOB, PIO_INPUT, \
-		PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
+			PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
 #define PPLC_INT_MASK  PIO_PB13
 #define PPLC_INT_PIO   PIOB
 #define PPLC_INT_ID    ID_PIOB
 #define PPLC_INT_TYPE  PIO_INPUT
 #define PPLC_INT_ATTR  (PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
 #define PPLC_INT_IRQn  PIOB_IRQn
- 
-/* Asynchronous PPLC Reset pin definition */ 
+
+/* Asynchronous PPLC Reset pin definition */
 #define PPLC_ARST_GPIO             (PIO_PB0_IDX)
 #define PPLC_ARST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_ARST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -225,7 +225,7 @@
 #define PPLC_ARST_TYPE   PIO_OUTPUT_1
 #define PPLC_ARST_ATTR   PIO_DEFAULT
 
-/* Synchronous PPLC Reset pin definition */ 
+/* Synchronous PPLC Reset pin definition */
 #define PPLC_SRST_GPIO             (PIO_PA25_IDX)
 #define PPLC_SRST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_SRST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -252,15 +252,15 @@
 #define PPLC_INT_SENSE (IOPORT_SENSE_FALLING)
 
 #define PPLC_INT       {PIO_PA15, PIOA, ID_PIOA, PIO_INPUT, \
-		PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
+			PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
 #define PPLC_INT_MASK  PIO_PA15
 #define PPLC_INT_PIO   PIOA
 #define PPLC_INT_ID    ID_PIOA
 #define PPLC_INT_TYPE  PIO_INPUT
 #define PPLC_INT_ATTR  (PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
 #define PPLC_INT_IRQn  PIOA_IRQn
- 
-/* Asynchronous PPLC Reset pin definition */ 
+
+/* Asynchronous PPLC Reset pin definition */
 #define PPLC_ARST_GPIO             (PIO_PA27_IDX)
 #define PPLC_ARST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_ARST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
@@ -273,7 +273,7 @@
 #define PPLC_ARST_TYPE   PIO_OUTPUT_1
 #define PPLC_ARST_ATTR   PIO_DEFAULT
 
-/* Synchronous PPLC Reset pin definition */ 
+/* Synchronous PPLC Reset pin definition */
 #define PPLC_SRST_GPIO             (PIO_PA28_IDX)
 #define PPLC_SRST_ACTIVE_LEVEL    IOPORT_PIN_LEVEL_LOW
 #define PPLC_SRST_INACTIVE_LEVEL  IOPORT_PIN_LEVEL_HIGH
--- sam/services/plc/usi/usi.h
+++ sam/services/plc/usi/usi.h
@@ -1,58 +1,58 @@
 /**
-* \file
-*
-* \brief PLC Service Universal Serial Interface
-*
-* Copyright (c) 2014 Atmel Corporation. All rights reserved.
-*
-* \asf_license_start
-*
-* \page License
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice,
-*    this list of conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*
-* 3. The name of Atmel may not be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-* 4. This software may only be redistributed and used in connection with an
-*    Atmel microcontroller product.
-*
-* THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
-* EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-* \asf_license_stop
-*
-*/
+ * \file
+ *
+ * \brief PLC Service Universal Serial Interface
+ *
+ * Copyright (c) 2014 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
 
 #ifndef USI_H_INCLUDED
 #define USI_H_INCLUDED
 
 #include "conf_usi.h"
 
-/// @cond 0
+/* / @cond 0 */
 /**INDENT-OFF**/
 #ifdef __cplusplus
 extern "C" {
 #endif
 /**INDENT-ON**/
-/// @endcond
+/* / @endcond */
 
 /**
  * \ingroup plc_group
@@ -64,8 +64,8 @@ extern "C" {
  * @{
  */
 
-//! \name Management Plane Protocol Spec.
-//@{
+/* ! \name Management Plane Protocol Spec. */
+/* @{ */
 #ifndef MNGP_PRIME_PROT
 #define MNGP_PRIME_PROT         0x00
 #endif
@@ -101,12 +101,11 @@ extern "C" {
 #ifndef MNGP_PRIME_EN_PIBRSP
 #define MNGP_PRIME_EN_PIBRSP    0x07
 #endif
-//@}
+/* @} */
 
-
-//! \name ATMEL serialized protocols
-//@{
-typedef enum{
+/* ! \name ATMEL serialized protocols */
+/* @{ */
+typedef enum {
 	PROTOCOL_SNIF_PRIME    = 0x13,
 	PROTOCOL_MAC_PRIME     = 0x17,
 	PROTOCOL_MLME_PRIME    = 0x18,
@@ -117,18 +116,17 @@ typedef enum{
 	PROTOCOL_ATPL230       = 0x22,
 	PROTOCOL_USER_DEFINED  = 0xFE,
 	PROTOCOL_INVALID       = 0xFF
-}usi_protocol_t;
-//@}
+} usi_protocol_t;
+/* @} */
 
-//! \name USI protocol parameters
-//@{
+/* ! \name USI protocol parameters */
+/* @{ */
 #define CMD_PRIME_PROTOCOL_MSK       0x3F
 #define CMD_PRIME_PROTOCOL(A)        ((A)&CMD_PRIME_PROTOCOL_MSK)
-//@}
+/* @} */
 
-
-//! USI operation results
-typedef enum{
+/* ! USI operation results */
+typedef enum {
 	USI_STATUS_PROTOCOL_NOT_FOUND = 0xFF,
 	USI_STATUS_TX_BUFFER_OVERFLOW,
 	USI_STATUS_TX_BUSY,
@@ -141,42 +139,42 @@ typedef enum{
 	USI_STATUS_INVALID
 } usi_status_t;
 
-//! Message Structure to communicate with USI layer
-typedef struct
-{
-	uint8_t uc_protocol_type;    //!<  Protocol Type
-	uint8_t *ptr_buf;            //!<  Pointer to data buffer
-	uint16_t us_len;             //!<  Length of data
-}x_usi_serial_cmd_params_t;
+/* ! Message Structure to communicate with USI layer */
+typedef struct {
+	uint8_t uc_protocol_type;    /* !<  Protocol Type */
+	uint8_t *ptr_buf;            /* !<  Pointer to data buffer */
+	uint16_t us_len;             /* !<  Length of data */
+} x_usi_serial_cmd_params_t;
 
-//! Type for callback function pointers
-typedef uint8_t (* pf_usi_decode_cmd)(uint8_t*, uint16_t);
+/* ! Type for callback function pointers */
+typedef uint8_t (*pf_usi_decode_cmd)(uint8_t *, uint16_t);
 
-//! \name Types to manage commands
-//@{
-typedef uint8_t (* pf_usi_get_cmd)(void);
-typedef void (* pf_usi_set_cmd)(uint8_t);
-//@}
+/* ! \name Types to manage commands */
+/* @{ */
+typedef uint8_t (*pf_usi_get_cmd)(void);
+typedef void (*pf_usi_set_cmd)(uint8_t);
+/* @} */
 
 /**
  *  \name Universal Serial Interface API functions
  */
-//@{
+/* @{ */
 void usi_init(void);
 void usi_process(void);
 
-usi_status_t usi_send_cmd (void *msg);
+usi_status_t usi_send_cmd(void *msg);
 void usi_txrx_block_timer(void);
-//@}
 
-//! @}
+/* @} */
 
-/// @cond 0
+/* ! @} */
+
+/* / @cond 0 */
 /**INDENT-OFF**/
 #ifdef __cplusplus
 }
 #endif
 /**INDENT-ON**/
-/// @endcond
+/* / @endcond */
 
 #endif /* USI_H_INCLUDED */
