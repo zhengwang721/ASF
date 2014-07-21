09d9d5f92b64eb12b3f82a8aa6675fe3fac50cd5
unknown <yue.cao@SHALT0295.corp.atmel.com>
2014-07-21 18:01:13 +0800

    update copyright

--- common/services/usb/class/cdc/device/udi_cdc.c
+++ common/services/usb/class/cdc/device/udi_cdc.c
@@ -3,7 +3,7 @@
  *
  * \brief USB Device Communication Device Class (CDC) interface.
  *
- * Copyright (c) 2009 - 2013 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2009 - 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
