642bae003909cce421680b957e65071b5400fff9
tonny.lan <shouzhen.lan@atmel.com>
2014-09-19 15:27:59 +0800

    ANPI-4448 Add AES support

---
+++ sam0/drivers/aes/aes_callback.h
@@ -0,0 +1,151 @@
+/**
+ *
+ * \file
+ *
+ * \brief SAM Advanced Encryption Standard driver.
+ *
+ * This file defines a useful set of functions for the AES on SAM devices.
+ *
+ * Copyright (c) 2014 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+#ifndef AES_CALLBACK_H_INCLUDED
+#define AES_CALLBACK_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <compiler.h>
+
+/**
+ * \addtogroup asfdoc_sam0_drivers_aes_group
+ *
+ * @{
+ */
+
+/** \name Callback configuration and initialization
+ * @{
+ */
+
+/** AES callback type. */
+enum aes_callback_type {
+	/** Encryption complete callback.*/
+	AES_CALLBACK_ENCRYPTION_COMPLETE = 0,
+	/** GF Multiplication Complete callback.*/
+	AES_CALLBACK_GF_MULTI_COMPLETE = 1,
+};
+
+
+/** AES interrupt callback function type. */
+typedef void (*aes_callback_t)(void);
+
+enum status_code aes_register_callback(
+	const aes_callback_t callback,
+	const enum aes_callback_type type);
+
+enum status_code aes_unregister_callback(
+	const aes_callback_t callback,
+	const enum aes_callback_type type);
+
+/** @} */
+
+
+/** \name Callback enabling and disabling
+ * @{
+ */
+
+/**
+ * \brief Enable an AES interrupt.
+ *
+ * \param[in,out] module  Pointer to the software instance struct
+ * \param[in] type Interrupt source type
+ *
+ * \return Status of the callback enable operation.
+ * \retval STATUS_OK The callback was enabled successfully.
+ * \retval STATUS_ERR_INVALID_ARG If an invalid callback type was supplied.
+ */
+static inline enum status_code aes_enable_interrupt(struct aes_module *const module,
+		const enum aes_callback_type type)
+{
+	if (type == AES_CALLBACK_ENCRYPTION_COMPLETE){
+		module->hw->INTENSET.reg = AES_INTENSET_ENCCMP;
+	} else if (type == AES_CALLBACK_GF_MULTI_COMPLETE){
+		module->hw->INTENSET.reg = AES_INTENSET_GFMCMP;
+	} else {
+		Assert(false);
+		return STATUS_ERR_INVALID_ARG;
+	}
+
+	return STATUS_OK;
+}
+
+/**
+ * \brief Disable an AES interrupt.
+ *
+ * \param[in,out] module  Pointer to the software instance struct
+ * \param[in]  type Interrupt source type
+ *
+ * \return Status of the callback enable operation.
+ * \retval STATUS_OK The callback was enabled successfully.
+ * \retval STATUS_ERR_INVALID_ARG If an invalid callback type was supplied.
+ */
+static inline enum status_code aes_disable_interrupt(struct aes_module *const module,
+		 const enum aes_callback_type type)
+{
+	if (type == AES_CALLBACK_ENCRYPTION_COMPLETE){
+		module->hw->INTENCLR.reg = AES_INTENCLR_ENCCMP;
+	} else if (type == AES_CALLBACK_GF_MULTI_COMPLETE){
+		module->hw->INTENCLR.reg = AES_INTENCLR_GFMCMP;
+	} else {
+		Assert(false);
+		return STATUS_ERR_INVALID_ARG;
+	}
+
+	return STATUS_OK;
+}
+
+/** @} */
+
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* AES_CALLBACK_H_INCLUDED */
