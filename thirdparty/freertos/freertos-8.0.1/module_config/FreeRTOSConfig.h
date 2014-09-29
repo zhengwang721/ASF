ac6e39648957b0ba6d592996ff3f3dee76478148
“zheng.wang” <zheng.wang@atmel.com>
2014-09-29 17:44:16 +0800

    [SAMG55] Fix some Jenkins issue.

--- thirdparty/freertos/freertos-8.0.1/module_config/FreeRTOSConfig.h
+++ thirdparty/freertos/freertos-8.0.1/module_config/FreeRTOSConfig.h
@@ -75,4 +75,9 @@ standard names - or at least those used in the unmodified vector table. */
 #define xPortPendSVHandler                      PendSV_Handler
 #define xPortSysTickHandler                     SysTick_Handler
 
+/* Interrupt priorities used by the kernel port layer itself.  These are generic
+to all Cortex-M ports, and do not rely on any particular library functions. */
+#define configKERNEL_INTERRUPT_PRIORITY 		( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+
 #endif /* FREERTOS_CONFIG_H */
