c8526dd577b34e42b8c6c7affcb6d828236c1ba6
Roeder Cao <yue.cao@atmel.com>
2014-10-14 16:27:51 +0800

    fix mismodify

--- common/services/usb/udc/udc_doc.h
+++ common/services/usb/udc/udc_doc.h
@@ -367,7 +367,7 @@ Event_VBUS_present() // VBUS interrupt or GPIO interrupt or other
  #define CONFIG_OSC_RC32_CAL         48000000UL
  #define CONFIG_OSC_AUTOCAL_RC32MHZ_REF_OSC  OSC_ID_USBSOF
  // CPU clock need of clock > 12MHz to run with USB (Here 24MHz)
- #define CONFIG_SYSCLK_SOURCE     SYSCLK_SRC_RC32MHz
+ #define CONFIG_SYSCLK_SOURCE     SYSCLK_SRC_RC32MHZ
  #define CONFIG_SYSCLK_PSADIV     SYSCLK_PSADIV_2
  #define CONFIG_SYSCLK_PSBCDIV    SYSCLK_PSBCDIV_1_1
  \endcode
