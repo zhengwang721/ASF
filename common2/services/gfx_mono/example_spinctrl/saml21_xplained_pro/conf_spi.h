bd0647a4ee6d5a570c4c6a3b8d24cfc3f19cd8a0
“zheng.wang” <zheng.wang@atmel.com>
2014-10-23 14:52:01 +0800

    [SAML21] Add SSD1306 and gfx mono support.

--- common2/services/gfx_mono/example_spinctrl/saml21_xplained_pro/conf_spi.h
+++ common2/services/gfx_mono/example_spinctrl/saml21_xplained_pro/conf_spi.h
@@ -1,9 +1,9 @@
 /**
  * \file
  *
- * \brief SSD1306 display controller driver configuration file.
+ * \brief SAM D21 SPI configuration
  *
- * Copyright (c) 2012-2014 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2014 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,28 +40,14 @@
  * \asf_license_stop
  *
  */
-#ifndef CONF_SSD1306_H_INCLUDED
-#define CONF_SSD1306_H_INCLUDED
 
-#include <board.h>
 
-// Interface configuration for SAM Xplained Pro
-#  define SSD1306_SPI                 EXT3_SPI_MODULE
-#  define CONFIG_SSD1306_FRAMEBUFFER
+#ifndef CONF_SPI_H_INCLUDED
+#  define CONF_SPI_H_INCLUDED
 
-#  define SSD1306_DC_PIN              EXT3_PIN_5
-#  define SSD1306_RES_PIN             EXT3_PIN_10
-#  define SSD1306_CS_PIN              EXT3_PIN_15
+#  define CONF_SPI_MASTER_ENABLE     true
+#  define CONF_SPI_SLAVE_ENABLE      false
+#  define CONF_SPI_TIMEOUT           10000
 
-#  define SSD1306_SPI_PINMUX_SETTING  EXT3_SPI_SERCOM_MUX_SETTING
-#  define SSD1306_SPI_PINMUX_PAD0     EXT3_SPI_SERCOM_PINMUX_PAD0
-#  define SSD1306_SPI_PINMUX_PAD1     PINMUX_UNUSED
-#  define SSD1306_SPI_PINMUX_PAD2     EXT3_SPI_SERCOM_PINMUX_PAD2
-#  define SSD1306_SPI_PINMUX_PAD3     EXT3_SPI_SERCOM_PINMUX_PAD3
+#endif /* CONF_SPI_H_INCLUDED */
 
-// Minimum clock period is 50ns@3.3V -> max frequency is 20MHz
-#define SSD1306_CLOCK_SPEED           1000000UL
-#define SSD1306_DISPLAY_CONTRAST_MAX  40
-#define SSD1306_DISPLAY_CONTRAST_MIN  30
-
-#endif /* CONF_SSD1306_H_INCLUDED */
