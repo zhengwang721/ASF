7bd3c1e157d45d6792afdf095c49c00e10477efb
sheng.hu <sheng.hu@atmel.com>
2014-11-10 11:44:12 +0800

    [ANPI-4455] Add USB host service support.

--- common/services/usb/class/hid/dual/mouse/example/saml21j18a_saml21_xplained_pro/conf_clocks.h
+++ common/services/usb/class/hid/dual/mouse/example/saml21j18a_saml21_xplained_pro/conf_clocks.h
@@ -53,7 +53,7 @@
 #  define CONF_CLOCK_BACKUP_DIVIDER               SYSTEM_MAIN_CLOCK_DIV_1
 
 /* SYSTEM_CLOCK_SOURCE_OSC16M configuration - Internal 16MHz oscillator */
-#  define CONF_CLOCK_OSC16M_FREQ_SEL              SYSTEM_OSC16M_4M 
+#  define CONF_CLOCK_OSC16M_FREQ_SEL              SYSTEM_OSC16M_16M 
 #  define CONF_CLOCK_OSC16M_ON_DEMAND             true
 #  define CONF_CLOCK_OSC16M_RUN_IN_STANDBY        false
 
@@ -90,7 +90,7 @@
 /* SYSTEM_CLOCK_SOURCE_DFLL configuration - Digital Frequency Locked Loop */
 #  define CONF_CLOCK_DFLL_ENABLE                  true
 #  define CONF_CLOCK_DFLL_LOOP_MODE               SYSTEM_CLOCK_DFLL_LOOP_MODE_CLOSED
-#  define CONF_CLOCK_DFLL_ON_DEMAND               true
+#  define CONF_CLOCK_DFLL_ON_DEMAND               false
 
 /* DFLL open loop mode configuration */
 #  define CONF_CLOCK_DFLL_COARSE_VALUE            (0x1f / 4)
