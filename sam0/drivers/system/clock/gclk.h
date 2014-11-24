34c6fddde91d024f546dc2f1903ec74cced343f0
tonny.lan <shouzhen.lan@atmel.com>
2014-11-24 14:27:38 +0800

    ANPI-4228 Add SAMC21 clock support

--- sam0/drivers/system/clock/gclk.h
+++ sam0/drivers/system/clock/gclk.h
@@ -206,6 +206,8 @@ static inline void system_gclk_gen_get_config_defaults(
 	config->high_when_disabled = false;
 #if SAML21
 	config->source_clock       = GCLK_SOURCE_OSC16M;
+#else if SAMC21
+	config->source_clock       = GCLK_SOURCE_OSC48M;
 #else
 	config->source_clock       = GCLK_SOURCE_OSC8M;
 #endif
