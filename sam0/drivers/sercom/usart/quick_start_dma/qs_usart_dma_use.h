dcfcb2c8099834ac8840f14bd9f6f85d7458d311
tonny.lan <shouzhen.lan@atmel.com>
2014-12-23 14:32:02 +0800

    ANPI-4227 Update USART for samc21 support

--- sam0/drivers/sercom/usart/quick_start_dma/qs_usart_dma_use.h
+++ sam0/drivers/sercom/usart/quick_start_dma/qs_usart_dma_use.h
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief SAM D21/R21/D10/D11 Quick Start Guide for Using Usart driver with DMA
+ * \brief SAM Quick Start Guide for Using Usart driver with DMA
  *
  * Copyright (C) 2014 Atmel Corporation. All rights reserved.
  *
@@ -48,6 +48,8 @@
  *    - SAMD21 Xplained Pro
  *    - SAMR21 Xplained Pro
  *    - SAMR11 Xplained Pro
+ *    - SAML21 Xplained Pro
+ *    - SAMC21 Xplained Pro
  *
  * This quick start will receiving eight bytes of data from PC terminal and transmit back the string
  * to the terminal through DMA. In this use case the USART will be configured with the following
--- sam0/drivers/sercom/usart/unit_test/unit_test.c
+++ sam0/drivers/sercom/usart/unit_test/unit_test.c
@@ -75,6 +75,7 @@
  *  - SAM D21 Xplained Pro board
  *  - SAM R21 Xplained Pro board
  *  - SAM L21 Xplained Pro board
+ *  - SAM C21 Xplained Pro board
  *
  * \section appdoc_sam0_usart_unit_test_setup Setup
  * The following connections has to be made using wires:
@@ -82,7 +83,7 @@
  *  - \b TX/RX: EXT1 PIN17 (PA04) <--> EXT1 PIN13 (PB09)
  * - SAM R21 Xplained Pro board
  *  - \b TX/RX: EXT1 PIN9  (PA22) <--> EXT1 PIN15 (PB03)
- * - SAM L21 Xplained Pro board
+ * - SAM L21/C21 Xplained Pro board
  *  - \b TX/RX: EXT2 PIN14 (PA18) <--> EXT1 PIN13 (PB09)
  *
  * To run the test:
