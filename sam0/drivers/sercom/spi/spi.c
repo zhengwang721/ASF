be3329f2f01b1315bd08e40cdd3e4cfa5872a4b4
candy.xiong <Qingtai.Xiong@atmel.com>
2014-12-25 10:53:15 +0800

    [ANPI-4927] fix comment

--- sam0/drivers/sercom/spi/spi.c
+++ sam0/drivers/sercom/spi/spi.c
@@ -515,6 +515,14 @@ enum status_code spi_init(
 		pm_index     = sercom_index + MCLK_APBCMASK_SERCOM0_Pos;
 		gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
 	}
+#elif (SAMC21)
+	if (sercom_index == 5) {
+		pm_index     = MCLK_APBCMASK_SERCOM5_Pos;
+		gclk_index   =  SERCOM5_GCLK_ID_CORE;
+		} else {
+		pm_index     = sercom_index + MCLK_APBCMASK_SERCOM0_Pos;
+		gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
+	}
 #else
 	pm_index     = sercom_index + PM_APBCMASK_SERCOM0_Pos;
 	gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
