57739594c7b106265b2e1426a6ce588a4ffa7fe4
candy.xiong <Qingtai.Xiong@atmel.com>
2015-02-10 14:48:05 +0800

    [ANPI-5087] add AC driver support

--- sam0/drivers/ac/quick_start/samda1_xplained_pro/conf_board.h
+++ sam0/drivers/ac/quick_start/samda1_xplained_pro/conf_board.h
@@ -1,9 +1,9 @@
 /**
  * \file
  *
- * \brief SAM D21 Xplained Pro test configuration.
+ * \brief SAM DA1 Xplained Pro board configuration.
  *
- * Copyright (c) 2013-2014 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,19 +40,11 @@
  * \asf_license_stop
  *
  */
- /**
+/*
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 
-#ifndef CONF_TEST_H_INCLUDED
-#define CONF_TEST_H_INCLUDED
+#ifndef CONF_BOARD_H_INCLUDED
+#define CONF_BOARD_H_INCLUDED
 
-#define CONF_STDIO_USART          EDBG_CDC_MODULE
-#define CONF_STDIO_MUX_SETTING    EDBG_CDC_SERCOM_MUX_SETTING
-#define CONF_STDIO_PINMUX_PAD0    EDBG_CDC_SERCOM_PINMUX_PAD0
-#define CONF_STDIO_PINMUX_PAD1    EDBG_CDC_SERCOM_PINMUX_PAD1
-#define CONF_STDIO_PINMUX_PAD2    EDBG_CDC_SERCOM_PINMUX_PAD2
-#define CONF_STDIO_PINMUX_PAD3    EDBG_CDC_SERCOM_PINMUX_PAD3
-#define CONF_STDIO_BAUDRATE       38400
-
-#endif /* CONF_TEST_H_INCLUDED */
+#endif /* CONF_BOARD_H_INCLUDED */
--- sam0/drivers/ac/unit_test/samda1j18a_samda1_xplained_pro/conf_test.h
+++ sam0/drivers/ac/unit_test/samda1j18a_samda1_xplained_pro/conf_test.h
@@ -1,9 +1,9 @@
 /**
  * \file
  *
- * \brief SAM D21 Xplained Pro test configuration.
+ * \brief SAM DA1 Xplained Pro test configuration.
  *
- * Copyright (c) 2013-2014 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,7 +40,7 @@
  * \asf_license_stop
  *
  */
- /**
+/*
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 
