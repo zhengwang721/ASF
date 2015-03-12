d692379a7a10674ebecaf9bf6bf6276e9b7b5176
Lina Zhao <lina.zhao@SHALT0320.corp.atmel.com>
2015-03-12 17:35:38 +0800

    updating adc2 to adc4 to avoid connection conflict with AC unit_test. No problems with samd20 or saml21

--- sam0/drivers/adc/unit_test/unit_test.c
+++ sam0/drivers/adc/unit_test/unit_test.c
@@ -79,7 +79,7 @@
  *
  * \section asfdoc_sam0_adc_unit_test_setup Setup
  * The following connections has to be made using wires:
- *  - \b DAC VOUT (PA02) <-----> ADC2 (PB08)
+ *  - \b DAC VOUT (PA02) <-----> ADC4 (PA04)
  *
  * To run the test:
  *  - Connect the supported Xplained Pro board to the computer using a
@@ -230,7 +230,7 @@ static void run_adc_init_test(const struct test_case *test)
 	/* Structure for ADC configuration */
 	struct adc_config config;
 	adc_get_config_defaults(&config);
-	config.positive_input = ADC_POSITIVE_INPUT_PIN2;
+	config.positive_input = ADC_POSITIVE_INPUT_PIN4;
 	config.negative_input = ADC_NEGATIVE_INPUT_GND;
 #if (SAML21)
 	config.reference      = ADC_REFERENCE_INTREF;
@@ -443,7 +443,7 @@ static void setup_adc_average_mode_test(const struct test_case *test)
 	adc_disable(&adc_inst);
 	struct adc_config config;
 	adc_get_config_defaults(&config);
-	config.positive_input     = ADC_POSITIVE_INPUT_PIN2;
+	config.positive_input     = ADC_POSITIVE_INPUT_PIN4;
 	config.negative_input     = ADC_NEGATIVE_INPUT_GND;
 #if (SAML21)
 	config.reference          = ADC_REFERENCE_INTREF;
@@ -522,7 +522,7 @@ static void setup_adc_window_mode_test(const struct test_case *test)
 	adc_disable(&adc_inst);
 	struct adc_config config;
 	adc_get_config_defaults(&config);
-	config.positive_input = ADC_POSITIVE_INPUT_PIN2;
+	config.positive_input = ADC_POSITIVE_INPUT_PIN4;
 	config.negative_input = ADC_NEGATIVE_INPUT_GND;
 #if (SAML21)
 	config.reference      = ADC_REFERENCE_INTREF;
