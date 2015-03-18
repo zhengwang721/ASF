b2819a20d33dccbfd862cb262eb9f8f4d13759dc
sheng.hu <sheng.hu@atmel.com>
2015-03-18 17:58:46 +0800

    [ANPI-5210] Update appnote for WDT module.

--- sam0/drivers/wdt/wdt.h
+++ sam0/drivers/wdt/wdt.h
@@ -438,10 +438,7 @@ void wdt_reset_count(void);
  *		<th>Changelog</th>
  *	</tr>
  *	<tr>
- *		<td>Add support for SAML21</td>
- *	</tr>
- *	<tr>
- *		<td>Add SAMD21 support and driver updated to follow driver type convention:
+ *		<td>Driver updated to follow driver type convention:
  *             \li wdt_init, wdt_enable, wdt_disable functions removed
  *             \li wdt_set_config function added
  *             \li WDT module enable state moved inside the configuration struct </td>
