6b4b9add3b4c48ca600fac2fb9e1cb6668ed9fcb
sheng.hu <sheng.hu@atmel.com>
2015-03-19 16:10:04 +0800

    [ANPI-5204] Update doc after review.

--- sam0/drivers/extint/extint.h
+++ sam0/drivers/extint/extint.h
@@ -675,7 +675,7 @@ static inline void extint_nmi_clear_detected(
  *  </tr>
  *  <tr>
  *      <td>E</td>
- *      <td>04/2014</td>
+ *      <td>04/2015</td>
  *      <td>Added support for SAML21 and SAMDA0/DA1.</td>
  *  </tr>
  *  <tr>
