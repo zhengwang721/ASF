ec57b79cae769bfc77f1b05af83d6b07369ef4be
Abirami Rengaiyan <abirami.rengaiyan@atmel.com>
2015-04-08 23:10:32 +0530

    Changed the Firmware version

--- thirdparty/wireless/avr2025_mac/apps/tal/performance_analyzer_2/inc/app_init.h
+++ thirdparty/wireless/avr2025_mac/apps/tal/performance_analyzer_2/inc/app_init.h
@@ -74,7 +74,7 @@
 
 /* === Macros =============================================================== */
 /* Version of the software */
-#define FIRMWARE_VERSION    3.0f
+#define FIRMWARE_VERSION    1.3
 
 
 
