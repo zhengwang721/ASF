6772a6ee1ac2d879553dcc228659d17939cf9408
E, Mathivanan <Mathivanan.E@atmel.com>
2015-04-16 12:36:11 +0530

    Adding BLE-SDK Source Code.

---
+++ thirdparty/wireless/ble_sdk_lib/ble_stack/external_host/interface.h
@@ -0,0 +1,96 @@
+#ifndef __INTERFACE_H__
+#define __INTERFACE_H__
+
+#include "event.h"
+
+#define INTERFACE_HDR_LENGTH		9
+
+#define INTERFACE_API_PKT_ID			0x05
+
+#define INTERFACE_SEND_BUF_MAX 	600
+#define INTERFACE_RCV_BUFF_LEN 500
+
+extern uint8_t interface_send_msg[INTERFACE_SEND_BUF_MAX];
+
+#define INTERFACE_MSG_INIT(msg_id, dest_id) \
+do{\
+	uint16_t __idx = INTERFACE_HDR_LENGTH;\
+	uint8_t* __ptr = NULL;\
+	bool timeout = false;\
+	interface_send_msg[0] = (INTERFACE_API_PKT_ID);\
+	interface_send_msg[1] = ((msg_id) & 0x00FF );\
+	interface_send_msg[2] = (((msg_id)>>8) & 0x00FF );\
+	interface_send_msg[3] = ((dest_id) & 0x00FF );\
+	interface_send_msg[4] = (((dest_id)>>8) & 0x00FF );\
+	interface_send_msg[5] = ((TASK_EXTERN) & 0x00FF );\
+	interface_send_msg[6] = (((TASK_EXTERN)>>8) & 0x00FF );\
+
+#define INTERFACE_PACK_ARG_UINT8(arg)\
+	interface_send_msg[__idx++] = (arg)
+
+#define INTERFACE_PACK_ARG_UINT16(arg)\
+	interface_send_msg[__idx++] = ((arg) & 0x00FF);\
+	interface_send_msg[__idx++] = (((arg) >> 8) & 0x00FF)
+
+#define INTERFACE_PACK_ARG_UINT32(arg) \
+	interface_send_msg[__idx++] = (uint8_t)((arg) & 0x00FF );\
+	interface_send_msg[__idx++] = (uint8_t)(( (arg) >> 8) & 0x00FF) ;\
+	interface_send_msg[__idx++] = (uint8_t)(( (arg) >> 16) & 0x00FF);\
+	interface_send_msg[__idx++] = (uint8_t)(( (arg) >> 24) & 0x00FF)
+
+#define INTERFACE_PACK_ARG_BLOCK(ptr,len)\
+	memcpy(interface_send_msg + __idx, ptr, len);\
+	__idx += len
+		
+#define INTERFACE_PACK_ARG_DUMMY(len)\
+	__idx += len
+
+#define INTERFACE_SEND_NO_WAIT()\
+	interface_send_msg[7] = ((__idx - INTERFACE_HDR_LENGTH) & 0x00FF );\
+	interface_send_msg[8] = (((__idx - INTERFACE_HDR_LENGTH)>>8) & 0x00FF);\
+	interface_send(interface_send_msg, __idx)
+
+#define INTERFACE_SEND_WAIT(msg, src)\
+	watched_event.msg_id = msg;\
+	watched_event.src_id = src;\
+	interface_send_msg[7] = ((__idx - INTERFACE_HDR_LENGTH) & 0x00FF );\
+	interface_send_msg[8] = (((__idx - INTERFACE_HDR_LENGTH)>>8) & 0x00FF);\
+	interface_send(interface_send_msg, __idx);\
+	platform_cmd_cmpl_wait(&timeout);\
+	if(timeout){return AT_BLE_FAILURE;}\
+	__idx = 0;\
+	__ptr = watched_event.params;
+
+#define INTERFACE_UNPACK_INIT(ptr)\
+do{\
+	uint8_t* __ptr = (uint8_t*)(ptr);\
+	uint32_t __idx = 0;
+
+#define INTERFACE_UNPACK_UINT8(ptr)\
+	*((uint8_t*)(ptr)) = __ptr[__idx++]
+
+#define INTERFACE_UNPACK_UINT16(ptr)\
+	*((uint16_t*)(ptr)) = __ptr[__idx]\
+		| (__ptr[__idx + 1] << 8);\
+		__idx += 2
+
+#define INTERFACE_UNPACK_UINT32(ptr)\
+	*((uint32_t*)(ptr)) = __ptr[__idx] \
+		| (__ptr[__idx + 1] << 8) \
+		| (__ptr[__idx + 2] << 16)\
+		| (__ptr[__idx + 3] << 24);\
+		__idx += 4
+
+#define INTERFACE_UNPACK_BLOCK(ptr, len)\
+	memcpy(ptr, __ptr + __idx, len);\
+	__idx += len;
+
+#define INTERFACE_UNPACK_SKIP(len)\
+		__idx += (len)
+
+#define INTERFACE_DONE()\
+}while(0)
+
+void interface_send(uint8_t* msg, uint16_t u16TxLen);
+
+#endif /* HCI_H_ */
---
+++ thirdparty/wireless/ble_sdk_lib/ble_stack/include/event.h
@@ -0,0 +1,19 @@
+#ifndef __EVENT_H__
+#define __EVENT_H__
+
+struct str_watched_event{
+	uint16_t msg_id;
+	uint16_t src_id;
+	uint8_t* params;
+};
+
+extern struct str_watched_event watched_event;
+
+void event_init(void);
+
+void event_post(uint16_t msg_id, uint16_t src_id, void * data);
+
+uint32_t special_events_handler(uint16_t msg_id, uint16_t src_id, uint8_t* data);
+
+
+#endif
