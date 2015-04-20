1e301a875e2b343158b6f7a091c8ee79120f51a9
selvamegala.s <selvamegala.s@atmel.com>
2015-04-20 11:14:11 +0530

    jenkins fixes

--- thirdparty/wireless/addons/ota/upgrade/client_upgrade.h
+++ thirdparty/wireless/addons/ota/upgrade/client_upgrade.h
@@ -3,7 +3,7 @@
  *
  * \brief Over-the-air client module implementation
  *
- * Copyright (C) 2014, Atmel Corporation. All rights reserved.
+ * Copyright (C) 2015, Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
