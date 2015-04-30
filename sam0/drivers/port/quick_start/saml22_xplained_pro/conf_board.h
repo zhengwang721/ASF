7c5a5600273c3c1c05e7f1c5cc74baed92a97cec
sheng.hu <sheng.hu@atmel.com>
2015-04-30 14:48:47 +0800

    [ANPI-5313] Add PORT driver support.

--- sam0/drivers/port/quick_start/saml22_xplained_pro/conf_board.h
+++ sam0/drivers/port/quick_start/saml22_xplained_pro/conf_board.h
@@ -1,9 +1,9 @@
 /**
  * \file
  *
- * \brief SAM L21 Xplained Pro board configuration.
+ * \brief SAM L22 Xplained Pro board configuration.
  *
- * Copyright (c) 2014-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
