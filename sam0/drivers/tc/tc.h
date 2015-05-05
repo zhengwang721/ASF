384a5b37ce600c9cc86184a836cf32683d8f8bc3
Nash Zhang <nash.zhang@atmel.com>
2015-05-05 09:10:28 +0800

    [ANPI-5250]Fix for TC header file update.

--- sam0/drivers/tc/tc.h
+++ sam0/drivers/tc/tc.h
@@ -473,6 +473,9 @@
 #  define FEATURE_TC_READ_SYNC
 /** IO pin edge capture*/
 #  define FEATURE_TC_IO_CAPTURE
+#endif
+
+#if (SAML21XXXB) || defined(__DOXYGEN__)
 /** Generate DMA triggers*/
 #  define FEATURE_TC_GENERATE_DMA_TRIGGER
 #endif
@@ -1448,7 +1451,7 @@ static inline void tc_dma_trigger_command(
 	}
 
 	/* Write command to execute */
-	tc_module->CTRLBSET.reg = TC_CTRLBSET_CMD(TC_CTRLBSET_CMD_DMATRG_Val);
+	tc_module->CTRLBSET.reg = TC_CTRLBSET_CMD(TC_CTRLBSET_CMD_DMAOS_Val);
 }
 /** @} */
 #endif
