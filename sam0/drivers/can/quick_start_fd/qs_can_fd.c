31e0eb78efa9d5f2d8edff5bb300a4461ef2dd7f
“zheng.wang” <zheng.wang@atmel.com>
2015-05-28 17:11:01 +0800

    [ANPI-5592] Change the CAN example clock to use DPLL with XOSC32K.

--- sam0/drivers/can/quick_start_fd/qs_can_fd.c
+++ sam0/drivers/can/quick_start_fd/qs_can_fd.c
@@ -131,6 +131,8 @@ static void configure_can(void)
 
 	/* Enable interrupts for this CAN module */
 	system_interrupt_enable(SYSTEM_INTERRUPT_MODULE_CAN0);
+	can_enable_interrupt(&can_instance, CAN_PROTOCAL_ERROR_ARBITRATION
+			| CAN_PROTOCAL_ERROR_DATA);
 }
 //! [can_init_setup]
 
@@ -321,6 +323,12 @@ void CAN0_Handler(void)
 		printf("\r\n\r\n");
 	}
 
+	if ((status & CAN_PROTOCAL_ERROR_ARBITRATION)
+			|| (status & CAN_PROTOCAL_ERROR_DATA)) {
+		can_clear_interrupt_status(&can_instance, CAN_PROTOCAL_ERROR_ARBITRATION
+				| CAN_PROTOCAL_ERROR_DATA);
+		printf("Protocal error, please double check the clock in two boards. \r\n\r\n");
+	}
 }
 //! [can_interrupt_handler]
 
