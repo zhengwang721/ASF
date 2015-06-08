5f6809cba35956db0164c4cbab8288251603c677
Ingrid Chen <ingrid.chen@atmel.com>
2015-06-08 15:05:38 +0800

    [ANPI-5619] SAMC21 - Update appnote for DAC module (fix comments)

--- sam0/drivers/dac/dac_sam_d_c/dac_feature.h
+++ sam0/drivers/dac/dac_sam_d_c/dac_feature.h
@@ -581,8 +581,6 @@ void dac_chan_disable_output_buffer(
  * \endif
  * \if DAC_DMA_USE_MODE_SELECTION
  *  - \subpage asfdoc_sam0_adc_dma_use_case_dac_in_dma
- * \else
- *  - \subpage asfdoc_sam0_adc_dma_use_case
  * \endif
  *
  * \if DAC_DMA_USE_MODE_SELECTION
