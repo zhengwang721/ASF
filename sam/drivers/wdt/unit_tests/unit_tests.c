eff49855595ba20878f90f1a39af528632b4491c
dongsheng.li <dongsheng.li@atmel.com>
2015-06-09 16:44:34 +0800

    no message

--- sam/drivers/wdt/unit_tests/unit_tests.c
+++ sam/drivers/wdt/unit_tests/unit_tests.c
@@ -3,7 +3,7 @@
  *
  * \brief Unit tests for WDT driver.
  *
- * Copyright (c) 2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2011-2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
