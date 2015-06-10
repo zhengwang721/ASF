9c8297e3dea29aa275a6b3f9c516ee3d6a22b3e1
dongsheng.li <dongsheng.li@atmel.com>
2015-06-10 15:27:29 +0800

    uart OK

--- sam/drivers/twihs/twihs_slave_example/samv71q21_samv71_xplained_ultra/conf_uart_serial.h
+++ sam/drivers/twihs/twihs_slave_example/samv71q21_samv71_xplained_ultra/conf_uart_serial.h
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief Serial UART service configuration.
+ * \brief USART Serial Configuration
  *
  * Copyright (C) 2015 Atmel Corporation. All rights reserved.
  *
@@ -44,14 +44,18 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 
-#ifndef CONF_UART_SERIAL_H
-#define CONF_UART_SERIAL_H
+#ifndef CONF_USART_SERIAL_H_INCLUDED
+#define CONF_USART_SERIAL_H_INCLUDED
 
 /** UART Interface */
 #define CONF_UART            CONSOLE_UART
 /** Baudrate setting */
-#define CONF_UART_BAUDRATE   115200
+#define CONF_UART_BAUDRATE   (115200UL)
+/** Character length setting */
+#define CONF_UART_CHAR_LENGTH  US_MR_CHRL_8_BIT
 /** Parity setting */
-#define CONF_UART_PARITY     UART_MR_PAR_NO
+#define CONF_UART_PARITY     US_MR_PAR_NO
+/** Stop bits setting */
+#define CONF_UART_STOP_BITS    US_MR_NBSTOP_1_BIT
 
-#endif/* CONF_UART_SERIAL_H_INCLUDED */
+#endif/* CONF_USART_SERIAL_H_INCLUDED */
