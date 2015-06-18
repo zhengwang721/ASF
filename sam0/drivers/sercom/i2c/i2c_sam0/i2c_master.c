f3f1475735777e398e70f6ab0905158a82229fb3
Jeffy.Zhou <Jianhui.Zhou@atmel.com>
2015-06-18 10:53:07 +0800

    no message

--- sam0/drivers/sercom/i2c/i2c_sam0/i2c_master.c
+++ sam0/drivers/sercom/i2c/i2c_sam0/i2c_master.c
@@ -240,6 +240,7 @@ enum status_code i2c_master_init(
 	
 	uint32_t sercom_index = _sercom_get_sercom_inst_index(module->hw);
 	uint32_t pm_index, gclk_index; 
+#if (SAML21) || (SAMC20) || (SAMC21)
 #if (SAML21)
 	if (sercom_index == 5) {
 		pm_index     = MCLK_APBDMASK_SERCOM5_Pos;
@@ -248,6 +249,10 @@ enum status_code i2c_master_init(
 		pm_index     = sercom_index + MCLK_APBCMASK_SERCOM0_Pos;
 		gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
 	}
+#else
+	pm_index     = sercom_index + MCLK_APBCMASK_SERCOM0_Pos;
+	gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
+#endif
 #else
 	pm_index     = sercom_index + PM_APBCMASK_SERCOM0_Pos;
 	gclk_index   = sercom_index + SERCOM0_GCLK_ID_CORE;
