93d511a0cd740bb3bb247e49e9d12ecd2bab70e1
Irun Walberg <irun.walberg@atmel.com>
2015-07-02 09:27:21 +0200

    updated introduction text to include ARM

--- sam0/drivers/opamp/opamp.h
+++ sam0/drivers/opamp/opamp.h
@@ -50,7 +50,7 @@
 /**
  * \defgroup asfdoc_sam0_opamp_group SAM Operational Amplifier Controller (OPAMP) Driver
  *
- * This driver for Atmel&reg; | SMART SAM devices provides an interface for the configuration
+ * This driver for Atmel&reg; | SMART ARM&reg;-based microcontrollers provides an interface for the configuration
  * and management of the device's Operational Amplifier Controller functionality.
  *
  * The following peripheral is used by this module:
--- sam0/drivers/system/clock/clock_samc20_c21/clock_feature.h
+++ sam0/drivers/system/clock/clock_samc20_c21/clock_feature.h
@@ -50,7 +50,7 @@ extern "C" {
 /**
  * \defgroup asfdoc_sam0_system_clock_group SAM C20/C21 System Clock Management Driver (SYSTEM CLOCK)
  *
- * This driver for Atmel&reg; | SMART SAM C20/C21 devices provides an interface for the configuration
+ * This driver for Atmel&reg; | SMART ARM&reg;-based microcontrollers provides an interface for the configuration
  * and management of the device's clocking related functions. This includes
  * the various clock sources, bus clocks and generic clocks within the device,
  * with functions to manage the enabling, disabling, source selection and
