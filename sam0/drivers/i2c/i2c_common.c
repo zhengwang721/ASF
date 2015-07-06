e332a1051fc111c0a720554afe44107f8f541672
unknown <jeffy.zhou@SHALT0371.corp.atmel.com>
2015-07-06 15:27:49 +0800

    fix copyright.

--- sam0/drivers/i2c/i2c_common.c
+++ sam0/drivers/i2c/i2c_common.c
@@ -3,7 +3,7 @@
  *
  * \brief I2C Driver
  *
- * Copyright (C) 2012-2015 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
