43754b22469241c31863e4a8a5973e67a11c2466
dongsheng.li <dongsheng.li@atmel.com>
2015-08-12 11:11:28 +0800

    Add license in getting_start_hex.h Modefy device support in asf.xml

--- common/components/memory/qspi_flash/s25fl1xx/example/getting_started_hex.h
+++ common/components/memory/qspi_flash/s25fl1xx/example/getting_started_hex.h
@@ -1,3 +1,50 @@
+/**
+ * \file
+ *
+ * \brief QSPI flash memory driver test xip code for S25FL116K.
+ *
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel micro controller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+
+#ifndef _GETTING_START_HEX_H_
+#define _GETTING_START_HEX_H_
+
 uint8_t buffercode[6612] = {
 	0x50, 0x10, 0x44, 0x20, 0xB5, 0x19, 0x00, 0x80, 0x09, 0x15, 0x00, 0x80,
 	0x61, 0x15, 0x00, 0x80, 0x71, 0x15, 0x00, 0x80, 0x7D, 0x15, 0x00, 0x80,
@@ -551,3 +598,5 @@ uint8_t buffercode[6612] = {
 	0xFF, 0xF7, 0x6E, 0xFF, 0x2D, 0x2D, 0x20, 0x25, 0x73, 0x0A, 0x0D, 0x00,
 	0x02, 0x00, 0x00, 0x00, 0x00, 0x09, 0x3D, 0x00, 0x01, 0x01, 0x00, 0x00,
 };
+
+#endif
