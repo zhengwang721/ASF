e2ebcd0ff91c3b39fcc4783fcbb3a1aa7634e8df
Magne Normann <magne.normann@atmel.com>
2015-08-12 12:41:33 +0200

    Changed DAx -> DA1

--- sam0/drivers/sercom/usart/usart.h
+++ sam0/drivers/sercom/usart/usart.h
@@ -68,7 +68,7 @@
  *  - Atmel | SMART SAM R21
  *  - Atmel | SMART SAM D10/D11
  *  - Atmel | SMART SAM L21
- *  - Atmel | SMART SAM DAx
+ *  - Atmel | SMART SAM DA1
  *  - Atmel | SMART SAM C20/C21
  *
  * The outline of this documentation is as follows:
@@ -99,35 +99,35 @@
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_SYNC_SCHEME_V2</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_OVER_SAMPLE</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_HARDWARE_FLOW_CONTROL</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_IRDA</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_LIN_SLAVE</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_COLLISION_DECTION</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_START_FRAME_DECTION</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_IMMEDIATE_BUFFER_OVERFLOW_NOTIFICATION</td>
- *    <td>SAM D21/R21/D10/D11/L21/DAx/C20/C21</td>
+ *    <td>SAM D21/R21/D10/D11/L21/DA1/C20/C21</td>
  *  </tr>
  *  <tr>
  *    <td>FEATURE_USART_RS485</td>
@@ -1442,7 +1442,7 @@ static inline bool lin_master_transmission_status(struct usart_module *const mod
  *	<tr>
  *		<td>42118F</td>
  *		<td>08/2015</td>
- *		<td>Add support for SAM L21, SAM DAx, and SAM C20/C21</td>
+ *		<td>Add support for SAM L21, SAM DA1, and SAM C20/C21</td>
  *	</tr>
  *	<tr>
  *		<td>42118E</td>
