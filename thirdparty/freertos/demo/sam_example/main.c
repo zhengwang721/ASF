c1bc1b18eb4ebb321c3b4a3e05493e7402892c16
frank li <frank.li@atmel.com>
2015-08-12 12:37:36 +0800

    add freertos for samv71 board

--- thirdparty/freertos/demo/sam_example/main.c
+++ thirdparty/freertos/demo/sam_example/main.c
@@ -3,7 +3,7 @@
  *
  * \brief FreeRTOS Real Time Kernel example.
  *
- * Copyright (c) 2012-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -46,13 +46,13 @@
  *
  * \section Purpose
  *
- * The FreeRTOS example will help users how to use FreeRTOS in SAM boards.
- * This basic application shows hwo to create task and get information of
+ * The FreeRTOS example will help users how to use FreeRTOS in SAMV71 boards.
+ * This basic application shows how to create task and get information of
  * created task.
  *
  * \section Requirements
  *
- * This package can be used with SAM boards.
+ * This package can be used with SAMV71 boards.
  *
  * \section Description
  *
@@ -220,6 +220,7 @@ int main(void)
 	printf("-- Freertos Example --\n\r");
 	printf("-- %s\n\r", BOARD_NAME);
 	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+
 
 	/* Create task to monitor processor activity */
 	if (xTaskCreate(task_monitor, "Monitor", TASK_MONITOR_STACK_SIZE, NULL,
