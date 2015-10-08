06224ffbce3a6749e740b867fe249f0675e9ca7f
frank li <frank.li@atmel.com>
2015-10-08 09:52:23 +0800

    SAME70 - Add AES driver support

--- sam/drivers/aes/unit_tests/samv71q21_samv71_xplained_ultra/conf_test.h
+++ sam/drivers/aes/unit_tests/samv71q21_samv71_xplained_ultra/conf_test.h
@@ -51,7 +51,11 @@
 #define CONF_TEST_USART      CONSOLE_UART
 /** Baudrate setting */
 #define CONF_TEST_BAUDRATE   115200
+/** Character length setting */
+#define CONF_TEST_CHAR_LENGTH  US_MR_CHRL_8_BIT
 /** Parity setting */
 #define CONF_TEST_PARITY     UART_MR_PAR_NO
+/** Stop bits setting */
+#define CONF_TEST_STOP_BITS    US_MR_NBSTOP_1_BIT
 
 #endif /* CONF_TEST_H_INCLUDED */
