8c77c67a56755d37386adeef4ef7f573c921548e
dongsheng.li <dongsheng.li@atmel.com>
2015-10-14 11:27:16 +0800

    Fix comments

--- common/services/sleepmgr/example/same70q21_same70_xplained_pro/conf_example.h
+++ common/services/sleepmgr/example/same70q21_same70_xplained_pro/conf_example.h
@@ -48,7 +48,7 @@
 
 /* Refer to the example C file for further documentation. */
 
-//! \note This is the SAMV71-Xplained-Ultra's on-board LED.
+//! \note This is the SAME70-Xplained-Pro's on-board LED.
 #define LED_ACTIVITY_STATUS_PIN   LED0_GPIO
 #define LED_STATUS_ON             IOPORT_PIN_LEVEL_LOW
 #define LED_STATUS_OFF            IOPORT_PIN_LEVEL_HIGH
