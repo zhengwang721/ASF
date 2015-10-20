88d4cac40eb523af489482d6219618836440d484
tonny.lan <shouzhen.lan@atmel.com>
2015-10-20 16:30:41 +0800

    Updated after review

--- sam0/boards/saml22_xplained_pro_b/board_init.c
+++ sam0/boards/saml22_xplained_pro_b/board_init.c
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief SAM L22 Xplained Pro board initialization
+ * \brief SAM L22 Xplained Pro B board initialization
  *
  * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
