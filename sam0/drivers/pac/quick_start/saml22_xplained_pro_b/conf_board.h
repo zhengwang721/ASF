793de800a053b3f922b2be9e0ea92f250cf762f3
unknown <lina.zhao@SHALT0320.corp.atmel.com>
2015-10-27 10:44:41 +0800

    no message

--- sam0/drivers/pac/quick_start/saml22_xplained_pro_b/conf_board.h
+++ sam0/drivers/pac/quick_start/saml22_xplained_pro_b/conf_board.h
@@ -3,7 +3,7 @@
  *
  * \brief SAM L22 Xplained Pro B board configuration.
  *
- * Copyright (c) 2014-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
