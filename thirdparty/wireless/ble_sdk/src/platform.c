3a89d966180f8215482b06786aa65e5b5ae3a7c4
Seetharam Rao <seetharam.rao_h_m@atmel.com>
2015-10-28 13:30:03 +0530

    resolved platform conflict

--- thirdparty/wireless/ble_sdk/src/platform.c
+++ thirdparty/wireless/ble_sdk/src/platform.c
@@ -166,8 +166,9 @@ at_ble_status_t platform_interface_send(uint8_t if_type, uint8_t* data, uint32_t
 		delay_ms(BTLC1000_WAKEUP_DELAY);
 	}
 #endif //ENABLE_POWER_SAVE
+	tx_done = false;
 	serial_drv_send(data, len);	
-
+	while(tx_done == false);
 	return AT_BLE_SUCCESS;
 }
 
@@ -470,11 +471,10 @@ void serial_tx_callback(void)
  {
 	 
  }
- 
+
  void stop_timer(void)
  {
 	 ticks = 0;
  }
-
 
 
