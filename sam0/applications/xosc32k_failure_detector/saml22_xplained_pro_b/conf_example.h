952fa942a3278f00a7fcb7e2170812490ae06b49
lina.zhao <lina.zhao@atmel.com>
2015-10-30 10:07:38 +0800

    no message

--- sam0/applications/xosc32k_failure_detector/saml22_xplained_pro_b/conf_example.h
+++ sam0/applications/xosc32k_failure_detector/saml22_xplained_pro_b/conf_example.h
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief SAM L22 Xplained Pro example configuration.
+ * \brief SAM L22 Xplained Pro B example configuration.
  *
  * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
