e15e1061af17901736d5fd43e446612031ee8ace
Kishore <katna_kishore.kumar>
2015-11-18 18:19:30 +0530

    Fix compilation warnings for __always_inline

--- sam/utils/compiler.h
+++ sam/utils/compiler.h
@@ -156,6 +156,10 @@
  * heuristics and inline the function no matter how big it thinks it
  * becomes.
  */
+#if defined(__CC_ARM) || (defined __GNUC__) || (defined __ICCARM__)
+#undef __always_inline
+#endif
+
 #if defined(__CC_ARM)
 #   define __always_inline   __forceinline
 #elif (defined __GNUC__)
