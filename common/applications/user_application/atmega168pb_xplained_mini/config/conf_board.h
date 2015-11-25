18ac2f77969de143aff9d7dc4f20098f73902fa1
“zheng.wang” <zheng.wang@atmel.com>
2015-11-25 13:52:25 +0800

    [ASFP-5782] Add Atmega 48PB,88PB,168PB devices and 168PB XMINI board support

--- common/applications/user_application/atmega168pb_xplained_mini/config/conf_board.h
+++ common/applications/user_application/atmega168pb_xplained_mini/config/conf_board.h
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief MEGA LED Example
+ * \brief MEGA-168PB Xplained board configuration template
  *
  * Copyright (C) 2015 Atmel Corporation. All rights reserved.
  *
@@ -40,64 +40,11 @@
  * \asf_license_stop
  *
  */
-
-/**
- * \mainpage MEGA LED Example
- * See \ref appdoc_main "here" for project documentation.
- * \copydetails appdoc_preface
- *
- *
- * \page appdoc_preface Overview
- * This application demonstrates a simple example to turn on and off the board LED.
- */
-
-/**
- * \page appdoc_main MEGA LED Example
- *
- * Overview:
- * - \ref appdoc_mega_led_app_intro
- * - \ref appdoc_mega_led_app_usage
- * - \ref appdoc_mega_led_app_compinfo
- * - \ref appdoc_mega_led_app_contactinfo
- *
- * \section appdoc_mega_led_app_intro Introduction
- * This application demonstrates a simple example turn on and off the board LED.
- *
- * This application has been tested on following boards:
- * - ATmega328p Xplained Mini
- * - ATmega328pb Xplained Mini
- * - ATmega168pb Xplained Mini
- *
- * \section appdoc_mega_led_app_usage Usage
- * The application uses button to control the LED, 
- * once the button is pressed, LED0 will turn on
- * once the button is released, LED0 will turn off
- *
- * \section appdoc_mega_led_app_compinfo Compilation Info
- * This software was written for the GNU GCC and IAR for MEGA.
- * Other compilers may or may not work.
- *
- * \section appdoc_mega_led_app_contactinfo Contact Information
- * For further information, visit
- * <a href="http://www.atmel.com">http://www.atmel.com</a>.
- */
 /*
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 
-#include <asf.h>
-int main (void)
-{
-	/* set board io port */
-	board_init();
-	bool button_state;
-	while(1){
-		button_state = ioport_get_pin_level(GPIO_PUSH_BUTTON_0);
-		if(button_state){
-			LED_Off(LED0);
-		}else{
-			LED_On(LED0);
-		}
-	}
-	
-}
+#ifndef CONF_BOARD_H
+#define CONF_BOARD_H
+
+#endif // CONF_BOARD_H
