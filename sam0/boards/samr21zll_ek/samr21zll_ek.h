49d1fd9ea917100a418ccfe5c5d47e8ee9fb6bbe
tonny.lan <shouzhen.lan@atmel.com>
2015-11-27 11:41:43 +0800

    Add 16MHz

--- sam0/boards/samr21zll_ek/samr21zll_ek.h
+++ sam0/boards/samr21zll_ek/samr21zll_ek.h
@@ -78,8 +78,8 @@ void system_board_init(void);
  *  @{ */
 #define BOARD_FREQ_SLCK_XTAL      (32768U)
 #define BOARD_FREQ_SLCK_BYPASS    (32768U)
-#define BOARD_FREQ_MAINCK_XTAL    0 /* Not Mounted */
-#define BOARD_FREQ_MAINCK_BYPASS  0 /* Not Mounted */
+#define BOARD_FREQ_MAINCK_XTAL    (16000000)
+#define BOARD_FREQ_MAINCK_BYPASS  (16000000)
 #define BOARD_MCK                 CHIP_FREQ_CPU_MAX
 #define BOARD_OSC_STARTUP_US      15625
 /** @} */
