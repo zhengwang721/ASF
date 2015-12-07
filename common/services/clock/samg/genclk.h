11810feb0d822ec3a45bd6f0e9b557457f6942ad
dongsheng.li <dongsheng.li@atmel.com>
2015-12-07 14:29:51 +0800

    no message

--- common/services/clock/samg/genclk.h
+++ common/services/clock/samg/genclk.h
@@ -104,6 +104,7 @@ enum genclk_source {
 //@{
 
 enum genclk_divider {
+#if SAMG55
 	GENCLK_PCK_PRES_1  = PMC_PCK_PRES(0), //!< Set PCK clock prescaler to 1
 	GENCLK_PCK_PRES_2  = PMC_PCK_PRES(1), //!< Set PCK clock prescaler to 2
 	GENCLK_PCK_PRES_4  = PMC_PCK_PRES(2), //!< Set PCK clock prescaler to 4
@@ -111,6 +112,15 @@ enum genclk_divider {
 	GENCLK_PCK_PRES_16 = PMC_PCK_PRES(4), //!< Set PCK clock prescaler to 16
 	GENCLK_PCK_PRES_32 = PMC_PCK_PRES(5), //!< Set PCK clock prescaler to 32
 	GENCLK_PCK_PRES_64 = PMC_PCK_PRES(6), //!< Set PCK clock prescaler to 64
+#else
+	GENCLK_PCK_PRES_1  = PMC_MCKR_PRES_CLK_1, //!< Set PCK clock prescaler to 1
+	GENCLK_PCK_PRES_2  = PMC_MCKR_PRES_CLK_2, //!< Set PCK clock prescaler to 2
+	GENCLK_PCK_PRES_4  = PMC_MCKR_PRES_CLK_4, //!< Set PCK clock prescaler to 4
+	GENCLK_PCK_PRES_8  = PMC_MCKR_PRES_CLK_8, //!< Set PCK clock prescaler to 8
+	GENCLK_PCK_PRES_16 = PMC_MCKR_PRES_CLK_16, //!< Set PCK clock prescaler to 16
+	GENCLK_PCK_PRES_32 = PMC_MCKR_PRES_CLK_32, //!< Set PCK clock prescaler to 32
+	GENCLK_PCK_PRES_64 = PMC_MCKR_PRES_CLK_64, //!< Set PCK clock prescaler to 64
+#endif
 };
 
 //@}
