0bdc3ce7420eda6f8de6cea85481d4b2b640a192
frank.li <Frank.li@atmel.com>
2016-01-05 15:49:58 +0800

    Modified \note

--- sam0/drivers/sercom/i2c/i2c_common.h
+++ sam0/drivers/sercom/i2c/i2c_common.h
@@ -136,8 +136,13 @@ extern "C" {
  *		<td>SAM D21/R21/D10/D11/L21/L22/DA1/C20/C21</td>
  *	</tr>
  * </table>
- * \note The specific features are only available in the driver when the
- * selected device supports those features.
+ * \note The specific features are only available in the driver when the selected
+ * device supports those features.When using the I2C high-speed mode for off-board communication,
+ * there are various high frequency interference, which can lead to distortion of the signals 
+ * and communication failure. When using Xplained Pro boards in order to test I2C high-speed 
+ * communication, the following recommendation should be followed:
+ * - Use the SDA-line on PA08 and SCL-line on PA09 for both boards. This will provide stronger pull-ups on both SDA and SCL.
+ * - The SCL should not be higher than 1.5MHz.
  *
  * \subsection asfdoc_sam0_sercom_i2c_functional_desc Functional Description
  * The I<SUP>2</SUP>C provides a simple two-wire bidirectional bus consisting of a
@@ -513,12 +518,7 @@ enum i2c_transfer_direction {
  * applications for \ref asfdoc_sam0_sercom_i2c_group. QSGs are simple examples with
  * step-by-step instructions to configure and use this driver in a selection of
  * use cases. Note that a QSG can be compiled as a standalone application or be
- * added to the user application. When using the I2C high-speed mode for off-board communication,
- * there are various high frequency interference, which can lead to distortion of the signals 
- * and communication failure. When using Xplained Pro boards in order to test I2C high-speed 
- * communication, the following recommendation should be followed:
- * - Use the SDA-line on PA08 and SCL-line on PA09 for both boards. This will provide stronger pull-ups on both SDA and SCL.
- * - The SCL should not be higher than 1.5MHz. 
+ * added to the user application.  
  *
  * \if I2C_MASTER_MODE
  * - \subpage asfdoc_sam0_sercom_i2c_master_basic_use_case "Quick Start Guide for the I2C Master module - Basic Use Case"
