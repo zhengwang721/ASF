95284e3b5232c88b1638728c7c059f27b0bf281d
Park, Kevin <kevin.park@atmel.com>
2016-01-12 12:24:47 +0900

    Merge HTP of "BluSDK5.0" branch into "BLU-SDK-SMART_v5.0" branch.

--- thirdparty/wireless/ble_smart_sdk/apps/htpt_temperature_sensor/htpt_app.h
+++ thirdparty/wireless/ble_smart_sdk/apps/htpt_temperature_sensor/htpt_app.h
@@ -40,103 +40,88 @@
  * \asf_license_stop
  *
  */
-
 /*
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
+
 
 #ifndef __HTPT_APP_H__
 #define __HTPT_APP_H__
 
 #include "at_ble_api.h"
-#include "stddef.h"     /* standard definition */
-#include "stdarg.h"
-#include "stdio.h"
 
-#define DBG_LOG_CONT     printf
+#define DBG_LOG_CONT	 printf
 
-#define DBG_LOG              printf("\r\n"); \
-	printf
-#define DEV_LOG_ENABLE
+#define DBG_LOG		     printf("\r\n");\
+						 printf
 
-#ifdef DEV_LOG_ENABLE
-#define DBG_LOG_DEV(...)        printf(__VA_ARGS__); \
-	print_log("\r\n");
-#else
-#define DBG_LOG_DEV             ALL_UNUSED
-#endif
+#define APP_HT_FAST_ADV 1600 //1000 ms
 
-#define APP_HT_FAST_ADV 100 /* 100 ms */
-
-#define APP_HT_ADV_TIMEOUT 1000 /* 100 Secs */
+#define APP_HT_ADV_TIMEOUT 1000 // 100 Secs
 
 #define SCAN_RESP_LEN 10
 #define ADV_DATA_LEN 18
 
 #define ADV_TYPE_LEN (0x01)
 
-#define HT_ADV_DATA_UUID_LEN     (2)
-#define HT_ADV_DATA_UUID_TYPE    (0x03)
-#define HT_ADV_DATA_UUID_DATA    "\x09\x18"
+#define HT_ADV_DATA_UUID_LEN	 (2)
+#define HT_ADV_DATA_UUID_TYPE	 (0x03)
+#define HT_ADV_DATA_UUID_DATA	 "\x09\x18"
 
-#define HT_ADV_DATA_APPEARANCE_LEN       (2)
-#define HT_ADV_DATA_APPEARANCE_TYPE      (0x19)
-#define HT_ADV_DATA_APPEARANCE_DATA      "\x00\x03"
+#define HT_ADV_DATA_APPEARANCE_LEN	 (2)
+#define HT_ADV_DATA_APPEARANCE_TYPE	 (0x19)
+#define HT_ADV_DATA_APPEARANCE_DATA	 "\x00\x03"
 
-#define HT_ADV_DATA_NAME_LEN     (9)
-#define HT_ADV_DATA_NAME_TYPE    (0x09)
-#define HT_ADV_DATA_NAME_DATA    "ATMEL-HTP"
+#define HT_ADV_DATA_NAME_LEN	 (9)
+#define HT_ADV_DATA_NAME_TYPE	 (0x09)
+#define HT_ADV_DATA_NAME_DATA	 "ATMEL-HTP"
 
 /* Typedef for health thermometer profile -  application */
-typedef struct htp_app {
+typedef struct htp_app{	
+	
 	/* Measured temperature value. Value may be Cecilius /Fahrenheit */
-	uint32_t temperature;
-
+	uint32_t temperature;	
+	
 	/* Temperature type string */
 	at_ble_htpt_temp_type temperature_type;
-
+	
 	/* Measurement Interval */
 	uint16_t measurement_interval;
-
+	
 	/* Minimum measurement interval */
 	uint16_t min_measurement_intv;
-
+	
 	/* Maximum measurement interval */
 	uint16_t max_meaurement_intv;
-
+	
 	/* Security Level */
 	at_ble_htpt_sec_level security_lvl;
-
+	
 	/* Optional Features */
 	at_ble_htpt_db_config_flag optional;
-
+	
 	at_ble_htpt_temp_flags flags;
-} htp_app_t;
+}htp_app_t;
 
 /**@brief Temperature measurement stability type
- */
-typedef enum {
-	UNSTABLE_TEMPERATURE_VAL = 0,
-	STABLE_TEMPERATURE_VAL = 1
-} stable_temp_reading;
-
-#define IEEE11073_EXPONENT                              (0xFF000000)
-
-#define IEEE754_MANTISA(val)                    ((uint32_t)(val * 10))
-
-#define IEEE754_TO_IEEE11073_FLOAT(f_val)       (IEEE11073_EXPONENT | IEEE754_MANTISA(f_val))
-
-/* Converting floating point IEEE754 format to floating point IEEE11073 format
- *
- * @Param[0] f_val floating point IEEE754 format
- * @return floating point IEEE11073
- *
- */
-static inline uint32_t convert_ieee754_ieee11073_float(float f_val)
+*/
+typedef enum
 {
-	uint32_t ieee11073_float;
-	ieee11073_float = IEEE754_TO_IEEE11073_FLOAT(f_val);
-	return (ieee11073_float);
-}
+	UNSTABLE_TEMPERATURE_VAL= 0,
+	STABLE_TEMPERATURE_VAL=1
+}stable_temp_reading;
 
+static at_ble_status_t app_connected_event_handler(void *params);
+static at_ble_status_t app_disconnected_event_handler(void *params);
+static at_ble_status_t app_pair_done_event_handler(void *params);
+static at_ble_status_t app_encryption_status_changed_handler(void *params);
+static at_ble_status_t  app_htpt_create_db_cfm_handler(void *params) ;
+static at_ble_status_t  app_htpt_error_ind_handler(void *params);
+static at_ble_status_t  app_htpt_disable_ind_handler(void *params);
+static at_ble_status_t  app_htpt_temp_send_cfm(void *params);
+static at_ble_status_t  app_htpt_meas_intv_chg_ind_handler(void *params);
+static at_ble_status_t  app_htpt_cfg_indntf_ind_handler(void *params);
+static at_ble_status_t  app_htptp_meas_intv_chg_req(void *params);
+static at_ble_status_t  app_htpt_enable_rsp_handler(void *params);
+static at_ble_status_t  app_htpt_meas_intv_upd_rsp(void *params);
 #endif /* __HTPT_APP_H__ */
