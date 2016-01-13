5f98bbc671e098ff718ba94e7d1d73a49c81bae4
Jeffy.Zhou <Jianhui.Zhou@atmel.com>
2016-01-13 10:14:57 +0800

    Update copyright.

--- sam0/drivers/system/clock/clock_samc20_c21/gclk.c
+++ sam0/drivers/system/clock/clock_samc20_c21/gclk.c
@@ -3,7 +3,7 @@
  *
  * \brief SAM C2x Generic Clock Driver
  *
- * Copyright (C) 2015 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2016 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
