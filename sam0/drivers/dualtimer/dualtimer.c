8fd14ff99d561b19610c7d217bace3c7a18db7cd
Jeffy.Zhou <Jianhui.Zhou@atmel.com>
2016-01-13 13:14:29 +0800

    Update copyright.

--- sam0/drivers/dualtimer/dualtimer.c
+++ sam0/drivers/dualtimer/dualtimer.c
@@ -3,7 +3,7 @@
  *
  * \brief SAM DUALTIMER Driver for SAMB11
  *
- * Copyright (C) 2016 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2015-2016 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
