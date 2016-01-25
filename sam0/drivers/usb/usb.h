1b34b2c3e1acf567f80b5bd6dcc49a3d7041ded7
dongsheng.li <dongsheng.li@atmel.com>
2016-01-25 14:37:24 +0800

    no message

--- sam0/drivers/usb/usb.h
+++ sam0/drivers/usb/usb.h
@@ -294,7 +294,7 @@ struct usb_module {
 	/** Bit mask for device callbacks enabled */
 	uint16_t device_enabled_callback_mask;
 	/** Bit mask for device endpoint callbacks registered */
-	uint8_t deivce_endpoint_registered_callback_mask[USB_EPT_NUM];
+	uint8_t device_endpoint_registered_callback_mask[USB_EPT_NUM];
 	/** Bit mask for device endpoint callbacks enabled */
 	uint8_t device_endpoint_enabled_callback_mask[USB_EPT_NUM];
 };
