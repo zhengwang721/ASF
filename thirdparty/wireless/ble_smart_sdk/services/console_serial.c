0df20486d9714251dbfbbe9efc6f9c641fa99d70
pillarseternity <alex.shin@atmel.com>
2016-02-01 15:59:25 +0900

    added function (int getchar_b11_timeout(uint32_t sed ))

--- thirdparty/wireless/ble_smart_sdk/services/console_serial.c
+++ thirdparty/wireless/ble_smart_sdk/services/console_serial.c
@@ -6,7 +6,11 @@
 #include "console_serial.h"
 
 struct uart_module uart_instance;
+struct uart_config config_uart;
+
 volatile static bool read_complete_flag = false;
+uint8_t string_input[1] = {0};
+uint32_t tick =0 ;
 
 static void uart_read_complete_callback(struct uart_module *const module)
 {
@@ -15,8 +19,6 @@ static void uart_read_complete_callback(struct uart_module *const module)
 
 void serial_console_init(void)
 {
-	struct uart_config config_uart;
-
 	system_clock_config(CLOCK_RESOURCE_XO_26_MHZ, CLOCK_FREQ_26_MHZ);
 
 	uart_get_config_defaults(&config_uart);
@@ -34,12 +36,47 @@ void serial_console_init(void)
 
 	stdio_serial_init(&uart_instance, CONF_STDIO_USART_MODULE, &config_uart);
 }
+		   
+void time_start(uint32_t sec)
+{
+	system_clock_get_value();
+	uint32_t main_clk = system_clock_get_value();
+
+	tick = (((double)(main_clk)) * ((double)0.0000493)) * (sec * 1000);
+	printf ( "tick count : %u %u", tick, main_clk);
+}
+
+uint32_t time_done()
+{
+	return --tick;
+}
+
+int getchar_b11_timeout(uint32_t sec)
+{
+	
+	read_complete_flag = false;
+	time_start(sec);
+	
+	string_input[0] = 0;	
+	uart_register_callback(&uart_instance, uart_read_complete_callback, UART_RX_COMPLETE);
+	uart_enable_callback(&uart_instance, UART_RX_COMPLETE);
+	uart_read_buffer_job(&uart_instance, string_input, sizeof(string_input));
+	
+	while (!read_complete_flag && time_done()>0 );
+	
+	if( tick == 0 )
+	{	
+		uart_unregister_callback(&uart_instance, UART_RX_COMPLETE);
+		uart_disable_callback(&uart_instance, UART_RX_COMPLETE);	
+	}
+	return string_input[0];	
+}
 
 int getchar_b11(void)
 {
-	uint8_t string_input[1];
 	read_complete_flag = false;
 	
+	string_input[0] = 0;
 	uart_register_callback(&uart_instance, uart_read_complete_callback, UART_RX_COMPLETE);
 	uart_enable_callback(&uart_instance, UART_RX_COMPLETE);
 	uart_read_buffer_job(&uart_instance, string_input, sizeof(string_input));
