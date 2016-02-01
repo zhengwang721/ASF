fe24b4f9d795e7a539ed0c7f452375d8ff51224b
pillarseternity <alex.shin@atmel.com>
2016-02-01 17:32:57 +0900

    removed  warnning messages

--- thirdparty/wireless/ble_smart_sdk/services/console_serial.h
+++ thirdparty/wireless/ble_smart_sdk/services/console_serial.h
@@ -6,7 +6,7 @@
 
 void serial_console_init(void);
 int getchar_b11(void);
-int getchar_b11_timeout(uint32_t sec);
-void getchar_aysnc(uart_callback_t callback_func, uint8_t * input);
+int getchar_b11_timeout(unsigned int sec);
+void getchar_aysnc(uart_callback_t callback_func, unsigned char * input);
 
 #endif /* COSOLE_SERIAL_H */
