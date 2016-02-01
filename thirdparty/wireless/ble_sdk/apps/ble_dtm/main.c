34dc783f85d598572ff518057ed029b367338144
mathivanan.e <mathivanan.e@atmel.com>
2016-02-01 12:08:34 +0530

    DTM IAR Build issue Fixed

--- thirdparty/wireless/ble_sdk/apps/ble_dtm/main.c
+++ thirdparty/wireless/ble_sdk/apps/ble_dtm/main.c
@@ -123,7 +123,6 @@ static void ble_init(void)
 	platform_init(pf_cfg.bus_info.bus_type, pf_cfg.bus_info.bus_flow_control_enabled);
 	
 	/*Trace Logs*/
-	trace_register_printFn((void *)&printf);
 	trace_set_level(TRACE_LVL_DISABLE);
 	
 	/* Initialize BLE device */
