7464c8c32088ac06311140dc7cf4625e02268990
frank.fang <shengchang.fang@atmel.com>
2016-06-13 17:06:14 +0800

    update 2015 -> 2016

--- common/services/freertos/sam/freertos_twi_master.c
+++ common/services/freertos/sam/freertos_twi_master.c
@@ -3,7 +3,7 @@
  *
  * \brief FreeRTOS Peripheral Control API For the TWI
  *
- * Copyright (c) 2012-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2012-2016 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
