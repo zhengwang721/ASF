9698dad2029491d5c16c156a7a9cfc6e8cd2330c
tonny.lan <shouzhen.lan@atmel.com>
2016-06-15 14:02:08 +0800

    SAML21 Update DMA examples

--- sam0/drivers/sercom/i2c/quick_start_slave_dma/qs_i2c_slave_dma.c
+++ sam0/drivers/sercom/i2c/quick_start_slave_dma/qs_i2c_slave_dma.c
@@ -3,7 +3,7 @@
  *
  * \brief SAM SERCOM I2C Slave with DMA Quick Start Guide
  *
- * Copyright (C) 2014-2015 Atmel Corporation. All rights reserved.
+ * Copyright (C) 2014-2016 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -96,7 +96,7 @@ struct dma_resource i2c_dma_resource;
 
 // [transfer_descriptor]
 COMPILER_ALIGNED(16)
-DmacDescriptor i2c_dma_descriptor;
+DmacDescriptor i2c_dma_descriptor SECTION_DMAC_DESCRIPTOR;
 // [transfer_descriptor]
 
 // [config_dma_resource]
