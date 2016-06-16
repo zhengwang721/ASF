8e4d725eec0877886a680d00190aac80d9ba5ee9
tonny.lan <shouzhen.lan@atmel.com>
2016-06-16 15:48:10 +0800

    Fix __I bit issue

--- sam0/drivers/tcc/tcc.h
+++ sam0/drivers/tcc/tcc.h
@@ -1853,7 +1853,7 @@ static inline void tcc_set_count_direction(
 	/* Get a pointer to the module's hardware instance */
 	Tcc *const tcc_module = module_inst->hw;
 
-	while (tcc_module->SYNCBUSY.bit.CTRLB) {
+	while (tcc_module->SYNCBUSY.reg) {
 		/* Wait for sync */
 	}
 
@@ -1883,7 +1883,7 @@ static inline void tcc_toggle_count_direction(
 	/* Get a pointer to the module's hardware instance */
 	Tcc *const tcc_module = module_inst->hw;
 
-	while (tcc_module->SYNCBUSY.bit.CTRLB) {
+	while (tcc_module->SYNCBUSY.reg) {
 		/* Wait for sync */
 	}
 	bool dir_value_1 = tcc_module->CTRLBSET.bit.DIR;
@@ -1938,7 +1938,7 @@ static inline void tcc_stop_counter(
 
 	/* Wait until last command is done */
 	do {
-		while (tcc_module->SYNCBUSY.bit.CTRLB) {
+		while (tcc_module->SYNCBUSY.reg) {
 			/* Wait for sync */
 		}
 		last_cmd = tcc_module->CTRLBSET.reg & TCC_CTRLBSET_CMD_Msk;
@@ -1977,7 +1977,7 @@ static inline void tcc_restart_counter(
 
 	/* Wait until last command is done */
 	do {
-		while (tcc_module->SYNCBUSY.bit.CTRLB) {
+		while (tcc_module->SYNCBUSY.reg) {
 			/* Wait for sync */
 		}
 		last_cmd = tcc_module->CTRLBSET.reg & TCC_CTRLBSET_CMD_Msk;
@@ -2021,14 +2021,14 @@ static inline void tcc_dma_trigger_command(
 	/* Get a pointer to the module's hardware instance */
 	Tcc *const tcc_module = module_inst->hw;
 
-	while (tcc_module->SYNCBUSY.bit.CTRLB) {
+	while (tcc_module->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 			/* Wait for sync */
 	}
 
 	/* Make certain that there are no conflicting commands in the register */
 	tcc_module->CTRLBCLR.reg = TCC_CTRLBCLR_CMD_NONE;
 
-	while (tcc_module->SYNCBUSY.bit.CTRLB) {
+	while (tcc_module->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 			/* Wait for sync */
 	}
 	
@@ -2117,7 +2117,7 @@ static inline void tcc_set_ramp_index(
 
 	/* Wait until last command is done */
 	do {
-		while (tcc_module->SYNCBUSY.bit.CTRLB) {
+		while (tcc_module->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 			/* Wait for sync */
 		}
 		if (TCC_RAMP_INDEX_DEFAULT == ramp_index) {
@@ -2245,7 +2245,7 @@ static inline void tcc_lock_double_buffer_update(
 	Assert(module_inst);
 	Assert(module_inst->hw);
 
-	while (module_inst->hw->SYNCBUSY.bit.CTRLB) {
+	while (module_inst->hw->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 		/* Wait for sync */
 	}
 	module_inst->hw->CTRLBSET.reg = TCC_CTRLBSET_LUPD;
@@ -2267,7 +2267,7 @@ static inline void tcc_unlock_double_buffer_update(
 	Assert(module_inst);
 	Assert(module_inst->hw);
 
-	while (module_inst->hw->SYNCBUSY.bit.CTRLB) {
+	while (module_inst->hw->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 		/* Wait for sync */
 	}
 	module_inst->hw->CTRLBCLR.reg = TCC_CTRLBCLR_LUPD;
@@ -2292,7 +2292,7 @@ static inline void tcc_force_double_buffer_update(
 
 	/* Wait until last command is done */
 	do {
-		while (tcc_module->SYNCBUSY.bit.CTRLB) {
+		while (tcc_module->SYNCBUSY.reg & TCC_SYNCBUSY_CTRLB) {
 			/* Wait for sync */
 		}
 		last_cmd = tcc_module->CTRLBSET.reg & TCC_CTRLBSET_CMD_Msk;
