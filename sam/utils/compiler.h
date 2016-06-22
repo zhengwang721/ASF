7607749b9c5787aa8e10c760c954e150fbe05b9a
“zheng.wang” <zheng.wang@atmel.com>
2016-06-22 15:21:54 +0800

    [ASFP-6074] Fix __builtin_ctz() issue when parameter is 0

--- sam/utils/compiler.h
+++ sam/utils/compiler.h
@@ -3,7 +3,7 @@
  *
  * \brief Commonly used includes, types and macros.
  *
- * Copyright (c) 2010-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2010-2016 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -582,9 +582,9 @@ typedef struct
  * \return The count of leading zero bits in \a u.
  */
 #if (defined __GNUC__) || (defined __CC_ARM)
-#   define clz(u)              __builtin_clz(u)
+#   define clz(u)              ((u) ? __builtin_ctz(u) : 32)
 #elif (defined __ICCARM__)
-#   define clz(u)              __CLZ(u)
+#   define clz(u)              ((u) ? __CLZ(u) : 32)
 #else
 #   define clz(u)              (((u) == 0)          ? 32 : \
                                 ((u) & (1ul << 31)) ?  0 : \
